14102ceb1dfec48425c42b33106af849d590c6ec
Matej Nikorovič <matej.nikorovic@vacuumlabs.com>
2018-10-11 15:47:47 +0200

    Added native system authentication

--- android/app/src/main/java/com/emurgo/MainApplication.java
+++ android/app/src/main/java/com/emurgo/MainApplication.java
@@ -3,6 +3,7 @@ package com.emurgo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.tradle.react.LocalAuthPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LocalAuthPackage(),
             new LinearGradientPackage(),
             new SvgPackage()
       );
