27b5795e9758727e7b9c1a9f65ebdbd8845b7853
Matej Nikorovič <matej.nikorovic@vacuumlabs.com>
2018-10-15 11:53:15 +0200

    Added biometry check

--- android/app/src/main/java/com/emurgo/MainApplication.java
+++ android/app/src/main/java/com/emurgo/MainApplication.java
@@ -3,6 +3,7 @@ package com.emurgo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.rnfs.RNFSPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FingerprintAuthPackage(),
             new RandomBytesPackage(),
             new RNFSPackage(),
             new LinearGradientPackage(),
