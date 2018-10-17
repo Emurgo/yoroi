cd941e3c93fbb1339b5e4f435facb27ddfca0b1b
Peter Peresini <peter.peresini@gmail.com>
2018-10-17 13:26:58 +0200

    Use describe+it to group api tests

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,39 +1,38 @@
 // @flow
-import fetch from 'node-fetch'
+import '../jest'
+
 import moment from 'moment'
 
 import * as api from './api'
 import {ApiError} from './errors'
-import {Logger, LogLevel} from '../utils/logging'
-global.fetch = fetch
 
-Logger.setLogLevel(LogLevel.Info)
+describe('History API', () => {
+  it('can fetch history', async () => {
 
-test('API: can fetch history', async () => {
-
-  const addresses = [
-    'Ae2tdPwUPEZKAx4zt8YLTGxrhX9L6R8QPWNeefZsPgwaigWab4mEw1ECUZ7',
-  ]
-  const ts = moment('1970-01-01')
+    const addresses = [
+      'Ae2tdPwUPEZKAx4zt8YLTGxrhX9L6R8QPWNeefZsPgwaigWab4mEw1ECUZ7',
+    ]
+    const ts = moment('1970-01-01')
 
 
-  // We are async
-  expect.assertions(1)
-  const result = await api.fetchNewTxHistory(ts, addresses)
+    // We are async
+    expect.assertions(1)
+    const result = await api.fetchNewTxHistory(ts, addresses)
 
-  // $FlowFixMe it seems like toMatchSnapshot is badly typed
-  expect(result[0]).toMatchSnapshot({best_block_num: expect.any(String)})
-})
+    // $FlowFixMe it seems like toMatchSnapshot is badly typed
+    expect(result[0]).toMatchSnapshot({best_block_num: expect.any(String)})
+  })
 
-test('API: throws ApiError', async () => {
+  it('throws ApiError on bad request', async () => {
 
-  const addresses = []
-  const ts = 'not-a-date'
+    const addresses = []
+    const ts = 'not-a-date'
 
-  // We are async
-  expect.assertions(1)
+    // We are async
+    expect.assertions(1)
 
-  await expect(
-    api.fetchNewTxHistory(ts, addresses)
-  ).rejects.toThrow(ApiError)
+    await expect(
+      api.fetchNewTxHistory(ts, addresses)
+    ).rejects.toThrow(ApiError)
+  })
 })
