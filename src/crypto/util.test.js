3ec3ce6258af0237b6ff807e2d66f88b227da6a1
Peter Peresini <peter.peresini@gmail.com>
2018-10-18 18:03:15 +0200

    Remove unused discovery logic

--- src/crypto/util.test.js
+++ src/crypto/util.test.js
@@ -64,46 +64,6 @@ test('Can validate invalid addresses', () => {
   addresses.forEach((address) => expect(isValidAddress(address)).toBe(false))
 })
 
-
-test('Can discover addresses', async () => {
-  const masterKey = getMasterKeyFromMnemonic(mnemonic)
-  const account = getAccountFromMasterKey(masterKey, CARDANO_CONFIG.TESTNET.PROTOCOL_MAGIC)
-  const used = getExternalAddresses(account, [0, 2, 7, 14, 21, 28, 35, 40, 45, 50, 55, 60])
-
-  const filterUsed = (addresses) => Promise.resolve(addresses.filter((addr) => used.includes(addr)))
-
-  const discover = (gapLimit, batchSize) => discoverAddresses({
-    account,
-    type: 'External',
-    highestUsedIndex: -1,
-    startIndex: 0,
-    filterUsedAddressesFn: filterUsed,
-    gapLimit,
-    batchSize,
-  })
-
-  expect.assertions(5)
-  // -1] [# 1] [...]
-  const result1 = await discover(1, 2)
-  expect(result1.addresses.length).toBe(2)
-
-  // -1] [# 1 # 3 4] [5 6 # 8 9]
-  const result2 = await discover(2, 5)
-  expect(result2.addresses.length).toBe(5)
-
-  // -1] [# 1 # 3 4] [5 6 # 8 9] [10 11 12 13 #] [15 16 17 18 19]
-  const result3 = await discover(3, 5)
-  expect(result3.addresses.length).toBe(20)
-
-  // -1] [# 1 # 3 4 5 6 # 8 9] [10 11 12 13 # 15 16 17 18 19]
-  const result4 = await discover(5, 10)
-  expect(result4.addresses.length).toBe(20)
-
-  // check if batchSize < gapSize works
-  // -1] [#] [1] [#] [3] [4] [5] [6] [#] [8] [9] [10] [11] [12]
-  const result5 = await discover(5, 1)
-  // TODO(ppershing): figure out why this results in 13 and not 12
-  expect(result5.addresses.length).toBe(13)
 })
 
 
@@ -118,4 +78,3 @@ test('Can encrypt / decrypt masterKey', () => {
 test('Make sure that we are using safe buffers', () => {
   // in response to https://github.com/nodejs/node/issues/4660
   expect((new Buffer(10)).toString('hex')).toBe('00000000000000000000')
-})
