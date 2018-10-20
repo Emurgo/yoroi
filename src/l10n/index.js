07dd95fcd92e742cff94559068930377d1d06e47
Peter Peresini <peter.peresini@gmail.com>
2018-10-20 07:50:49 +0200

    Add automatic dummy language translations

--- src/l10n/index.js
+++ src/l10n/index.js
@@ -1,13 +1,52 @@
 // @flow
+import _ from 'lodash'
 import LocalizedStrings from 'localized-strings'
 
 import en from './en'
 
 import type {Translation} from './type'
 
+const transform = (obj, transformer) =>
+  _.isPlainObject(obj)
+    ? _.mapValues(obj, (v) => transform(v, transformer))
+    : transformer(obj)
+
+
+// "Translates" a string or a function returning string
+// into random characters from targetChars
+const dummyTranslate = (targetChars) => (obj: any): any => {
+  const replace = (str: string) => {
+    let oldStr = ''
+    while (str !== oldStr) {
+      oldStr = str
+      const target = targetChars[Math.floor(targetChars.length * Math.random())]
+
+      str = str.replace(/[a-zA-Z]/, target)
+    }
+    return str
+  }
+  if (_.isFunction(obj)) {
+    return (...args) => replace(obj(...args))
+  } else {
+    return replace(obj)
+  }
+}
+
+const dummyCn = (
+  transform(en, dummyTranslate('发送支援请求时出现问题')): Translation
+)
+
+const dummyJa = (
+  transform(en, dummyTranslate('インポートしようとしたウ')): Translation
+)
+
+
 const strings = new LocalizedStrings({
   en,
+  cn: dummyCn,
+  ja: dummyJa,
 })
 
+strings.setLanguage('ja')
 
 export default (strings: Translation)
