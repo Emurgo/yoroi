f8cb4bc91e025277dbe09e3de03c3818c70eb873
Peter Peresini <peter.peresini@gmail.com>
2018-10-28 13:44:24 +0100

    Fix TxDetails screen

--- src/components/TxHistory/TxHistoryNavigator.js
+++ src/components/TxHistory/TxHistoryNavigator.js
@@ -7,7 +7,12 @@ import {TX_HISTORY_ROUTES} from '../../RoutesList'
 const TxHistoryNavigator = createStackNavigator(
   {
     [TX_HISTORY_ROUTES.MAIN]: TxHistory,
-    [TX_HISTORY_ROUTES.TX_DETAIL]: TxDetails,
+    [TX_HISTORY_ROUTES.TX_DETAIL]: {
+      screen: TxDetails,
+      navigationOptions: ({navigation}) => ({
+        title: navigation.getParam('title'),
+      }),
+    },
   },
   {
     initialRouteName: TX_HISTORY_ROUTES.MAIN,
