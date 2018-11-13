51bb777f33d0cfa0baa980199166f9155e58a7d0
Peter Peresini <peter.peresini@gmail.com>
2018-11-13 17:24:55 +0100

    Make navigation to Settings work, also add missing back buttons on some screens

--- src/components/TxHistory/TxHistoryNavigator.js
+++ src/components/TxHistory/TxHistoryNavigator.js
@@ -1,17 +1,37 @@
 // @flow
+import React from 'react'
+import {Button} from '../UiKit'
 import {createStackNavigator} from 'react-navigation'
 import TxHistory from './TxHistory'
 import TxDetails from './TxDetails'
-import {TX_HISTORY_ROUTES} from '../../RoutesList'
+import {TX_HISTORY_ROUTES, WALLET_ROUTES} from '../../RoutesList'
+import SettingsScreenNavigator from '../Settings/SettingsScreenNavigator'
 
 const TxHistoryNavigator = createStackNavigator(
   {
-    [TX_HISTORY_ROUTES.MAIN]: TxHistory,
+    [TX_HISTORY_ROUTES.MAIN]: {
+      screen: TxHistory,
+      navigationOptions: ({navigation}) => ({
+        title: navigation.getParam('title'),
+        headerRight: (
+          <Button
+            onPress={() => navigation.navigate(WALLET_ROUTES.SETTINGS)}
+            title={'\u2699'}
+          />
+        ),
+      }),
+    },
     [TX_HISTORY_ROUTES.TX_DETAIL]: {
       screen: TxDetails,
       navigationOptions: ({navigation}) => ({
         title: navigation.getParam('title'),
       }),
+    },
+    [WALLET_ROUTES.SETTINGS]: {
+      screen: SettingsScreenNavigator,
+      navigationOptions: {
+        header: null,
+      },
     },
   },
   {
