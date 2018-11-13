2b51913edcbda43c157922bb0d861435abd19937
David Pribula <davidpribula@gmail.com>
2018-11-13 17:15:13 +0100

    Adding title to the RecoveryPhraseExplanationDialog

--- src/components/WalletInit/CreateWallet/RecoveryPhraseExplanationDialog.js
+++ src/components/WalletInit/CreateWallet/RecoveryPhraseExplanationDialog.js
@@ -17,6 +17,10 @@ import {COLORS} from '../../../styles/config'
 import type {State} from '../../../state'
 import type {SubTranslation} from '../../../l10n/typeHelpers'
 
+import {withNavigationTitle, withTranslations} from '../../../utils/renderUtils'
+import type {Navigation} from '../../../types/navigation'
+import type {ComponentType} from 'react'
+
 const getTranslations = (state: State) =>
   state.trans.RecoveryPhraseExplanationDialog
 
@@ -43,10 +47,9 @@ const RecoveryPhraseExplanationDialog = ({
   </Screen>
 )
 
-export default compose(
-  connect((state) => ({
-    translations: getTranslations(state),
-  })),
+export default (compose(
+  withTranslations(getTranslations),
+  withNavigationTitle(({translations}) => translations.title),
   withHandlers({
     navigateToRecoveryPhrase: ({navigation}) => (event) => {
       const name = navigation.getParam('name')
@@ -60,4 +63,4 @@ export default compose(
       })
     },
   }),
-)(RecoveryPhraseExplanationDialog)
+)(RecoveryPhraseExplanationDialog): ComponentType<{navigation: Navigation}>)
