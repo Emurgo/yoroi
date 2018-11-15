429fc88448ab664283152c53f406e0e0e7666458
Peter Peresini <peter.peresini@gmail.com>
2018-11-15 11:30:23 +0100

    Add NavigationService so that redux can navigate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 
 import React from 'react'
 import AppNavigator from './AppNavigator'
+import NavigationService from './NavigationService'
 
-const App = () => <AppNavigator />
+const App = () => (
+  <AppNavigator
+    ref={(navigatorRef) => {
+      NavigationService.setTopLevelNavigator(navigatorRef)
+    }}
+  />
+)
 
 export default App
