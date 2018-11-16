d4429dbe6f7f4b5942d942580e93d9d877e8acd9
David Pribula <davidpribula@gmail.com>
2018-11-16 11:36:18 +0100

    Localization file refactor

--- src/l10n/en.js
+++ src/l10n/en.js
@@ -24,40 +24,51 @@ const l10n = {
       offline: 'You are offline. Please check settings on your device.',
     },
   },
-  LoginScreen: {
+
+  Login: {
     title: 'Enter PIN',
   },
+
   LanguagePicker: {
     selectLanguage: 'SELECT YOUR LANGAUAGE',
-    continue: 'CHOOSE LANGUAGE',
+    chooseLanguage: 'CHOOSE LANGUAGE',
   },
-  TxHistoryListItem: {
-    transactionType: {
-      SENT: 'ADA sent',
-      RECEIVED: 'ADA received',
-      SELF: 'Intrawallet',
-      MULTI: 'Multiparty',
-    },
-    assuranceLevelHeader: 'Assurance level:',
-    assuranceLevel: {
-      LOW: 'Low',
-      MEDIUM: 'Medium',
-      HIGH: 'High',
-      PENDING: 'Pending',
-      FAILED: 'Failed',
-    },
-    fee: (fee: number) => `Fee: ${fee}`,
-  },
+
   TxHistory: {
     availableAmount: {
       label: 'Available funds',
     },
+
+    TxHistoryListItem: {
+      transactionType: {
+        SENT: 'ADA sent',
+        RECEIVED: 'ADA received',
+        SELF: 'Intrawallet',
+        MULTI: 'Multiparty',
+      },
+      assuranceLevelHeader: 'Assurance level:',
+      assuranceLevel: {
+        LOW: 'Low',
+        MEDIUM: 'Medium',
+        HIGH: 'High',
+        PENDING: 'Pending',
+        FAILED: 'Failed',
+      },
+      fee: (fee: number) => `Fee: ${fee}`,
+    },
+
+    TxNavigationButtons: {
+      sendButton: 'SEND',
+      receiveButton: 'RECEIVE',
+    },
   },
+
   TxDetails: {
     fromAddresses: 'From Addresses',
     toAddresses: 'To Addresses',
     transactionId: 'Transaction ID',
     txAssuranceLevel: 'Transaction assurance level',
+
     transactionHeader: {
       SENT: 'ADA sent',
       RECEIVED: 'ADA received',
@@ -67,61 +78,82 @@ const l10n = {
     formatConfirmations: (cnt: number) =>
       `${cnt} ${pluralizeEn(cnt, 'CONFIRMATION', 'CONFIRMATIONS')}`,
   },
-  TxNavigationButtons: {
-    sendButton: 'SEND',
-    receiveButton: 'RECEIVE',
-  },
-  Send: {
-    Main: {
-      title: 'Send',
-      scanCode: 'Scan QR code',
-      address: 'Address',
-      amount: 'Amount',
-      continue: 'Continue',
-      availableAmount: {
-        label: 'Available amount:',
-        isFetching: 'Checking balance...',
-        hasError: 'Error',
-      },
-      validationErrors: {
-        pendingOutgoingTransaction: [
-          'You cannot send a new transaction while',
-          'an existing one is still pending',
-        ].join(' '),
-        offline: 'You are offline',
-        serverFailed: 'There is a failing transaction',
-        invalidAddress: 'Please enter valid address',
-        invalidAmountErrors: {
-          INVALID_AMOUNT: 'Please enter valid amount',
-          INSUFFICIENT_BALANCE: 'Not enough money to make this transaction',
-        },
-      },
-      fetchingError:
-        'We are experiencing issue with fetching your current balance.',
+
+  SendTx: {
+    title: 'Send',
+    scanCode: 'Scan QR code',
+    address: 'Address',
+    amount: 'Amount',
+    continue: 'Continue',
+    fetchingError:
+      'We are experiencing issue with fetching your current balance.',
+
+    AvailableAmount: {
+      label: 'Available amount:',
+      isFetching: 'Checking balance...',
+      error: 'Error',
     },
+
+    ValidationErrors: {
+      pendingOutgoingTransaction: [
+        'You cannot send a new transaction while',
+        'an existing one is still pending',
+      ].join(' '),
+      offline: 'You are offline',
+      serverFailed: 'There is a failing transaction',
+      invalidAddress: 'Please enter valid address',
+      invalidAmountErrors: {
+        INVALID_AMOUNT: 'Please enter valid amount',
+        INSUFFICIENT_BALANCE: 'Not enough money to make this transaction',
+      },
+    },
+
     Confirmation: {
       confirmButton: 'Confirm',
       amount: 'Amount',
       availableFunds: 'AVAILABLE FUNDS:',
       balanceAfterTx: 'Balance after transaction',
       fees: 'Fees',
-      password: 'Wallet password',
+      walletPassword: 'Wallet password',
       receiver: 'Receiver',
     },
-    SubmitModal: {
-      submitting: 'Please wait while transaction is submited',
+    submitWaitMessage: 'Please wait while transaction is submited',
+  },
+
+  ReceiveTx: {
+    title: 'Receive',
+    infoText: [
+      'Share this address to receive payments.',
+      'To protect your privacy, new address are',
+      'generated automatically once you use them.',
+    ].join(' '),
+    generate: 'GENERATE',
+    cannotGenerate: 'You have to use some of your addresses',
+    // TODO put into ReceiveScreen
+    walletAddressTitle: 'Your wallet address',
+    walletAddressesTitle: 'Your wallet addresses',
+    hideUsedAddresses: 'hide used',
+    showUsedAddresses: 'show used',
+
+    AddressDetail: {
+      BIP32path: 'BIP32 path:',
+      copyLabel: 'COPY ADDRESS',
+      copiedLabel: 'COPIED',
     },
   },
-  WalletInitScreen: {
+
+  WalletInit: {
     createWallet: 'CREATE A NEW WALLET',
     restoreWallet: 'RESTORE WALLET FROM BACKUP',
+
+    WalletDescription: {
+      webLightWalletMessage: 'Yoroi is Web Light Wallet for Cardano',
+      secureFastSimpleMessage: 'Secure Fast Simple',
+      byEmurgo: 'By',
+    },
   },
-  WalletDescription: {
-    line1: 'Yoroi is Web Light Wallet for Cardano',
-    line2: 'Secure Fast Simple',
-    byEmurgo: 'By',
-  },
-  CreateWalletScreen: {
+
+  CreateWallet: {
     title: 'Create a new wallet',
     nameLabel: 'Wallet name',
     passwordLabel: 'Wallet password',
@@ -133,80 +165,55 @@ const l10n = {
     passwordNumber: '1 number',
     createButton: 'CREATE PERSONAL WALLET',
   },
-  ChangeWalletName: {
-    walletName: 'Wallet name',
-    changeButtonText: 'CHANGE NAME',
-    walletValidationText:
-      'Wallet name requires at least 3 and at most\
- 40 letters.',
-  },
-  ReceiveScreen: {
-    title: 'Receive',
-    infoText: [
-      'Share this address to receive payments.',
-      'To protect your privacy, new address are',
-      'generated automatically once you use them.',
-    ].join(' '),
-    generate: 'GENERATE',
-    cannotGenerate: 'You have to use some of your addresses',
-  },
-  AddressDetail: {
-    walletAddress: 'Your wallet address',
-  },
-  AddressesList: {
-    walletAddresses: 'Your wallet addresses',
-    hideUsedAddresses: 'hide used',
-    showUsedAddresses: 'show used',
-  },
-  AddressModal: {
-    BIP32path: 'BIP32 path:',
-    copyLabel: 'COPY ADDRESS',
-    copiedLabel: 'COPIED',
-  },
-  RecoveryPhraseConfirmationDialog: {
-    title: 'Recovery phrase',
-    keysStorageCheckbox:
-      'I understand that my secret keys are held securely on this device only,\
- not on the company`s servers',
-    newDeviceRecoveryCheckbox:
-      'I understand that if this application is moved to another device\
- or delete, my money can be only recovered with the backup phrase that\
- I have written down and saved in secure place.',
-    confirmationButton: 'I UNDERSTAND',
-  },
-  RecoveryPhraseConfirmationScreen: {
-    title: 'Recovery phrase',
-    instructions:
-      'Tap each word in the correct order to verify your recovery phrase',
-    inputLabel: 'Recovery phrase',
-    invalidPhrase: 'Recovery phrase does not match',
-    clearButton: 'CLEAR',
-    confirmButton: 'CONFIRM',
-  },
-  RecoveryPhraseExplanationDialog: {
-    title: 'Recovery phrase',
-    paragraph1:
-      'On the following screen, you will see a set of 15 random words. This\
- is your wallet backup phrase. It can be entered in version of Yoroi in order\
- to back up or restore your wallet`s funds and private key.',
-    paragraph2:
-      'Make sure nobody looks into your screen unless you want them to have\
- access to your funds.',
-    nextButton: 'NEXT',
-  },
-  RecoveryPhraseScreen: {
+
+  RecoveryPhrase: {
     title: 'Recovery phrase',
     mnemonicNote:
       'Please, make sure you have carefully written down your recovery phrase\
- somewhere safe. You will need this phrase to use and restore your wallet.\
- Phrase is case sensitive.',
+      somewhere safe. You will need this phrase to use and restore your wallet.\
+      Phrase is case sensitive.',
     confirmationButton: 'YES, I`VE WRITTEN IT DOWN',
+
+    RecoveryExplanationDialog: {
+      title: 'Recovery phrase',
+      paragraph1:
+        'On the following screen, you will see a set of 15 random words. This\
+      is your wallet backup phrase. It can be entered in version of Yoroi in order\
+      to back up or restore your wallet`s funds and private key.',
+      paragraph2:
+        'Make sure nobody looks into your screen unless you want them to have\
+        access to your funds.',
+      nextButton: 'NEXT',
+    },
+
+    RecoveryConfirmationScreen: {
+      title: 'Recovery phrase',
+      instructions:
+        'Tap each word in the correct order to verify your recovery phrase',
+      inputLabel: 'Recovery phrase',
+      invalidPhrase: 'Recovery phrase does not match',
+      clearButton: 'CLEAR',
+      confirmButton: 'CONFIRM',
+    },
+
+    RecoveryConfirmationDialog: {
+      title: 'Recovery phrase',
+      keysStorageCheckbox:
+        'I understand that my secret keys are held securely on this device only,\
+      not on the company`s servers',
+      newDeviceRecoveryCheckbox:
+        'I understand that if this application is moved to another device\
+      or delete, my money can be only recovered with the backup phrase that\
+      I have written down and saved in secure place.',
+      confirmationButton: 'I UNDERSTAND',
+    },
   },
-  RestoreWalletScreen: {
+
+  RestoreWallet: {
     title: 'Restore wallet',
     instructions:
       'To restore your wallet please provide the recovery phrase you\
- received when you created your wallet for the first time.',
+      received when you created your wallet for the first time.',
     phrase: 'Recovery phrase',
     restoreButton: 'RESTORE WALLET',
     errors: {
@@ -218,54 +225,84 @@ const l10n = {
       },
     },
   },
-  SettingsScreen: {
+
+  Settings: {
     title: 'Settings',
-    walletName: 'Wallet name',
     edit: 'Edit',
     privacy: 'Privacy',
-    changePin: 'Change PIN',
-    changePassword: 'Change password',
-    fingerprintSignIn: 'Sign in with your fingerprint',
-    easyConfirmation: 'Easy transaction confirmation',
-    downloadLogs: 'Download logs',
-    downloadLogsText:
-      'If you want to inspect logs, you can download\
- them here. Logs do not contain sensitive information, and it would be\
- helpful to attach them to problem reports to help the team investigate\
- the issue you are experiencing.',
-    removeWallet: 'Remove wallet',
-    language: 'Your language',
-    termsOfUse: 'Terms of Use',
-    support: 'Support',
-  },
-  SupportScreen: {
-    title: 'Support',
-    faq: {
-      label: 'See frequently asked questions',
-      description:
-        'If you are experiencing issues, please see the FAQ\
- on Yoroi website for quidance on known issues.',
-      url: 'https://yoroi-wallet.com/faq/',
+
+    WalletSettings: {
+      walletName: 'Wallet name',
+      ChangeWalletName: {
+        walletName: 'Wallet name',
+        changeButtonText: 'CHANGE NAME',
+        walletValidationText:
+          'Wallet name requires at least 3 and at most\
+       40 letters.',
+      },
+
+      changePasswordTitle: 'Change password',
+      ChangePassword: {},
+
+      removeWalletTitle: 'Remove wallet',
+      RemoveWalet: {},
+
+      easyConfirmationTitle: 'Easy transaction confirmation',
+      EasyConfirmation: {},
     },
-    report: {
-      label: 'Report a problem',
-      description:
-        'If the FAQ does not solve the issue you are\
- experiencing, please use our Support request feature.',
-      url: 'https://yoroi-wallet.com/support/',
+
+    ApplicationSettings: {
+      languageTitle: 'Your language',
+      Language: {},
+
+      changePinTitle: 'Change PIN',
+      ChangePin: {},
+
+      fingerprintSignInTitle: 'Sign in with your fingerprint',
+
+      downloadLogsTitle: 'Download logs',
+      downloadLogsText:
+        'If you want to inspect logs, you can download\
+      them here. Logs do not contain sensitive information, and it would be\
+      helpful to attach them to problem reports to help the team investigate\
+      the issue you are experiencing.',
+
+      termsOfUseTitle: 'Terms of use',
+      TermsOfService: {
+        title: 'Terms of Service Agreement',
+        content: [...termsOfService],
+      },
+
+      supportTitle: 'Support',
+      Support: {
+        title: 'Support',
+        faq: {
+          label: 'See frequently asked questions',
+          description:
+            'If you are experiencing issues, please see the FAQ\
+     on Yoroi website for quidance on known issues.',
+          url: 'https://yoroi-wallet.com/faq/',
+        },
+        report: {
+          label: 'Report a problem',
+          description:
+            'If the FAQ does not solve the issue you are\
+     experiencing, please use our Support request feature.',
+          url: 'https://yoroi-wallet.com/support/',
+        },
+      },
     },
   },
-  TermsOfServiceScreen: {
-    title: 'Terms of Service Agreement',
-    content: [...termsOfService],
-  },
+
   WalletSelectionScreen: {
     addWallet: 'Add wallet',
     header: 'Your wallets',
   },
+
   SigninScreen: {
     welcomeText: 'Sign with PIN / TouchID',
   },
+
   ConfirmScreen: {
     ErrorDialogs: {
       WrongPassword: {
@@ -278,6 +315,7 @@ const l10n = {
       },
       okTextButton: 'OK',
     },
+
     Confirmation: {
       confirmButton: 'Confirm',
       amount: 'Amount',
@@ -288,30 +326,35 @@ const l10n = {
       receiver: 'Receiver',
     },
   },
-  FingerprintLinkScreen: {
+
+  FingerprintLink: {
     enableFingerprintsMessage:
       'Enable use of fingerprints in device settings first!',
     notNowButton: 'Not now',
     linkButton: 'Link',
   },
+
   BiometryAuth: {
     errorDialogTitle: 'Error',
+
+    Authentication: {
+      authenticationRequiredTitle: 'Authentication required',
+      touchInstructions: 'Touch the sensor',
+      withPin: 'Authenticate using PIN',
+      pinCodeLabel: ' ',
+    },
+
     NotSupportedErrors: {
       deviceNotSupported: 'Your device does not support biometry!',
       biometryOrPinNotSet: 'Go to system setttings and rise your security',
       pinSetButBiometryNot: 'Go to system setttings and rise your security',
       notSupported: 'Not supported error',
     },
+
     AuthenticateErrors: {
       text: 'Authenticate error',
       canceled: 'User canceled authentication',
       failed: 'Authentication failed',
-    },
-    Authentication: {
-      authenticationRequiredTitle: 'Authentication required',
-      touchInstructions: 'Touch the sensor',
-      withPin: 'Authenticate using PIN',
-      pinCodeLabel: ' ',
     },
   },
 }
