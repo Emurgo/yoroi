6a6b853fdda964d4cfb2f621468b6bbd865590ab
Dusan Plavak <dusan.plavak@gmail.com>
2018-11-27 17:22:22 +0100

    Fix missing create wallet title

--- src/components/WalletInit/CreateWallet/CreateWalletScreen.js
+++ src/components/WalletInit/CreateWallet/CreateWalletScreen.js
@@ -2,18 +2,16 @@
 
 import React from 'react'
 import {compose} from 'redux'
-import {connect} from 'react-redux'
 import {withHandlers, withState} from 'recompose'
 
 import MnemonicExplanationModal from './MnemonicExplanationModal'
 import {WALLET_INIT_ROUTES} from '../../../RoutesList'
-import {withNavigationTitle} from '../../../utils/renderUtils'
+import {withNavigationTitle, withTranslations} from '../../../utils/renderUtils'
 import WalletForm from '../WalletForm'
 
 import type {State} from '../../../state'
 
-const getTranslations = (state: State) =>
-  state.trans.CreateOrRestoreWalletScreen
+const getTranslations = (state: State) => state.trans.CreateWalletScreen
 
 const CreateWalletScreen = ({
   formSubmit,
@@ -32,9 +30,7 @@ const CreateWalletScreen = ({
 )
 
 export default compose(
-  connect((state) => ({
-    translations: getTranslations(state),
-  })),
+  withTranslations(getTranslations),
   withNavigationTitle(({translations}) => translations.title),
   withState('visibleMnemonicExplanation', 'setMnemonicExplanation', false),
   withState('formData', 'setFormData', null),
