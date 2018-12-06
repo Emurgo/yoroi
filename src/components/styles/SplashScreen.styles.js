640599e59c5fdb03060bd33c7d0b20a50589827f
Jozef Latka <jozef.latka@hotmail.com>
2018-12-06 08:54:40 +0100

    Styled splash screen

---
+++ src/components/styles/SplashScreen.styles.js
@@ -0,0 +1,15 @@
+// @flow
+
+import {StyleSheet} from 'react-native'
+
+import {COLORS} from '../../styles/config'
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: COLORS.BACKGROUND_BLUE,
+  },
+})
+
+export default styles
