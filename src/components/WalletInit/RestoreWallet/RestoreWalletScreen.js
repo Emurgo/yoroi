e74c445629b63cdce204632490dda758fad48137
Peter Peresini <peter.peresini@gmail.com>
2018-12-12 15:46:23 +0100

    Add "visible-password" keyboard type to ValidatedInput

--- src/components/WalletInit/RestoreWallet/RestoreWalletScreen.js
+++ src/components/WalletInit/RestoreWallet/RestoreWalletScreen.js
@@ -91,6 +91,7 @@ const RestoreWalletScreen = ({
             error={errorText}
             // hopefully this prevents keyboard from learning the mnemonic
             autoCorrect={false}
+            keyboardType="visible-password"
           />
         </View>
         <Button
