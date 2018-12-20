201ab3771a494bba86831e4cf8e6df48ca773b7c
Jan Ferko <julyloov@gmail.com>
2018-12-20 17:32:21 +0100

    Add default value parameter to env.getBoolean

--- src/env.js
+++ src/env.js
@@ -1,9 +1,13 @@
 // @flow
 import BuildConfig from 'react-native-config'
+import _ from 'lodash'
 
 const getString = (key: string) => BuildConfig[key]
 
-const getBoolean = (key: string) => getString(key) === 'true'
+const getBoolean = (key: string, defaultValue: boolean) => {
+  const value = getString(key)
+  return _.isNil(value) ? defaultValue : value === 'true'
+}
 
 export default {
   getString,
