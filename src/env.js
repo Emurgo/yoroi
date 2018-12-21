e1d394b5c7e0af5869e7f469e1d5138b4622023e
Jan Ferko <julyloov@gmail.com>
2018-12-21 10:08:20 +0100

    Add stricter boolean parsing from env variables

--- src/env.js
+++ src/env.js
@@ -1,12 +1,18 @@
 // @flow
 import BuildConfig from 'react-native-config'
-import _ from 'lodash'
 
 const getString = (key: string) => BuildConfig[key]
 
 const getBoolean = (key: string, defaultValue: boolean) => {
   const value = getString(key)
-  return _.isNil(value) ? defaultValue : value === 'true'
+  switch (value) {
+    case 'true':
+      return true
+    case 'false':
+      return false
+    default:
+      return defaultValue
+  }
 }
 
 export default {
