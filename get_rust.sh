699f0b9bbc4ba4b6c71582c4d999c21a29204493
Nicolas Arqueros <nykholas@gmail.com>
2019-01-19 13:24:48 -0500

    add missing targets for ios

--- get_rust.sh
+++ get_rust.sh
@@ -11,7 +11,8 @@ else
 
   # use nightly version. 
   rustup install nightly-2018-06-05
-  rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
+  rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android \
+                    aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
   cargo install cargo-lipo
   export
   cargo
