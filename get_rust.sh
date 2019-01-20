9fe0aa6ebe0f0ffe4de3fd85704e2272f7561fc6
Nicolas Arqueros <nykholas@gmail.com>
2019-01-20 01:58:57 -0500

    another try with rust

--- get_rust.sh
+++ get_rust.sh
@@ -10,7 +10,8 @@ else
   source $HOME/.cargo/env
 
   # use nightly version. 
-  rustup install nightly-2018-10-30
+  rustup install nightly-2018-11-25
+  rustup default nightly-2018-11-25
   rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android \
                     aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
   cargo install cargo-lipo
