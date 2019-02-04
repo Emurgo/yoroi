63bf7a96b14ee0018ff8e201ad23e5d2b5e3b68c
Mykola Kharechko <mykola.kharechko@infopulse.com>
2019-02-04 15:03:33 +0200

    make  optional.

--- get_rust.sh
+++ get_rust.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> $BASH_ENV
-
+source $HOME/.cargo/env
 if rustup --version; then
   echo "Rustup is already installed"
 else
@@ -14,9 +13,16 @@ else
   rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
 
   # cargo-lipo required only for ios build
-  if [ "$1" != "--skip-cargo-lipo" ] || [ -z "$YOROI_ANDROID_BUILD" ]
+  if [ "$1" != "--skip-cargo-lipo" ] || [[ -z "$YOROI_ANDROID_BUILD" ]]
   then
     cargo install cargo-lipo
+  fi
+
+  if [[ -z "$BASH_ENV" ]]
+  then
+    echo "$BASH_ENV not set. Rust not added to $PATH"
+  else
+    echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> $BASH_ENV
   fi
 
   export
