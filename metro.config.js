7f8e6daa3f497ce8bf6134b080c145b426ac3b17
Nicolas Arqueros <nykholas@gmail.com>
2019-07-14 17:23:52 -0400

    fix compilation issue

--- metro.config.js
+++ metro.config.js
@@ -14,4 +14,18 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    assetExts: ['png', 'md', 'json'],
+    extraNodeModules: {
+      buffer: require.resolve('safe-buffer'),
+      crypto: require.resolve('react-native-crypto'),
+      event: require.resolve('events'),
+      fs: require.resolve('react-native-fs'),
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('stream-browserify'),
+      url: require.resolve('url'),
+      util: require.resolve('util'),
+      vm: require.resolve('vm-browserify'),
+    },
+  },
 }
