246536ddad49eb82038f5ac430c8b04897587632
v-almonacid <ealmonacid@gmail.com>
2019-08-14 20:15:35 +0200

    use short commit and fix script

--- get_commit.sh
+++ get_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROD_ENV_FILE=.env.production
 STAGING_ENV_FILE=.env.staging
-LAST_COMMIT=$(git rev-parse HEAD)
+LAST_COMMIT=$(git rev-parse --short HEAD)
 echo "Building app on commit $LAST_COMMIT"
 
 if [ -f "$PROD_ENV_FILE" ]; then
@@ -10,7 +10,7 @@ if [ -f "$PROD_ENV_FILE" ]; then
     echo "COMMIT key not detected in $PROD_ENV_FILE file. Inserting last commit hash..."
     echo "COMMIT=$LAST_COMMIT" >> $PROD_ENV_FILE
   else
-    sed "/^COMMIT/s/=.*$/=$LAST_COMMIT/" $PROD_ENV_FILE
+    sed -i.bak "/^COMMIT/s/=.*$/=$LAST_COMMIT/" $PROD_ENV_FILE
   fi
 fi
 
@@ -20,6 +20,8 @@ if [ -f "$STAGING_ENV_FILE" ]; then
     echo "COMMIT key not detected in $STAGING_ENV_FILE file. Inserting last commit hash..."
     echo "COMMIT=$LAST_COMMIT" >> $STAGING_ENV_FILE
   else
-    sed "/^COMMIT/s/=.*$/=$LAST_COMMIT/" $STAGING_ENV_FILE
+    sed -i.bak "/^COMMIT/s/=.*$/=$LAST_COMMIT/" $STAGING_ENV_FILE
   fi
 fi
+
+find . -name "*.bak" -type f -delete
