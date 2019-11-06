9e0cc1eae7c689f876e64c198efa9c0c3ee0bf08
v-almonacid <ealmonacid@gmail.com>
2019-11-06 17:33:50 +0100

    forgot turn off debug screen

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import env from './env'
 
 const IS_DEBUG = __DEV__
 // debugging flags
-const _SHOW_INIT_DEBUG_SCREEN = true
+const _SHOW_INIT_DEBUG_SCREEN = false
 const _PREFILL_WALLET_INFO = false
 const _USE_TESTNET = env.getBoolean('USE_TESTNET', true)
 const _SENTRY = env.getString('SENTRY')
