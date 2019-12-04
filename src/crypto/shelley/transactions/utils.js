af3ce5c24d0581e36d97d9301ad2d8c5e4a2016e
v-almonacid <ealmonacid@gmail.com>
2019-12-04 10:43:38 -0300

    remove shelleyTxEqual

--- src/crypto/shelley/transactions/utils.js
+++ src/crypto/shelley/transactions/utils.js
@@ -91,57 +91,6 @@ export const getShelleyTxReceivers = async (
   return receivers
 }
 
-export const shelleyTxEqual = async (
-  req1: InputOutput,
-  req2: InputOutput,
-): Promise<boolean> => {
-  const inputs1 = await req1.inputs()
-  const inputs2 = await req2.inputs()
-  if ((await inputs1.size()) !== (await inputs2.size())) {
-    return false
-  }
-
-  const outputs1 = await req1.outputs()
-  const outputs2 = await req2.outputs()
-  if ((await outputs1.size()) !== (await outputs2.size())) {
-    return false
-  }
-
-  for (let i = 0; i < (await inputs1.size()); i++) {
-    const input1 = Buffer.from(
-      await (await inputs1.get(i)).as_bytes(),
-    ).toString('hex')
-    const input2 = Buffer.from(
-      await (await inputs2.get(i)).as_bytes(),
-    ).toString('hex')
-    if (input1 !== input2) {
-      return false
-    }
-  }
-  for (let i = 0; i < (await outputs1.size()); i++) {
-    const output1 = await outputs1.get(i)
-    const output2 = await outputs2.get(i)
-
-    if (
-      (await (await output1.value()).to_str()) !==
-      (await (await output2.value()).to_str())
-    ) {
-      return false
-    }
-    const out1Addr = Buffer.from(
-      await (await output1.address()).as_bytes(),
-    ).toString('hex')
-    const out2Addr = Buffer.from(
-      await (await output2.address()).as_bytes(),
-    ).toString('hex')
-    if (out1Addr !== out2Addr) {
-      return false
-    }
-  }
-
-  return true
-}
-
 export const generateAuthData = async (
   bindingSignature: AccountBindingSignature,
   certificate: Certificate,
@@ -159,7 +108,7 @@ export const generateAuthData = async (
     }
     default:
       throw new Error(
-        `generateAuthData unexptected cert type ${await certificate.get_type()}`,
+        `generateAuthData unexpected cert type ${await certificate.get_type()}`,
       )
   }
 }
