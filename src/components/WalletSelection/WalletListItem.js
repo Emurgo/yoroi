7a32066cb4efcd6e77f8bbb683402e3184bcaa30
v-almonacid <ealmonacid@gmail.com>
2020-02-03 17:24:07 -0300

    improve design for wallet list icon after review feedback

--- src/components/WalletSelection/WalletListItem.js
+++ src/components/WalletSelection/WalletListItem.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import {TouchableOpacity, Text, View} from 'react-native'
 import {compose, withHandlers} from 'recompose'
+import LinearGradient from 'react-native-linear-gradient'
 
 import CardanoIcon from '../../assets/CardanoIcon'
 import {COLORS} from '../../styles/config'
@@ -19,11 +20,26 @@ type ExternalProps = {
 
 const WalletListItem = ({wallet, onPress}) => (
   <TouchableOpacity activeOpacity={0.5} onPress={onPress} style={styles.item}>
-    <View style={styles.icon}>
-      <CardanoIcon height={28} width={28} color={COLORS.WHITE} shelley={wallet.isShelley} />
-    </View>
+    {
+    /* eslint-disable */
+    wallet.isShelley
+      ? <View>
+          <LinearGradient
+            start={{x: 0, y: 1}}
+            end={{x: 1, y: 0}}
+            colors={['#1A44B7', '#F14D78']}
+            style={styles.icon}
+          >
+            <CardanoIcon height={28} width={28} color={COLORS.WHITE} />
+          </LinearGradient>
+        </View>
+      : <View style={styles.icon}>
+          <CardanoIcon height={28} width={28} color={COLORS.WHITE} />
+        </View>
+    /* eslint-enable */
+    }
     <Text style={styles.nameText}>
-      {wallet.name}
+      {wallet.name}{wallet.isShelley && ' Testnet'}
     </Text>
   </TouchableOpacity>
 )
