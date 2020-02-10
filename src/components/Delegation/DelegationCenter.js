e4cf4e182f8a0135726abc7c7e4ba3041dbe9d8a
v-almonacid <ealmonacid@gmail.com>
2020-02-10 11:35:27 -0300

    add waiting dialog while building delegation tx (to avoid multi-clicks)

--- src/components/Delegation/DelegationCenter.js
+++ src/components/Delegation/DelegationCenter.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {View, WebView} from 'react-native'
 // import {compose} from 'recompose'
 import {compose} from 'redux'
-import {withHandlers} from 'recompose'
+import {withHandlers, withStateHandlers} from 'recompose'
 import {injectIntl, defineMessages} from 'react-intl'
 import type {IntlShape} from 'react-intl'
 import type {ComponentType} from 'react'
@@ -16,9 +16,10 @@ import {Logger} from '../../utils/logging'
 import walletManager from '../../crypto/wallet'
 import {getShelleyTxFee} from '../../crypto/shelley/transactions/utils'
 import {InsufficientFunds} from '../../crypto/errors'
-import {errorMessages} from '../../i18n/global-messages'
+import globalMessages, {errorMessages} from '../../i18n/global-messages'
 import {handleGeneralError, showErrorDialog} from '../../actions'
 import {NetworkError, ApiError} from '../../api/errors'
+import {PleaseWaitModal} from '../UiKit'
 
 import styles from './styles/DelegationCenter.style'
 
@@ -53,17 +54,24 @@ const prepareStakingURL = (
   return finalURL
 }
 
-const DelegationCenter = ({navigation, intl, handleOnMessage}) => {
+const DelegationCenter = ({navigation, intl, handleOnMessage, busy}) => {
   const approxAdaToDelegate = navigation.getParam('approxAdaToDelegate')
   const poolList = navigation.getParam('pools')
   return (
-    <View style={styles.container}>
-      <WebView
-        useWebKit
-        source={{uri: prepareStakingURL(approxAdaToDelegate, poolList)}}
-        onMessage={(event) => handleOnMessage(event, intl)}
+    <>
+      <View style={styles.container}>
+        <WebView
+          useWebKit
+          source={{uri: prepareStakingURL(approxAdaToDelegate, poolList)}}
+          onMessage={(event) => handleOnMessage(event, intl)}
+        />
+      </View>
+      <PleaseWaitModal
+        title={''}
+        spinnerText={intl.formatMessage(globalMessages.pleaseWait)}
+        visible={busy}
       />
-    </View>
+    </>
   )
 }
 
@@ -80,8 +88,17 @@ type ExternalProps = {|
 export default injectIntl(
   (compose(
     withNavigationTitle(({intl}) => intl.formatMessage(messages.title)),
+    withStateHandlers(
+      {
+        busy: false,
+      },
+      {
+        setBusy: () => (busy) => ({busy}),
+      }
+    ),
     withHandlers({
-      handleOnMessage: ({navigation}) => async (event, intl) => {
+      handleOnMessage: ({navigation, setBusy}) => async (event, intl) => {
+        setBusy(true)
         try {
           const pools: Array<SelectedPool> = JSON.parse(
             decodeURI(event.nativeEvent.data),
@@ -97,6 +114,7 @@ export default injectIntl(
               utxos,
             )
             const fee = await getShelleyTxFee(delegationTxData.unsignedTx.IOs)
+            setBusy(false)
             navigation.navigate(STAKING_CENTER_ROUTES.DELEGATION_CONFIRM, {
               poolName: pools[0].name,
               poolHash: pools[0].poolHash,
@@ -121,6 +139,8 @@ export default injectIntl(
               intl,
             )
           }
+        } finally {
+          setBusy(false)
         }
       },
     }),
