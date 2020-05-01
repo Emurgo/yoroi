fa540b9f5fb274a53a20a2ba992a1825c1c0a47e
v-almonacid <ealmonacid@gmail.com>
2020-05-01 15:13:47 +0200

    fix linking of react-native-ble-plx on iOS

--- ios/dummy_ble.swift
+++ ios/dummy_ble.swift
@@ -2,11 +2,7 @@
 //  dummy_ble.swift
 //  emurgo
 //
-// why do we need this file? see https://github.com/Polidea/react-native-ble-plx#ios-react-native--060-example-setup
-//
-//  NOTE: after upgrading to RN 0.60+ this file can be safely deleted
-//
-//  Created by Vicente Almonacid on 08/04/2020.
+//  Created by Vicente Almonacid on 30/04/2020.
 //  Copyright © 2020 Facebook. All rights reserved.
 //
 
