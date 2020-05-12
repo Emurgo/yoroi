ae3be18f53875f75aa46ac507c85e2091f1c471c
v-almonacid <ealmonacid@gmail.com>
2020-05-12 18:40:15 +0200

    replace react-native-qrcode by react-native-qrcode-svg

--- src/components/Receive/AddressDetail.js
+++ src/components/Receive/AddressDetail.js
@@ -1,14 +1,14 @@
 // @flow
 
 import React from 'react'
-import QRCode from 'react-native-qrcode'
+import QRCode from 'react-native-qrcode-svg'
 
 type Props = {
   address: string,
 }
 
 const AddressDetail = ({address}: Props) => (
-  <QRCode value={address} size={140} bgColor="black" fgColor="white" />
+  <QRCode value={address} size={140} backgroundColor="white" color="black" />
 )
 
 export default AddressDetail
