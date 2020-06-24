c43dbf76d9c241a7dc7440c06bd6c09d9f8f1b66
v-almonacid <ealmonacid@gmail.com>
2020-06-24 13:54:16 +0200

    change min usb sdk to 24

--- src/config.js
+++ src/config.js
@@ -104,7 +104,7 @@ const HARDWARE_WALLETS = {
     VENDOR: 'ledger.com',
     MODEL: 'Nano',
     ENABLE_USB_TRANSPORT: true,
-    USB_MIN_SDK: 26, // USB transport only supported for Android SDK >= 26
+    USB_MIN_SDK: 24, // USB transport officially supported for Android SDK >= 24
   },
 }
 
