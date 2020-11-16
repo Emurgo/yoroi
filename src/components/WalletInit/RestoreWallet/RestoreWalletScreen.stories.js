f05a78de056711f936ebbd4be01975a2b3327345
v-almonacid <ealmonacid@gmail.com>
2020-11-16 17:23:34 +0100

    add mnemonic length in restore wallet screen [ch6544]

--- src/components/WalletInit/RestoreWallet/RestoreWalletScreen.stories.js
+++ src/components/WalletInit/RestoreWallet/RestoreWalletScreen.stories.js
@@ -6,11 +6,14 @@ import {storiesOf} from '@storybook/react-native'
 import RestoreWalletScreen from './RestoreWalletScreen'
 import {CONFIG} from '../../../config/config'
 
-storiesOf('RestoreWalletScreen', module).add('Default', ({route}) => {
-  route.params = {
-    networkId: CONFIG.NETWORKS.BYRON_MAINNET.NETWORK_ID,
-    walletImplementationId:
-      CONFIG.WALLETS.HASKELL_SHELLEY.WALLET_IMPLEMENTATION_ID,
-  }
-  return <RestoreWalletScreen route={route} />
-})
+storiesOf('RestoreWalletScreen', module).add(
+  'Default',
+  ({route, navigation}) => {
+    route.params = {
+      networkId: CONFIG.NETWORKS.BYRON_MAINNET.NETWORK_ID,
+      walletImplementationId:
+        CONFIG.WALLETS.HASKELL_SHELLEY.WALLET_IMPLEMENTATION_ID,
+    }
+    return <RestoreWalletScreen route={route} navigation={navigation} />
+  },
+)
