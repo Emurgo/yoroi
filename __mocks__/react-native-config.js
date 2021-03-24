c1bc554897b3f543f3494e99f5042b8bcb7ca04c
v-almonacid <ealmonacid@gmail.com>
2021-03-24 18:15:08 +0100

    roll back to mainnet for unit tests

--- __mocks__/react-native-config.js
+++ __mocks__/react-native-config.js
@@ -1,5 +1,9 @@
 // @flow
 
+/**
+ * note: some tests require mainnet configuration (e.g. backend api tests),
+ * so setting this flag to true will result in some tests failing
+ */
 export default {
-  USE_TESTNET: 'true',
+  USE_TESTNET: 'false',
 }
