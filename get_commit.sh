000fc5987696b1515e7d21eb73b899d4190a7acb
v-almonacid <ealmonacid@gmail.com>
2021-03-25 10:16:53 +0100

    build storybook as a separate android flavor

--- get_commit.sh
+++ get_commit.sh
@@ -4,8 +4,9 @@ BASE_DIR="$(dirname ${BASH_SOURCE[0]})"
 cd $BASE_DIR
 PROD_ENV_FILE=".env.production"
 STAGING_ENV_FILE=".env.staging"
+STORYBOOK_ENV_FILE=".env.storybook"
 
-declare -a FILES=($PROD_ENV_FILE $STAGING_ENV_FILE)
+declare -a FILES=($PROD_ENV_FILE $STAGING_ENV_FILE $STORYBOOK_ENV_FILE)
 LAST_COMMIT=$(git rev-parse --short HEAD)
 if [ -z "$LAST_COMMIT" ]; then
   echo "ERROR: Couldn't get last commit hash"
