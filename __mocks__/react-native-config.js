87c98146be648b46d048e2113a0f4bb66b3cd3e0
Vicente <ealmonacid@gmail.com>
2021-04-21 15:46:48 +0200

    add unit tests for balance computation (#1251)
    * add unit tests for processTransactions
    
    * add tests for balance computation selectors
    
    * roll back to mainnet for unit tests
    
    * update after rebase

---
+++ __mocks__/react-native-config.js
@@ -0,0 +1,9 @@
+// @flow
+
+/**
+ * note: some tests require mainnet configuration (e.g. backend api tests),
+ * so setting this flag to true will result in some tests failing
+ */
+export default {
+  USE_TESTNET: 'false',
+}
