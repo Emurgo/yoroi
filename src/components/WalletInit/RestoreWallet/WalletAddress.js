038233279339fd8ff53d6e6e64cb66d48a38a9a8
Vicente <ealmonacid@gmail.com>
2021-05-20 12:30:47 +0200

    Move to react-native-community/clipboard (#1343)

--- src/components/WalletInit/RestoreWallet/WalletAddress.js
+++ src/components/WalletInit/RestoreWallet/WalletAddress.js
@@ -3,14 +3,8 @@ import React, {useState} from 'react'
 import type {ComponentType} from 'react'
 import {compose} from 'redux'
 import {withHandlers, withStateHandlers} from 'recompose'
-import {
-  View,
-  Image,
-  TouchableOpacity,
-  Clipboard,
-  Animated,
-  Linking,
-} from 'react-native'
+import {View, Image, TouchableOpacity, Animated, Linking} from 'react-native'
+import Clipboard from '@react-native-community/clipboard'
 import {injectIntl, defineMessages, intlShape} from 'react-intl'
 import {debounce} from 'lodash'
 
