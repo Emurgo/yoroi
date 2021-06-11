6b8123765f9c58840f40cde512f99b547fc522b4
Vicente <ealmonacid@gmail.com>
2021-06-11 12:51:27 +0200

    add support for Hungarian (#1401)

--- src/components/Common/LanguagePicker.js
+++ src/components/Common/LanguagePicker.js
@@ -18,6 +18,7 @@ import frenchFlagIcon from '../../assets/img/flags/french.png'
 import italianFlagIcon from '../../assets/img/flags/italian.png'
 import dutchFlagIcon from '../../assets/img/flags/dutch.png'
 import czechFlagIcon from '../../assets/img/flags/czech.png'
+import hungarianFlagIcon from '../../assets/img/flags/hungarian.png'
 import LanguageListItem from './LanguageListItem'
 import {LANGUAGES} from '../../i18n/languages'
 import {Button, StatusBar} from '../UiKit'
@@ -86,6 +87,11 @@ const messages = defineMessages({
   czech: {
     id: 'components.common.languagepicker.czech',
     defaultMessage: 'Čeština',
+    description: 'some desc',
+  },
+  hungarian: {
+    id: 'components.common.languagepicker.hungarian',
+    defaultMessage: 'Magyar',
     description: 'some desc',
   },
   continueButton: {
@@ -173,6 +179,11 @@ const supportedLanguages = (intl) => {
       label: intl.formatMessage(messages.czech),
       code: LANGUAGES.CZECH,
       icon: czechFlagIcon,
+    },
+    {
+      label: intl.formatMessage(messages.hungarian),
+      code: LANGUAGES.HUNGARIAN,
+      icon: hungarianFlagIcon,
     },
     // TODO: Add back when chinese traditional is available
     // {
