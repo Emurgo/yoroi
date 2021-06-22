bbd78274d69d17567aefd91199fbf2693f77ac11
Kevin Sullivan <wolverine.ks@gmail.com>
2021-06-21 20:02:40 -0700

    remove withNavigationTitle from TxHistory

--- src/components/TxHistory/TxHistoryNavigator.js
+++ src/components/TxHistory/TxHistoryNavigator.js
@@ -1,13 +1,14 @@
 // @flow
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
+import {useSelector} from 'react-redux'
 
 import {Button} from '../UiKit'
 import TxHistory from './TxHistory'
 import TxDetails from './TxDetails'
 import {TX_HISTORY_ROUTES, WALLET_ROOT_ROUTES} from '../../RoutesList'
 import iconGear from '../../assets/img/gear.png'
-
+import {walletMetaSelector} from '../../selectors'
 import {
   defaultNavigationOptions,
   defaultStackNavigatorOptions,
@@ -22,37 +23,41 @@ type TxHistoryRoutes = {
 
 const Stack = createStackNavigator<any, TxHistoryRoutes, any>()
 
-const TxHistoryNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{...defaultStackNavigatorOptions}}
-    initialRouteName={TX_HISTORY_ROUTES.MAIN}
-  >
-    <Stack.Screen
-      name={TX_HISTORY_ROUTES.MAIN}
-      component={TxHistory}
-      options={({navigation, route}) => ({
-        title: route.params?.title ?? undefined,
-        headerRight: () => (
-          <Button
-            style={styles.settingsButton}
-            onPress={() => navigation.navigate(WALLET_ROOT_ROUTES.SETTINGS)}
-            iconImage={iconGear}
-            title=""
-            withoutBackground
-          />
-        ),
-        ...defaultNavigationOptions,
-      })}
-    />
-    <Stack.Screen
-      name={TX_HISTORY_ROUTES.TX_DETAIL}
-      component={TxDetails}
-      options={({route}) => ({
-        title: route.params?.title ?? undefined,
-        ...defaultNavigationOptions,
-      })}
-    />
-  </Stack.Navigator>
-)
+const TxHistoryNavigator = () => {
+  const walletMeta = useSelector(walletMetaSelector)
+
+  return (
+    <Stack.Navigator
+      screenOptions={{...defaultStackNavigatorOptions}}
+      initialRouteName={TX_HISTORY_ROUTES.MAIN}
+    >
+      <Stack.Screen
+        name={TX_HISTORY_ROUTES.MAIN}
+        component={TxHistory}
+        options={({navigation}) => ({
+          title: walletMeta.name,
+          headerRight: () => (
+            <Button
+              style={styles.settingsButton}
+              onPress={() => navigation.navigate(WALLET_ROOT_ROUTES.SETTINGS)}
+              iconImage={iconGear}
+              title=""
+              withoutBackground
+            />
+          ),
+          ...defaultNavigationOptions,
+        })}
+      />
+      <Stack.Screen
+        name={TX_HISTORY_ROUTES.TX_DETAIL}
+        component={TxDetails}
+        options={({route}) => ({
+          title: route.params?.title ?? undefined,
+          ...defaultNavigationOptions,
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default TxHistoryNavigator
