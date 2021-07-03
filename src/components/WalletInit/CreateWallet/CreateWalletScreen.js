17f71c07b9d28f44d1004aadd4cf289045e1fcd1
Kevin Sullivan <wolverine.ks@gmail.com>
2021-07-03 17:15:39 -0500

    remove hocs from CreateWalletScreen

--- src/components/WalletInit/CreateWallet/CreateWalletScreen.js
+++ src/components/WalletInit/CreateWallet/CreateWalletScreen.js
@@ -1,65 +1,62 @@
 // @flow
 
 import React from 'react'
-import {compose} from 'redux'
-import {withHandlers, withStateHandlers} from 'recompose'
 
 import {WALLET_INIT_ROUTES} from '../../../RoutesList'
 import {generateAdaMnemonic} from '../../../crypto/byron/util'
 import WalletForm from '../WalletForm'
+import Screen from '../../Screen'
 
 import MnemonicExplanationModal from './MnemonicExplanationModal'
 
-const CreateWalletScreen = ({
-  setFormData,
-  hideMnemonicExplanation,
-  visibleMnemonicExplanation,
-  navigateToMnemonicScreen,
-  navigation,
-}) => (
-  <>
-    <WalletForm onSubmit={setFormData} navigation={navigation} />
-    <MnemonicExplanationModal
-      visible={visibleMnemonicExplanation}
-      onRequestClose={hideMnemonicExplanation}
-      onConfirm={navigateToMnemonicScreen}
-    />
-  </>
-)
+type Props = {|
+  navigation: any,
+  route: any,
+|}
 
-export default compose(
-  withStateHandlers(
-    {
-      visibleMnemonicExplanation: false,
-      formData: null,
-    },
-    {
-      setFormData: () => (formData: any) => ({
-        formData,
-        visibleMnemonicExplanation: true,
-      }),
-      hideMnemonicExplanation: () => () => ({
-        visibleMnemonicExplanation: false,
-      }),
-      clear: () => () => ({
-        visibleMnemonicExplanation: false,
-        formData: null,
-      }),
-    },
-  ),
-  withHandlers({
-    navigateToMnemonicScreen: ({formData, clear, navigation, route}) => () => {
-      clear()
-      // TODO(v-almonacid): we need to generate mnemonics according to the
-      // target network.
-      const mnemonic = generateAdaMnemonic()
-      const {networkId, walletImplementationId} = route.params
-      navigation.navigate(WALLET_INIT_ROUTES.MNEMONIC_SHOW, {
-        mnemonic,
-        networkId,
-        walletImplementationId,
-        ...formData,
-      })
-    },
-  }),
-)(CreateWalletScreen)
+const CreateWalletScreen = ({navigation, route}: Props) => {
+  const [
+    visibleMnemonicExplanation,
+    setVisibleMnemonicExplanation,
+  ] = React.useState(false)
+  const [formData, _setFormData] = React.useState(null)
+
+  const setFormData = (formData: any) => {
+    _setFormData(formData)
+    setVisibleMnemonicExplanation(true)
+  }
+
+  const hideMnemonicExplanation = () => setVisibleMnemonicExplanation(false)
+
+  const clear = () => {
+    setVisibleMnemonicExplanation(false)
+    _setFormData(null)
+  }
+
+  const navigateToMnemonicScreen = () => {
+    clear()
+    // TODO(v-almonacid): we need to generate mnemonics according to the
+    // target network.
+    const mnemonic = generateAdaMnemonic()
+    const {networkId, walletImplementationId} = route.params
+    navigation.navigate(WALLET_INIT_ROUTES.MNEMONIC_SHOW, {
+      mnemonic,
+      networkId,
+      walletImplementationId,
+      ...formData,
+    })
+  }
+
+  return (
+    <Screen>
+      <WalletForm onSubmit={setFormData} navigation={navigation} />
+      <MnemonicExplanationModal
+        visible={visibleMnemonicExplanation}
+        onRequestClose={hideMnemonicExplanation}
+        onConfirm={navigateToMnemonicScreen}
+      />
+    </Screen>
+  )
+}
+
+export default CreateWalletScreen
