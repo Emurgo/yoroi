b653218b33a75964bdf9a450fcaa1f4ad5fd593e
Kevin Sullivan <wolverine.ks@gmail.com>
2021-07-03 22:02:15 -0500

    remove hocs from ChangeCustomPin

--- src/components/Settings/ChangeCustomPinScreen.js
+++ src/components/Settings/ChangeCustomPinScreen.js
@@ -1,9 +1,7 @@
 // @flow
 import React from 'react'
 import {View} from 'react-native'
-import {connect} from 'react-redux'
-import {compose} from 'redux'
-import {withHandlers, withState} from 'recompose'
+import {useDispatch, useSelector} from 'react-redux'
 import {injectIntl, defineMessages, type IntlShape} from 'react-intl'
 
 import PinInput from '../Common/PinInput'
@@ -18,8 +16,6 @@ import {errorMessages} from '../../i18n/global-messages'
 import styles from './styles/ChangeCustomPinScreen.style'
 
 import type {Navigation} from '../../types/navigation'
-import type {State} from '../../state'
-import type {ComponentType} from 'react'
 
 const messages = defineMessages({
   currentPinInputTitle: {
@@ -51,95 +47,71 @@ const messages = defineMessages({
   },
 })
 
-const handleVerifyPin = ({
-  currentPinHash,
-  setIsCurrentPinVerified,
-  intl,
-}) => async (pin): Promise<boolean> => {
-  let isPinValid
-  try {
-    isPinValid = await authenticateByCustomPin(currentPinHash, pin)
-  } catch (err) {
-    setIsCurrentPinVerified(false)
-    await showErrorDialog(errorMessages.generalError, intl, {
-      message: err.message,
-    })
-    return true
+type Props = {intl: IntlShape, navigation: Navigation}
+
+const ChangeCustomPinScreen = ({intl, navigation}: Props) => {
+  const dispatch = useDispatch()
+  const currentPinHash = useSelector(customPinHashSelector)
+  const [isCurrentPinVerified, setIsCurrentPinVerified] = React.useState(false)
+  const handleVerifyPin = async (pin): Promise<boolean> => {
+    let isPinValid
+    try {
+      if (!currentPinHash) {
+        throw new Error('Missing currentPinHash')
+      }
+      isPinValid = await authenticateByCustomPin(currentPinHash, pin)
+    } catch (err) {
+      setIsCurrentPinVerified(false)
+      await showErrorDialog(errorMessages.generalError, intl, {
+        message: err.message,
+      })
+      return true
+    }
+
+    if (isPinValid) {
+      setIsCurrentPinVerified(true)
+      return false
+    } else {
+      await showErrorDialog(errorMessages.incorrectPin, intl)
+      return true
+    }
   }
 
-  if (isPinValid) {
-    setIsCurrentPinVerified(true)
-    return false
-  } else {
-    await showErrorDialog(errorMessages.incorrectPin, intl)
-    return true
+  const handleNewPinEnter = async (pin) => {
+    await dispatch(encryptAndStoreCustomPin(pin))
+    navigation.goBack()
   }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar type="dark" />
+
+      {isCurrentPinVerified ? (
+        <PinRegistrationForm
+          onPinEntered={handleNewPinEnter}
+          labels={{
+            PinInput: {
+              title: intl.formatMessage(messages.pinInputTitle),
+              subtitle: intl.formatMessage(messages.pinInputSubtitle),
+            },
+            PinConfirmationInput: {
+              title: intl.formatMessage(messages.pinConfirmationTitle),
+            },
+          }}
+          navigation={navigation}
+        />
+      ) : (
+        <PinInput
+          labels={{
+            title: intl.formatMessage(messages.currentPinInputTitle),
+            subtitle: intl.formatMessage(messages.currentPinInputSubtitle),
+          }}
+          onPinEnter={handleVerifyPin}
+          pinMaxLength={CONFIG.PIN_LENGTH}
+        />
+      )}
+    </View>
+  )
 }
 
-const handleNewPinEnter = ({navigation, encryptAndStoreCustomPin}) => async (
-  pin,
-) => {
-  await encryptAndStoreCustomPin(pin)
-  navigation.goBack()
-}
-
-const ChangeCustomPinScreen = (
-  {
-    intl,
-    isCurrentPinVerified,
-    handleNewPinEnter,
-    handleVerifyPin,
-    navigation,
-  }: {intl: IntlShape} & Object /* TODO: type */,
-) => (
-  <View style={styles.container}>
-    <StatusBar type="dark" />
-
-    {isCurrentPinVerified ? (
-      <PinRegistrationForm
-        onPinEntered={handleNewPinEnter}
-        labels={{
-          PinInput: {
-            title: intl.formatMessage(messages.pinInputTitle),
-            subtitle: intl.formatMessage(messages.pinInputSubtitle),
-          },
-          PinConfirmationInput: {
-            title: intl.formatMessage(messages.pinConfirmationTitle),
-          },
-        }}
-        navigation={navigation}
-      />
-    ) : (
-      <PinInput
-        labels={{
-          title: intl.formatMessage(messages.currentPinInputTitle),
-          subtitle: intl.formatMessage(messages.currentPinInputSubtitle),
-        }}
-        onPinEnter={handleVerifyPin}
-        pinMaxLength={CONFIG.PIN_LENGTH}
-      />
-    )}
-  </View>
-)
-
-export default injectIntl(
-  (compose(
-    connect(
-      (state: State) => ({
-        currentPinHash: customPinHashSelector(state),
-      }),
-      {
-        encryptAndStoreCustomPin,
-      },
-    ),
-    withState('isCurrentPinVerified', 'setIsCurrentPinVerified', false),
-    withHandlers({
-      handleVerifyPin,
-      handleNewPinEnter,
-    }),
-  )(ChangeCustomPinScreen): ComponentType<{|
-    navigation: Navigation,
-    route: any,
-    intl: IntlShape,
-  |}>),
-)
+export default injectIntl(ChangeCustomPinScreen)
