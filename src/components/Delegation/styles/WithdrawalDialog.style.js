83d530476a95a0b7c280a60e9547e1be40148415
Kevin Sullivan <wolverine.ks@gmail.com>
2021-07-22 12:15:20 -0500

    simplify WithdrawalDialog * remove unused prop * convert to static layout

--- src/components/Delegation/styles/WithdrawalDialog.style.js
+++ src/components/Delegation/styles/WithdrawalDialog.style.js
@@ -6,7 +6,6 @@ import {theme} from '../../../styles/config'
 
 export default StyleSheet.create({
   paragraph: {
-    marginBottom: theme.spacing.paragraphBottomMargin,
     ...theme.text,
   },
 })
