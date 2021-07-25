0be61a855ec1f70c2186ff8fbe6f2aa91ffa8e92
Kevin Sullivan <wolverine.ks@gmail.com>
2021-07-25 16:42:04 -0500

    update Checkbox * remove hocs

---
+++ src/components/UiKit/Checkbox.stories.js
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+import {storiesOf} from '@storybook/react-native'
+import {action} from '@storybook/addon-actions'
+
+import Checkbox from './Checkbox'
+
+const styles = StyleSheet.create({
+  checkbox: {
+    padding: 16,
+    justifyContent: 'center',
+    flex: 1,
+  },
+})
+
+const CheckboxWrapper = () => {
+  const [checked, setChecked] = React.useState(false)
+
+  return (
+    <Checkbox
+      text={checked ? 'this is the checked text' : 'this is the unchecked text'}
+      checked={checked}
+      onChange={(value) => {
+        action('checked')(value)
+        setChecked(value)
+      }}
+    />
+  )
+}
+
+storiesOf('Checkbox', module)
+  .addDecorator((getStory) => <View style={styles.checkbox}>{getStory()}</View>)
+  .add('default', () => <CheckboxWrapper />)
