55128e0e2bc09fffd0a262fcd2881c7a17a177b5
Kevin Sullivan <wolverine.ks@gmail.com>
2021-09-01 18:14:43 -0500

    migrate BiomtricsLink to navigation hooks

--- src/components/Settings/BiometricsLinkScreen.stories.js
+++ src/components/Settings/BiometricsLinkScreen.stories.js
@@ -6,6 +6,4 @@ import {storiesOf} from '@storybook/react-native'
 
 import BiometricsLinkScreen from './BiometricsLinkScreen'
 
-storiesOf('BiometricsLinkScreen', module).add('Default', ({navigation, route}) => (
-  <BiometricsLinkScreen navigation={navigation} route={route} />
-))
+storiesOf('BiometricsLinkScreen', module).add('Default', () => <BiometricsLinkScreen />)
