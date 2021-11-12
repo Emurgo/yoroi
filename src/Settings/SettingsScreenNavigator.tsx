53b7a5f6f4c0b57736bfbbde76a421ef0c2ebcc6
Kevin Sullivan <wolverine.ks@gmail.com>
2021-11-12 11:34:21 -0600

    formatting

--- src/Settings/SettingsScreenNavigator.tsx
+++ src/Settings/SettingsScreenNavigator.tsx
@@ -21,49 +21,7 @@ import {RemoveWalletScreen} from './RemoveWalletScreen'
 import {WalletSettingsScreen} from './WalletSettingsScreen'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// prettier-ignore
-type SettingsTabRoutes = {
-  'wallet-settings': any
-  'app-settings': any
-}
-/* eslint-enable @typescript-eslint/no-explicit-any */
-
-const Tab = createMaterialTopTabNavigator<SettingsTabRoutes>()
-const SettingsTabNavigator = () => {
-  const strings = useStrings()
-
-  return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarLabel: route.name === SETTINGS_TABS.WALLET_SETTINGS ? strings.walletTabTitle : strings.appTabTitle,
-      })}
-      tabBarOptions={{
-        style: {
-          backgroundColor: COLORS.BACKGROUND_BLUE,
-          elevation: 0,
-          shadowOpacity: 0,
-        },
-        tabStyle: {
-          elevation: 0,
-          shadowOpacity: 0,
-        },
-        labelStyle: {
-          color: COLORS.WHITE,
-        },
-        indicatorStyle: {
-          backgroundColor: '#fff',
-          height: 2,
-        },
-      }}
-    >
-      <Tab.Screen name={SETTINGS_TABS.WALLET_SETTINGS} component={WalletSettingsScreen} />
-      <Tab.Screen name={SETTINGS_TABS.APP_SETTINGS} component={ApplicationSettingsScreen} />
-    </Tab.Navigator>
-  )
-}
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-type SettingsStackNavigatorRoutes = {
+const Stack = createStackNavigator<{
   settings: any
   'change-wallet-name': any
   'terms-of-use': any
@@ -76,10 +34,8 @@ type SettingsStackNavigatorRoutes = {
   'change-custom-pin': any
   'bio-authenticate': any
   'setup-custom-pin': any
-}
+}>()
 /* eslint-enable @typescript-eslint/no-explicit-any */
-
-const Stack = createStackNavigator<SettingsStackNavigatorRoutes>()
 export const SettingsScreenNavigator = () => {
   const strings = useStrings()
 
@@ -158,6 +114,33 @@ export const SettingsScreenNavigator = () => {
         options={{title: strings.customPinTitle}}
       />
     </Stack.Navigator>
+  )
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const Tab = createMaterialTopTabNavigator<{
+  'wallet-settings': any
+  'app-settings': any
+}>()
+/* eslint-enable @typescript-eslint/no-explicit-any */
+const SettingsTabNavigator = () => {
+  const strings = useStrings()
+
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarLabel: route.name === SETTINGS_TABS.WALLET_SETTINGS ? strings.walletTabTitle : strings.appTabTitle,
+      })}
+      tabBarOptions={{
+        style: {backgroundColor: COLORS.BACKGROUND_BLUE, elevation: 0, shadowOpacity: 0},
+        tabStyle: {elevation: 0, shadowOpacity: 0},
+        labelStyle: {color: COLORS.WHITE},
+        indicatorStyle: {backgroundColor: '#fff', height: 2},
+      }}
+    >
+      <Tab.Screen name={SETTINGS_TABS.WALLET_SETTINGS} component={WalletSettingsScreen} />
+      <Tab.Screen name={SETTINGS_TABS.APP_SETTINGS} component={ApplicationSettingsScreen} />
+    </Tab.Navigator>
   )
 }
 
