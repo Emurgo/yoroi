11252260a4261b4312612da5a824084c87c83d66
Kevin Sullivan <wolverine.ks@gmail.com>
2021-11-12 20:05:54 -0600

    colocate styles

--- legacy/components/Delegation/StakingCenter.js
+++ legacy/components/Delegation/StakingCenter.js
@@ -6,7 +6,7 @@ import React, {useEffect, useState} from 'react'
 import type {IntlShape} from 'react-intl'
 import {useIntl} from 'react-intl'
 import {defineMessages} from 'react-intl'
-import {View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import {WebView} from 'react-native-webview'
 import {useSelector} from 'react-redux'
 
@@ -37,141 +37,6 @@ import {PleaseWaitModal} from '../UiKit'
 import AccountAutoRefresher from './AccountAutoRefresher'
 import PoolDetailScreen from './PoolDetailScreen'
 import PoolWarningModal from './PoolWarningModal'
-import styles from './styles/DelegationCenter.style'
-
-const IS_STAKING_ON_TEST_BUILD = isNightly() || CONFIG.IS_TESTNET_BUILD
-
-type SelectedPool = {|
-  +poolName: ?string,
-  +poolHash: string,
-|}
-
-/**
- * Prepares WebView's target staking URI
- * @param {*} poolList : Array of delegated pool hash
- */
-const prepareStakingURL = (poolList: Array<string> | null, amountToDelegate: string | null, locale: string): string => {
-  // source=mobile is constant and already included
-  let finalURL = CONFIG.NETWORKS.HASKELL_SHELLEY.POOL_EXPLORER
-
-  const lang = locale.slice(0, 2)
-  finalURL += `&lang=${lang}`
-
-  if (poolList != null) {
-    finalURL += `&delegated=${encodeURIComponent(JSON.stringify(poolList))}`
-  }
-  if (amountToDelegate != null) {
-    finalURL += `&totalAda=${amountToDelegate}`
-  }
-  return finalURL
-}
-
-const navigateToDelegationConfirm = async (
-  accountBalance: ?BigNumber,
-  utxos: Array<RawUtxo>,
-  selectedPools: Array<SelectedPool>,
-  defaultAsset: DefaultAsset,
-  intl: IntlShape,
-  navigation,
-  serverStatus: ServerStatusCache,
-) => {
-  try {
-    const selectedPool = selectedPools[0]
-    if (accountBalance == null) return
-    const transactionData = await walletManager.createDelegationTx(
-      selectedPool.poolHash,
-      accountBalance,
-      utxos,
-      defaultAsset,
-      serverStatus.serverTime,
-    )
-    const transactionFee = await transactionData.signRequest.fee()
-    navigation.navigate(STAKING_CENTER_ROUTES.DELEGATION_CONFIRM, {
-      poolName: selectedPool.poolName,
-      poolHash: selectedPool.poolHash,
-      transactionData,
-      transactionFee,
-    })
-  } catch (e) {
-    if (e instanceof InsufficientFunds) {
-      await showErrorDialog(errorMessages.insufficientBalance, intl)
-    } else {
-      Logger.error(e)
-      await showErrorDialog(errorMessages.generalError, intl, {
-        message: e.message,
-      })
-    }
-  }
-}
-
-const _handleOnMessage = async (
-  selectedPoolHashes: Array<string>,
-  setSelectedPools: (selectedPools: Array<SelectedPool>) => void,
-  setReputationInfo: (reputationInfo: Object) => void,
-  setShowPoolWarning: (showPoolWarning: boolean) => void,
-  accountBalance: ?BigNumber,
-  utxos: Array<RawUtxo>,
-  defaultAsset,
-  intl: IntlShape,
-  navigation,
-  serverStatus: ServerStatusCache,
-) => {
-  try {
-    const poolInfoResponse = await walletManager.fetchPoolInfo({
-      poolIds: selectedPoolHashes,
-    })
-    const poolInfo = ObjectValues(poolInfoResponse)[0]
-    Logger.debug('StakingCenter::poolInfo', poolInfo)
-
-    // TODO: fetch reputation info once an endpoint is implemented
-    const poolsReputation: {[key: string]: mixed} = {}
-
-    if (poolInfo.info != null) {
-      const selectedPools: Array<SelectedPool> = [
-        {
-          poolName: poolInfo.info.name,
-          poolHash: selectedPoolHashes[0],
-        },
-      ]
-      setSelectedPools(selectedPools)
-
-      // check if pool in blacklist
-      const poolsInBlackList = []
-      for (const pool of selectedPoolHashes) {
-        if (pool in poolsReputation) {
-          poolsInBlackList.push(pool)
-        }
-      }
-      if (poolsInBlackList.length > 0) {
-        setReputationInfo(poolsReputation[poolsInBlackList[0]])
-        setShowPoolWarning(true)
-      } else {
-        await navigateToDelegationConfirm(
-          accountBalance,
-          utxos,
-          selectedPools,
-          defaultAsset,
-          intl,
-          navigation,
-          serverStatus,
-        )
-      }
-    } else {
-      await showErrorDialog(noPoolDataDialog, intl)
-    }
-  } catch (e) {
-    if (e instanceof NetworkError) {
-      await showErrorDialog(errorMessages.networkError, intl)
-    } else if (e instanceof ApiError) {
-      await showErrorDialog(noPoolDataDialog, intl)
-    } else {
-      Logger.error(e)
-      await showErrorDialog(errorMessages.generalError, intl, {
-        message: e.message,
-      })
-    }
-  }
-}
 
 const StakingCenter = () => {
   const intl = useIntl()
@@ -309,3 +174,143 @@ const useStrings = () => {
     pleaseWait: intl.formatMessage(globalMessages.pleaseWait),
   }
 }
+
+const IS_STAKING_ON_TEST_BUILD = isNightly() || CONFIG.IS_TESTNET_BUILD
+
+type SelectedPool = {|
+  +poolName: ?string,
+  +poolHash: string,
+|}
+
+/**
+ * Prepares WebView's target staking URI
+ * @param {*} poolList : Array of delegated pool hash
+ */
+const prepareStakingURL = (poolList: Array<string> | null, amountToDelegate: string | null, locale: string): string => {
+  // source=mobile is constant and already included
+  let finalURL = CONFIG.NETWORKS.HASKELL_SHELLEY.POOL_EXPLORER
+
+  const lang = locale.slice(0, 2)
+  finalURL += `&lang=${lang}`
+
+  if (poolList != null) {
+    finalURL += `&delegated=${encodeURIComponent(JSON.stringify(poolList))}`
+  }
+  if (amountToDelegate != null) {
+    finalURL += `&totalAda=${amountToDelegate}`
+  }
+  return finalURL
+}
+
+const navigateToDelegationConfirm = async (
+  accountBalance: ?BigNumber,
+  utxos: Array<RawUtxo>,
+  selectedPools: Array<SelectedPool>,
+  defaultAsset: DefaultAsset,
+  intl: IntlShape,
+  navigation,
+  serverStatus: ServerStatusCache,
+) => {
+  try {
+    const selectedPool = selectedPools[0]
+    if (accountBalance == null) return
+    const transactionData = await walletManager.createDelegationTx(
+      selectedPool.poolHash,
+      accountBalance,
+      utxos,
+      defaultAsset,
+      serverStatus.serverTime,
+    )
+    const transactionFee = await transactionData.signRequest.fee()
+    navigation.navigate(STAKING_CENTER_ROUTES.DELEGATION_CONFIRM, {
+      poolName: selectedPool.poolName,
+      poolHash: selectedPool.poolHash,
+      transactionData,
+      transactionFee,
+    })
+  } catch (e) {
+    if (e instanceof InsufficientFunds) {
+      await showErrorDialog(errorMessages.insufficientBalance, intl)
+    } else {
+      Logger.error(e)
+      await showErrorDialog(errorMessages.generalError, intl, {
+        message: e.message,
+      })
+    }
+  }
+}
+
+const _handleOnMessage = async (
+  selectedPoolHashes: Array<string>,
+  setSelectedPools: (selectedPools: Array<SelectedPool>) => void,
+  setReputationInfo: (reputationInfo: Object) => void,
+  setShowPoolWarning: (showPoolWarning: boolean) => void,
+  accountBalance: ?BigNumber,
+  utxos: Array<RawUtxo>,
+  defaultAsset,
+  intl: IntlShape,
+  navigation,
+  serverStatus: ServerStatusCache,
+) => {
+  try {
+    const poolInfoResponse = await walletManager.fetchPoolInfo({
+      poolIds: selectedPoolHashes,
+    })
+    const poolInfo = ObjectValues(poolInfoResponse)[0]
+    Logger.debug('StakingCenter::poolInfo', poolInfo)
+
+    // TODO: fetch reputation info once an endpoint is implemented
+    const poolsReputation: {[key: string]: mixed} = {}
+
+    if (poolInfo.info != null) {
+      const selectedPools: Array<SelectedPool> = [
+        {
+          poolName: poolInfo.info.name,
+          poolHash: selectedPoolHashes[0],
+        },
+      ]
+      setSelectedPools(selectedPools)
+
+      // check if pool in blacklist
+      const poolsInBlackList = []
+      for (const pool of selectedPoolHashes) {
+        if (pool in poolsReputation) {
+          poolsInBlackList.push(pool)
+        }
+      }
+      if (poolsInBlackList.length > 0) {
+        setReputationInfo(poolsReputation[poolsInBlackList[0]])
+        setShowPoolWarning(true)
+      } else {
+        await navigateToDelegationConfirm(
+          accountBalance,
+          utxos,
+          selectedPools,
+          defaultAsset,
+          intl,
+          navigation,
+          serverStatus,
+        )
+      }
+    } else {
+      await showErrorDialog(noPoolDataDialog, intl)
+    }
+  } catch (e) {
+    if (e instanceof NetworkError) {
+      await showErrorDialog(errorMessages.networkError, intl)
+    } else if (e instanceof ApiError) {
+      await showErrorDialog(noPoolDataDialog, intl)
+    } else {
+      Logger.error(e)
+      await showErrorDialog(errorMessages.generalError, intl, {
+        message: e.message,
+      })
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
