8f00aae0fcd5887ef4e424818085e2ee541ca117
Kevin Sullivan <wolverine.ks@gmail.com>
2021-11-23 05:48:18 -0600

    migrate to selected wallet context

--- src/Settings/ChangePassword/ChangePasswordScreen.stories.tsx
+++ src/Settings/ChangePassword/ChangePasswordScreen.stories.tsx
@@ -1,6 +1,17 @@
+import {action} from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react-native'
 import React from 'react'
 
+import {SelectedWalletProvider} from '../../SelectedWallet'
+import {WalletInterface} from '../../types'
 import {ChangePasswordScreen} from './ChangePasswordScreen'
 
-storiesOf('ChangePasswordScreen', module).add('Default', () => <ChangePasswordScreen />)
+const wallet = {
+  changePassword: action('changePassword'),
+} as WalletInterface
+
+storiesOf('ChangePasswordScreen', module).add('Default', () => (
+  <SelectedWalletProvider wallet={wallet}>
+    <ChangePasswordScreen />
+  </SelectedWalletProvider>
+))
