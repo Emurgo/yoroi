d20bb5163c0f56b433c10b32e17c54be843a5a5d
Kevin Sullivan <wolverine.ks@gmail.com>
2021-11-23 05:47:00 -0600

    fix voting banner stories

--- legacy/components/Catalyst/VotingBanner.stories.js
+++ legacy/components/Catalyst/VotingBanner.stories.js
@@ -4,8 +4,22 @@ import {action} from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react-native'
 import React from 'react'
 
+// $FlowExpectedError
+import {SelectedWalletProvider} from '../../../src/SelectedWallet'
 import VotingBanner from './VotingBanner'
 
+const wallet = {
+  walletImplementationId: 'haskell-shelley',
+}
+
 storiesOf('Voting Banner', module)
-  .add('default', () => <VotingBanner onPress={action('onPress')} disabled={false} />)
-  .add('disabled', () => <VotingBanner onPress={action('onPress')} disabled />)
+  .add('default', () => (
+    <SelectedWalletProvider wallet={wallet}>
+      <VotingBanner onPress={action('onPress')} disabled={false} />
+    </SelectedWalletProvider>
+  ))
+  .add('disabled', () => (
+    <SelectedWalletProvider wallet={wallet}>
+      <VotingBanner onPress={action('onPress')} disabled />
+    </SelectedWalletProvider>
+  ))
