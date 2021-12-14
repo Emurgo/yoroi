b9d3b6d497b32a82a60b7816c1648ce681840b94
Denis Nebytov <denis@emurgo.io>
2021-12-14 22:42:05 +0300

    Added error elements

--- tests/screenObjects/createWalletScreens/createWalletCredentials.screen.ts
+++ tests/screenObjects/createWalletScreens/createWalletCredentials.screen.ts
@@ -1,6 +1,10 @@
+import {WALLET_ALREADY_EXISTS, MUST_BE_FILLED} from "../../helpers/errors"
+
 const getAllEditFields = () => driver.$$('//android.widget.EditText')
 
 export const walletNameEdit = () => getAllEditFields()[0]
+export const walletNameExistsError = () => driver.$(`[text="${WALLET_ALREADY_EXISTS}"]`);
+export const walletNameIsEmptyError = () => driver.$(`text="${MUST_BE_FILLED}"`);
 export const spendingPasswordEdit = () => getAllEditFields()[1]
 export const repeatSpendingPasswordEdit = () => getAllEditFields()[2]
 export const continueButton = () => driver.$('//*[@resource-id="walletFormContinueButton"]')
