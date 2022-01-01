34471d67dced6fa5053c55aacd46ce7b12bcb95a
stackchain <30806844+stackchain@users.noreply.github.com>
2022-01-01 12:53:04 -0500

    [chore] Revamp ReceiveScreen

--- src/Receive/ReceiveScreen.tsx
+++ src/Receive/ReceiveScreen.tsx
@@ -1,20 +1,23 @@
 import _ from 'lodash'
 import React from 'react'
 import {defineMessages, useIntl} from 'react-intl'
-import {ActivityIndicator, ScrollView, StyleSheet, View} from 'react-native'
+import {ActivityIndicator, ScrollView, StyleSheet, Text, View} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {useDispatch, useSelector} from 'react-redux'
 
 import {generateNewReceiveAddress, generateNewReceiveAddressIfNeeded} from '../../legacy/actions'
-import {Banner, Button, OfflineBanner, Spacer, StatusBar} from '../../legacy/components/UiKit'
+import {Banner, Button, Modal, OfflineBanner, Spacer, StatusBar} from '../../legacy/components/UiKit'
+import {UI_V2} from '../../legacy/config/config'
 import {
   canGenerateNewReceiveAddressSelector,
   isUsedAddressIndexSelector,
   receiveAddressesSelector,
 } from '../../legacy/selectors'
 import {COLORS} from '../../legacy/styles/config'
+import {Icon} from '../components'
 import {AddressDetail} from './AddressDetail'
 import {UnusedAddresses, UsedAddresses} from './Addresses'
+import {useReceiveContextInfoModal} from './Context'
 
 export const ReceiveScreen = () => {
   const strings = useStrings()
@@ -37,11 +40,12 @@ export const ReceiveScreen = () => {
 
   return (
     <SafeAreaView edges={['left', 'right']} style={styles.safeAreaView}>
-      <StatusBar type="dark" />
+      <StatusBar type={UI_V2 ? 'light' : 'dark'} />
       <OfflineBanner />
 
       <ScrollView>
-        <Banner text={strings.infoText} />
+        {!UI_V2 ? <Banner text={strings.infoText} /> : <InfoModal />}
+        {UI_V2 && <Spacer height={24} />}
 
         <Content>
           <View style={styles.address}>
@@ -72,9 +76,29 @@ export const ReceiveScreen = () => {
   )
 }
 
+const InfoModal = () => {
+  const strings = useStrings()
+  const {isInfoModalOpen, hideInfoModal} = useReceiveContextInfoModal()
+
+  return (
+    <Modal visible={isInfoModalOpen} showCloseIcon onRequestClose={hideInfoModal} title={strings.infoTitle}>
+      <Spacer height={16} />
+
+      <Icon.Info size={45} color={COLORS.ACTION_GRAY} style={styles.infoIcon} />
+
+      <Spacer height={32} />
+      <Text style={styles.infoText}>{strings.infoText}</Text>
+    </Modal>
+  )
+}
+
 const Content = (props) => <View {...props} style={styles.content} />
 
 const messages = defineMessages({
+  infoTitle: {
+    id: 'global.info',
+    defaultMessage: '!!!Info',
+  },
   infoText: {
     id: 'components.receive.receivescreen.infoText',
     defaultMessage:
@@ -96,6 +120,7 @@ const useStrings = () => {
   const intl = useIntl()
 
   return {
+    infoTitle: intl.formatMessage(messages.infoTitle),
     infoText: intl.formatMessage(messages.infoText),
     generateButton: intl.formatMessage(messages.generateButton),
     cannotGenerate: intl.formatMessage(messages.cannotGenerate),
@@ -114,5 +139,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     minHeight: 180,
+  },
+  infoText: {
+    lineHeight: 24,
+    fontSize: 16,
+  },
+  infoIcon: {
+    alignSelf: 'center',
+    padding: 2,
   },
 })
