cc275705fac44b3d8b69eab6a718cbe98f2d79ee
Kevin Sullivan <wolverine.ks@gmail.com>
2022-04-11 12:28:39 -0500

    fix import

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import 'react-native-gesture-handler' // required by react-navigation
 import './global'
-import './legacy/i18n/polyfills' // https://formatjs.io/docs/polyfills
+import './src/i18n/polyfills' // https://formatjs.io/docs/polyfills
 import './src/index'
