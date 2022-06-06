9c84288b5650b4f5cce422e1e651c602dbef9caf
Kevin Sullivan <wolverine.ks@gmail.com>
2022-06-06 00:42:19 -0500

    fix polyfill

--- polyfills.ts
+++ polyfills.ts
@@ -1,12 +1,3 @@
-if (!Object.entries) {
-  Object.entries = function (obj) {
-    var ownProps = Object.keys(obj),
-      i = ownProps.length,
-      resArray = new Array(i) // preallocate the Array
-    while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]]
-
-    return resArray
-  }
-}
+Object.fromEntries = Object.fromEntries || ((arr) => arr.reduce((acc, [k, v]) => ((acc[k] = v), acc), {}))
 
 export {}
