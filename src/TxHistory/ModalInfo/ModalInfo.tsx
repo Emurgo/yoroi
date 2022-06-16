429f0d27787a6e27467c755f487e035623a50979
Javier Bueno <javierbueno@emurgo.io>
2022-06-16 18:36:59 +0200

    fix after juliano review

--- src/TxHistory/ModalInfo/ModalInfo.tsx
+++ src/TxHistory/ModalInfo/ModalInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {defineMessages, useIntl} from 'react-intl'
+import {StyleSheet} from 'react-native'
 
 import {Icon, Modal, Spacer} from '../../components'
 import {COLORS} from '../../theme'
@@ -16,7 +17,7 @@ export const ModalInfo: React.FC<Props> = ({visible, hideModalInfo, children}) =
     <Modal visible={visible} showCloseIcon onRequestClose={hideModalInfo} title={strings.infoTitle}>
       <Spacer height={16} />
 
-      <Icon.Info size={45} color={COLORS.ACTION_GRAY} />
+      <Icon.Info style={styles.infoIcon} size={45} color={COLORS.ACTION_GRAY} />
 
       <Spacer height={32} />
       {children}
@@ -28,6 +29,13 @@ const messages = defineMessages({
   infoTitle: {
     id: 'global.info',
     defaultMessage: '!!!Info',
+  },
+})
+
+const styles = StyleSheet.create({
+  infoIcon: {
+    alignSelf: 'center',
+    padding: 2,
   },
 })
 
