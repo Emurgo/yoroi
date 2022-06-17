91a05573bc990aa4052ac99cc66f1ceff4463d79
stackchain <30806844+stackchain@users.noreply.github.com>
2022-06-17 13:54:58 -0400

    fix: Send token stuck

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -148,16 +148,16 @@ export const fetchTokenInfo = async ({wallet, tokenId}: {wallet: WalletInterface
   }
 
   return {
-    networkId: 300,
+    networkId: wallet.networkId,
     identifier: tokenId,
     isDefault: false,
     metadata: {
       type: 'Cardano',
       policyId: tokenId.split('.')[0],
       assetName: tokenId.split('.')[1],
-      ticker: null,
-      longName: null,
-      numberOfDecimals: tokenMetadata.decimals || 0,
+      ticker: tokenMetadata.ticker ?? null,
+      longName: tokenMetadata.longName ?? null,
+      numberOfDecimals: tokenMetadata.decimals ?? 0,
       maxSupply: null,
     },
   }
