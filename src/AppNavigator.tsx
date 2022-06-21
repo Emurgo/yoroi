f797576654a85671fc6c2038df5e56027dadcbdf
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2022-06-21 12:59:11 -0400

    Fix pin nav (#2026)

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -14,10 +14,10 @@ import {BiometricAuthScreen} from './BiometricAuth'
 import {Boundary} from './components'
 import {FirstRunNavigator} from './FirstRun/FirstRunNavigator'
 import {errorMessages} from './i18n/global-messages'
-import {checkBiometricStatus, showErrorDialog, signin} from './legacy/actions'
+import {checkBiometricStatus, reloadAppSettings, setSystemAuth, showErrorDialog, signin} from './legacy/actions'
+import {DeveloperScreen} from './legacy/DeveloperScreen'
 import {canBiometricEncryptionBeEnabled, recreateAppSignInKeys} from './legacy/deviceSettings'
 import env from './legacy/env'
-import IndexScreen from './legacy/IndexScreen'
 import KeyStore from './legacy/KeyStore'
 import {
   canEnableBiometricSelector,
@@ -119,7 +119,7 @@ const NavigatorSwitch = () => {
           {isSystemAuthEnabled && !canEnableBiometrics && (
             <Stack.Screen //
               name="setup-custom-pin"
-              component={CreatePinScreen}
+              component={CreatePinScreenWrapper}
               options={{title: strings.customPinTitle}}
             />
           )}
@@ -138,11 +138,25 @@ const NavigatorSwitch = () => {
       {/* Development */}
       {__DEV__ && (
         <Stack.Group>
-          <Stack.Screen name="screens-index" component={IndexScreen} options={{headerShown: false}} />
+          <Stack.Screen name="developer" component={DeveloperScreen} options={{headerShown: false}} />
           <Stack.Screen name="storybook" component={StorybookScreen} />
         </Stack.Group>
       )}
     </Stack.Navigator>
+  )
+}
+
+const CreatePinScreenWrapper = () => {
+  const dispatch = useDispatch()
+
+  return (
+    <CreatePinScreen
+      onDone={async () => {
+        await dispatch(reloadAppSettings())
+        await dispatch(setSystemAuth(false))
+        dispatch(signin())
+      }}
+    />
   )
 }
 
