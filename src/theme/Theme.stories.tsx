660e6ce7b40833f9c883dfec293bb94c400929db
Kevin Sullivan <wolverine.ks@gmail.com>
2022-06-22 10:25:57 -0500

    Update Theme.stories.tsx

--- src/theme/Theme.stories.tsx
+++ src/theme/Theme.stories.tsx
@@ -1,7 +1,8 @@
 import {storiesOf} from '@storybook/react-native'
 import * as React from 'react'
-import {ScrollView, Switch, Text} from 'react-native'
+import {ScrollView, Switch, Text, View} from 'react-native'
 
+import {Spacer} from '../components'
 import {useTheme} from './ThemeProvider'
 
 storiesOf('ThemeProvider', module).add('ThemeProvider', () => <ThemeGallery />)
@@ -10,13 +11,25 @@ const ThemeGallery = () => {
   const {theme, colorScheme, selectColorScheme} = useTheme()
 
   return (
-    <ScrollView>
-      <Switch
-        value={colorScheme === 'light'}
-        onChange={() => selectColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
-      />
-      <Text>colorScheme: {colorScheme}</Text>
-      <Text>theme: {JSON.stringify(theme, null, 2)}</Text>
-    </ScrollView>
+    <>
+      <View style={{justifyContent: 'space-between', flexDirection: 'row', paddingVertical: 8, paddingHorizontal: 16}}>
+        <Text>colorScheme: {colorScheme}</Text>
+
+        <Switch
+          value={colorScheme === 'light'}
+          onChange={() => selectColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
+        />
+      </View>
+
+      <ScrollView contentContainerStyle={{padding: 16}}>
+        <Text>Palette: {JSON.stringify(theme.color, null, 2)}</Text>
+
+        <Spacer height={16} />
+        <View style={{height: 1, backgroundColor: 'black'}} />
+        <Spacer height={16} />
+
+        <Text>Typography: {JSON.stringify(theme.typography, null, 2)}</Text>
+      </ScrollView>
+    </>
   )
 }
