73fb21891f53f7423914f45008df71b041aef5c1
jemubm <105349292+jemubm@users.noreply.github.com>
2022-07-06 20:28:54 +0200

    feat: revamping terms of service (#2054)

--- src/Legal/TermsOfService/TermsOfService.tsx
+++ src/Legal/TermsOfService/TermsOfService.tsx
@@ -2,10 +2,35 @@ import React from 'react'
 import {ActivityIndicator} from 'react-native'
 import Markdown from 'react-native-easy-markdown'
 
+import {lightPalette} from '../../theme'
 import {useTos} from '..'
 
 export const TermsOfService = ({languageCode}: {languageCode: string}) => {
   const tos = useTos({languageCode})
 
-  return tos ? <Markdown>{tos}</Markdown> : <ActivityIndicator size="large" color="black" />
+  return tos ? (
+    <Markdown
+      markdownStyles={{
+        text: {
+          fontFamily: 'Rubik-Regular',
+          fontSize: 16,
+          lineHeight: 24,
+          color: lightPalette.gray['900'],
+          paddingVertical: 8,
+        },
+        h2: {fontFamily: 'Rubik-Medium', lineHeight: 24, color: lightPalette.gray['900'], paddingVertical: 8},
+        h1: {
+          fontFamily: 'Rubik-Medium',
+          fontSize: 32,
+          lineHeight: 45,
+          color: lightPalette.gray['900'],
+          paddingVertical: 10,
+        },
+      }}
+    >
+      {tos}
+    </Markdown>
+  ) : (
+    <ActivityIndicator size="large" color="black" />
+  )
 }
