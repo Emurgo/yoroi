88219208baaa441e3e14453009b0b9b86ba629cd
jemubm <105349292+jemubm@users.noreply.github.com>
2022-07-13 04:12:21 +0200

    Privacy mode (#2065)

---
+++ src/components/HideableText/HideableText.stories.tsx
@@ -0,0 +1,37 @@
+import {storiesOf} from '@storybook/react-native'
+import React from 'react'
+import {StyleSheet} from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context'
+
+import {usePrivacyMode, useSetPrivacyMode} from '../../Settings/PrivacyMode/PrivacyMode'
+import {Button, Spacer, Text} from '..'
+import {HideableText} from './HideableText'
+
+storiesOf('HideableText', module).add('Default', () => <HideableTextTest />)
+
+const HideableTextTest = () => {
+  const privacyMode = usePrivacyMode()
+  const setPrivacyMode = useSetPrivacyMode()
+
+  return (
+    <SafeAreaView edges={['bottom', 'right', 'left']} style={styles.mainColumn}>
+      <Text style={styles.text}>Privacy Mode: {privacyMode}</Text>
+      <Spacer height={20} />
+      <HideableText text="Lorem ipsum dolor sit amet" />
+      <Spacer height={20} />
+      <Button
+        onPress={() => setPrivacyMode(privacyMode === 'SHOWN' ? 'HIDDEN' : 'SHOWN')}
+        title="Change Privacy Mode"
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainColumn: {
+    padding: 16,
+  },
+  text: {
+    lineHeight: 22,
+  },
+})
