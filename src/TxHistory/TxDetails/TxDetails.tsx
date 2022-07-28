2a5d47a3c99d11b89544a00cd3cf1dc4c28730da
stackchain <30806844+stackchain@users.noreply.github.com>
2022-07-27 22:09:18 -0400

    fix: Wrong string checks

--- src/TxHistory/TxDetails/TxDetails.tsx
+++ src/TxHistory/TxDetails/TxDetails.tsx
@@ -127,7 +127,7 @@ export const TxDetails = () => {
         </View>
       </Screen>
 
-      {addressDetail && (
+      {!!addressDetail && (
         <AddressModal
           visible
           onRequestClose={() => setAddressDetail(null)}
