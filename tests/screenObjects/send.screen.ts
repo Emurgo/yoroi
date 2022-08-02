a38d8e84f8183e16fe5ab4027ca8177b0f0990ed
Denis <nebytov.d@gmail.com>
2022-08-02 18:23:34 +0300

    More screens

--- tests/screenObjects/send.screen.ts
+++ tests/screenObjects/send.screen.ts
@@ -2,3 +2,4 @@ export const receiverAddressInput = () => driver.$('//*[@resource-id="addressInp
 export const amountInput = () => driver.$('//*[@resource-id="amountFieldInput"]').$('android.widget.EditText')
 export const sendAllCheckbox = () => driver.$('//*[@resource-id="sendAllCheckbox"]')
 export const continueButton = () => driver.$('//*[@resource-id="continueButton"]')
+export const confirmTxButton = () => driver.$('//*[@resource-id="confirmTxButton"]')
--- tests/screenObjects/walletHistory.screen.ts
+++ tests/screenObjects/walletHistory.screen.ts
@@ -1,2 +1,6 @@
 export const sendButton = () => driver.$('//*[@resource-id="sendButton"]')
 export const receiveButton = () => driver.$('//*[@resource-id="receiveButton"]')
+
+export const isDisplayed = async () => {
+  return (await sendButton().isDisplayed) && (await receiveButton().isDisplayed)
+}
