e2afd556c81268d4530274ffcce2596e6ce62372
Javier Bueno <javierbueno@emurgo.io>
2022-08-11 17:22:09 +0200

    Removed unnecesary flatlist id

--- src/Send/AssetSelectorScreen/AssetSelectorScreen.tsx
+++ src/Send/AssetSelectorScreen/AssetSelectorScreen.tsx
@@ -67,7 +67,6 @@ export const AssetSelectorScreen = ({balances, onSelect, onSelectAll}: Props) =>
           <Boundary>
             <AssetSelectorItem
               wallet={wallet}
-              key={tokenId}
               tokenId={tokenId}
               quantity={quantity}
               onPress={onSelect}
