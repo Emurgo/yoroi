2d285892d3475be6aa531e0d9663e2a1a731dc1e
Javier Bueno <javierbueno@emurgo.io>
2022-08-11 19:23:36 +0200

    Entry component refactor

--- src/Send/ConfirmScreen/ConfirmScreen.tsx
+++ src/Send/ConfirmScreen/ConfirmScreen.tsx
@@ -103,7 +103,7 @@ export const ConfirmScreen = () => {
 
           {Object.entries(selectedTokens).map(([tokenId, quantity]) => (
             <Boundary key={tokenId}>
-              <Entry tokenEntry={[tokenId, quantity]} />
+              <Entry tokenId={tokenId} quantity={quantity} />
             </Boundary>
           ))}
 
@@ -137,7 +137,7 @@ export const ConfirmScreen = () => {
   )
 }
 
-const Entry = ({tokenEntry: [tokenId, quantity]}: {tokenEntry: [TokenId, Quantity]}) => {
+const Entry = ({tokenId, quantity}: {tokenId: TokenId; quantity: Quantity}) => {
   const wallet = useSelectedWallet()
   const tokenInfo = useTokenInfo({wallet, tokenId})
 
