c8eed3e84a94679522ab1eb14ecf8df0fa60cf3c
Javier Bueno <javierbueno@emurgo.io>
2022-08-26 21:07:49 +0200

    wallet close refactor

---
+++ src/legacy/useCloseWallet.tsx
@@ -0,0 +1,38 @@
+import {UseMutationOptions} from 'react-query'
+import {useDispatch} from 'react-redux'
+
+import {useCloseWallet, useCloseWalletWithWalletMeta} from '../hooks'
+import {clearAccountState} from './account'
+import {WalletMeta} from './state'
+import {clearUTXOs} from './utxo'
+
+const useCloseWalletWrapper = (options?: UseMutationOptions<void, Error>) => {
+  const dispatch = useDispatch()
+
+  const {closeWallet, ...mutation} = useCloseWallet({
+    onSuccess: () => {
+      dispatch(clearUTXOs())
+      dispatch(clearAccountState())
+    },
+    ...options,
+  })
+
+  return {closeWallet, ...mutation}
+}
+
+const useCloseWalletWithWalletMetaWrapper = (options?: UseMutationOptions<WalletMeta, Error, WalletMeta>) => {
+  const dispatch = useDispatch()
+
+  const {closeWalletWithWalletMeta, ...mutation} = useCloseWalletWithWalletMeta({
+    onSuccess: (walletMeta, args, context) => {
+      dispatch(clearUTXOs())
+      dispatch(clearAccountState())
+      options?.onSuccess?.(walletMeta, args, context)
+    },
+    ...options,
+  })
+
+  return {closeWalletWithWalletMeta, ...mutation}
+}
+
+export {useCloseWalletWrapper as useCloseWallet, useCloseWalletWithWalletMetaWrapper as useCloseWalletWithWalletMeta}
