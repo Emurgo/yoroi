1b0f1f0744d246ffb81911f1842a507e5307bf7e
Kevin Sullivan <wolverine.ks@gmail.com>
2022-09-20 23:39:09 -0500

    fix boundary style, move balances

--- src/components/Boundary/Boundary.tsx
+++ src/components/Boundary/Boundary.tsx
@@ -79,6 +79,6 @@ export const ErrorFallback: React.FC<ErrorFallbackProps> = ({error, resetErrorBo
 )
 
 const styles = StyleSheet.create({
-  container: {alignItems: 'center', justifyContent: 'center'},
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
   errorHeader: {alignItems: 'center', justifyContent: 'center', padding: 16},
 })
