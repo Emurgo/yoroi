73442903f127aece17602b5ca1cb512c8eca9d43
Denis <nebytov.d@gmail.com>
2022-09-23 21:36:21 +0300

    Added the test file

---
+++ tests/specs/invisibleWallets.test.ts
@@ -0,0 +1,44 @@
+import {enterPinCodeIfNecessary, firstAppLaunch, restoreWallet} from "../helpers/utils";
+import {ALL_TEST_WALLETS, VALID_PIN} from "../constants";
+import {getWalletButton} from "../screenObjects/myWallets.screen";
+import * as walletHistoryScreen from "../screenObjects/walletHistory.screen";
+import * as myWalletsScreen from "../screenObjects/myWallets.screen";
+
+
+describe('Invisible wallets case', function () {
+  // Execute a block of code before every tests
+  beforeEach(() => {
+    driver.launchApp()
+  })
+  // Execute a block of code after every tests
+  afterEach(() => {
+    driver.closeApp()
+  })
+
+  it('Prepare the app', async () => {
+    await firstAppLaunch(VALID_PIN)
+  })
+
+  it('Invisible wallets test', async () => {
+    await enterPinCodeIfNecessary(VALID_PIN)
+
+    for (const testWallet of ALL_TEST_WALLETS) {
+      await restoreWallet(testWallet);
+    }
+
+    for (let i = 0; i < 10; i++) {
+      for (const testWallet of ALL_TEST_WALLETS) {
+        // Open wallet
+        const walletButton = await getWalletButton(testWallet.name)
+        await walletButton.click()
+
+        // wait for the back button
+        await driver.waitUntil(async () => await walletHistoryScreen.backButton().isDisplayed(), 100)
+        // Press back
+        await walletHistoryScreen.backButton().click()
+
+        await driver.waitUntil(async () => await myWalletsScreen.pageTitle().isDisplayed())
+      }
+    }
+  })
+});
