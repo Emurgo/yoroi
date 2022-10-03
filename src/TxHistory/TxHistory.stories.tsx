305f5cecaa3a1af2082356c1fef436f3983e6e63
Javier Bueno <javierbueno@emurgo.io>
2022-10-03 17:35:18 +0200

    CR: update

--- src/TxHistory/TxHistory.stories.tsx
+++ src/TxHistory/TxHistory.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import {mockWallet} from '../../storybook'
 import {SelectedWalletProvider} from '../SelectedWallet'
+import {SendProvider} from '../Send/Context/SendContext'
 import {TxHistory as TxHistoryScreen} from './TxHistory'
 
 storiesOf('V2/TxHistory', module)
@@ -10,6 +11,21 @@ storiesOf('V2/TxHistory', module)
     return (
       <SelectedWalletProvider wallet={mockWallet}>
         <TxHistoryScreen />
+      </SelectedWalletProvider>
+    )
+  })
+  .add('default with API errors', () => {
+    const wallet = {
+      ...mockWallet,
+      fetchCurrentPrice: () => Promise.reject(new Error('fetchCurrentPrice failed')),
+      fetchUTXOs: () => Promise.reject(new Error('fetchUTXOs failed')),
+    }
+
+    return (
+      <SelectedWalletProvider wallet={wallet}>
+        <SendProvider key={wallet.id} wallet={wallet}>
+          <TxHistoryScreen />
+        </SendProvider>
       </SelectedWalletProvider>
     )
   })
