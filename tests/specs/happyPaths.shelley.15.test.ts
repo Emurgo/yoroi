db1f63e34d2c284b31ac4fb08077a384460c09f4
Denis <nebytov.d@gmail.com>
2022-11-08 13:24:59 +0300

    test branch

--- tests/specs/happyPaths.shelley.15.test.ts
+++ tests/specs/happyPaths.shelley.15.test.ts
@@ -1,32 +1,17 @@
-import * as addWalletScreen from '../screenObjects/addWallet.screen'
-import * as createNewWalletCredentialsScreen from '../screenObjects/createWalletScreens/createWalletCredentials.screen'
-import * as nobodyLookingScreen from '../screenObjects/createWalletScreens/nobodyLookingNotification.screen'
-import * as recoveryPhraseRememberScreen from '../screenObjects/createWalletScreens/recoveryPhraseRemember.screen'
-import * as recoveryPhraseNotificationScreen from '../screenObjects/createWalletScreens/recoveryPhraseNotification.screen'
-import * as recoveryPhraseEnterScreen from '../screenObjects/createWalletScreens/recoveryPhraseEnter.screen'
-import * as myWalletsScreen from '../screenObjects/myWallets.screen'
-import * as walletHistoryScreen from '../screenObjects/walletHistory.screen'
-import * as sendScreen from '../screenObjects/send.screen'
-import * as selectWalletToRestoreScreen from '../screenObjects/selectWalletToRestore.screen'
-import * as recoveryPhraseInputScreen from '../screenObjects/restoreWalletsScreens/recoveryPhraseEnterManually.screen'
-import * as verifyRestoredWalletScreen from '../screenObjects/restoreWalletsScreens/verifyRestoredWallet.screen'
-import * as walletBottomPanel from '../screenObjects/walletBottomPanel.screen'
-import * as stakingDashboard from '../screenObjects/stakingScreens/stakingDashboard.screen'
-import * as stakingCenter from '../screenObjects/stakingScreens/stakingCenter.screen'
-import * as confirmDelegationScreen from '../screenObjects/stakingScreens/confirmDelegating.screen'
-import * as withdrawWarningScreen from '../screenObjects/stakingScreens/withdrawWarning.screen'
-import * as confirmWithdrawTransactionScreen from '../screenObjects/stakingScreens/confirmWithdrawTransaction.screen'
+import {AssertionError} from 'chai'
+import chai from 'chai'
+
 import {
-  WALLET_NAME,
-  DEFAULT_TIMEOUT,
   DEFAULT_INTERVAL,
-  VALID_PIN,
+  DEFAULT_TIMEOUT,
   NORMAL_15_WORD_WALLET,
-  WalletType,
   SPENDING_PASSWORD,
+  STAKE_POOL_ID,
   TADA_TOKEN,
   TWO_MINUTES_TIMEOUT,
-  STAKE_POOL_ID,
+  VALID_PIN,
+  WALLET_NAME,
+  WalletType,
 } from '../constants'
 import {
   checkForErrors,
@@ -41,17 +26,34 @@ import {
   openWallet,
   repeatRecoveryPhrase,
 } from '../screenFunctions/myWallet.screenFunctions'
+import {enterPinCodeIfNecessary} from '../screenFunctions/prepare.screenFunctions'
+import {balanceAndFeeIsCalculated,prepareTransaction} from '../screenFunctions/send.screenFunctions'
 import {
   checkTokenInAssets,
   getLatestTxTime,
   getReceiveAddress,
   waitForNewTransaction,
 } from '../screenFunctions/walletHistory.screenFunctions'
-import {prepareTransaction, balanceAndFeeIsCalculated} from '../screenFunctions/send.screenFunctions'
-import {enterPinCodeIfNecessary} from '../screenFunctions/prepare.screenFunctions'
-import {AssertionError} from 'chai'
+import * as addWalletScreen from '../screenObjects/addWallet.screen'
+import * as createNewWalletCredentialsScreen from '../screenObjects/createWalletScreens/createWalletCredentials.screen'
+import * as nobodyLookingScreen from '../screenObjects/createWalletScreens/nobodyLookingNotification.screen'
+import * as recoveryPhraseEnterScreen from '../screenObjects/createWalletScreens/recoveryPhraseEnter.screen'
+import * as recoveryPhraseNotificationScreen from '../screenObjects/createWalletScreens/recoveryPhraseNotification.screen'
+import * as recoveryPhraseRememberScreen from '../screenObjects/createWalletScreens/recoveryPhraseRemember.screen'
+import * as myWalletsScreen from '../screenObjects/myWallets.screen'
+import * as recoveryPhraseInputScreen from '../screenObjects/restoreWalletsScreens/recoveryPhraseEnterManually.screen'
+import * as verifyRestoredWalletScreen from '../screenObjects/restoreWalletsScreens/verifyRestoredWallet.screen'
+import * as selectWalletToRestoreScreen from '../screenObjects/selectWalletToRestore.screen'
+import * as sendScreen from '../screenObjects/send.screen'
+import * as confirmDelegationScreen from '../screenObjects/stakingScreens/confirmDelegating.screen'
+import * as confirmWithdrawTransactionScreen from '../screenObjects/stakingScreens/confirmWithdrawTransaction.screen'
+import * as stakingCenter from '../screenObjects/stakingScreens/stakingCenter.screen'
+import * as stakingDashboard from '../screenObjects/stakingScreens/stakingDashboard.screen'
+import * as withdrawWarningScreen from '../screenObjects/stakingScreens/withdrawWarning.screen'
+import * as walletBottomPanel from '../screenObjects/walletBottomPanel.screen'
+import * as walletHistoryScreen from '../screenObjects/walletHistory.screen'
 
-const expect = require('chai').expect
+const expect = chai.expect
 
 describe('Happy paths', () => {
   describe('Creating a wallet', () => {
@@ -59,7 +61,7 @@ describe('Happy paths', () => {
       await myWalletsScreen.addWalletTestnetButton().click()
       await addWalletScreen.createWalletButton().click()
 
-      await driver.waitUntil(async () => await createNewWalletCredentialsScreen.credentialsView().isDisplayed())
+      await driver.waitUntil(async () => createNewWalletCredentialsScreen.credentialsView().isDisplayed())
       await enterWalletCredentials(WALLET_NAME)
       await hideKeyboard()
       await createNewWalletCredentialsScreen.continueButton().click()
@@ -77,7 +79,7 @@ describe('Happy paths', () => {
       await recoveryPhraseEnterScreen.confirmButton().click()
 
       await enterPinCodeIfNecessary(VALID_PIN)
-      await driver.waitUntil(async () => await myWalletsScreen.pageTitle().isDisplayed())
+      await driver.waitUntil(async () => myWalletsScreen.pageTitle().isDisplayed())
 
       expect(
         await driver.$(`[text="${WALLET_NAME}"]`).waitForExist({timeout: DEFAULT_TIMEOUT, interval: DEFAULT_INTERVAL}),
@@ -107,11 +109,11 @@ describe('Happy paths', () => {
       )
       await verifyRestoredWalletScreen.continueButton().click()
 
-      await driver.waitUntil(async () => await createNewWalletCredentialsScreen.credentialsView().isDisplayed())
+      await driver.waitUntil(async () => createNewWalletCredentialsScreen.credentialsView().isDisplayed())
       await enterWalletCredentials(NORMAL_15_WORD_WALLET.name)
       await createNewWalletCredentialsScreen.continueButton().click()
 
-      await driver.waitUntil(async () => await myWalletsScreen.pageTitle().isDisplayed())
+      await driver.waitUntil(async () => myWalletsScreen.pageTitle().isDisplayed())
 
       expect(
         await driver
@@ -127,9 +129,9 @@ describe('Happy paths', () => {
       const receiverAddress = await getReceiveAddress()
       await walletHistoryScreen.sendButton().click()
       await prepareTransaction(receiverAddress, TADA_TOKEN, '1')
-      await driver.waitUntil(async () => await balanceAndFeeIsCalculated())
+      await driver.waitUntil(async () => balanceAndFeeIsCalculated())
       await sendScreen.continueButton().click()
-      await driver.waitUntil(async () => await sendScreen.confirmTxButton().isDisplayed())
+      await driver.waitUntil(async () => sendScreen.confirmTxButton().isDisplayed())
       await enterNewValue(sendScreen.confirmSpendingPasswordInput, SPENDING_PASSWORD)
       await sendScreen.confirmTxButton().click()
 
@@ -141,7 +143,7 @@ describe('Happy paths', () => {
       ).to.be.true
 
       try {
-        await driver.waitUntil(async () => await waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
+        await driver.waitUntil(async () => waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
       } catch (e) {
         throw new AssertionError('There is no new transaction')
       }
@@ -155,9 +157,9 @@ describe('Happy paths', () => {
       const receiverAddress = await getReceiveAddress()
       await walletHistoryScreen.sendButton().click()
       await prepareTransaction(receiverAddress, tokenName, '1')
-      await driver.waitUntil(async () => await balanceAndFeeIsCalculated())
+      await driver.waitUntil(async () => balanceAndFeeIsCalculated())
       await sendScreen.continueButton().click()
-      await driver.waitUntil(async () => await sendScreen.confirmTxButton().isDisplayed())
+      await driver.waitUntil(async () => sendScreen.confirmTxButton().isDisplayed())
       await enterNewValue(sendScreen.confirmSpendingPasswordInput, SPENDING_PASSWORD)
       await sendScreen.confirmTxButton().click()
 
@@ -169,7 +171,7 @@ describe('Happy paths', () => {
       ).to.be.true
 
       try {
-        await driver.waitUntil(async () => await waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
+        await driver.waitUntil(async () => waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
       } catch (e) {
         throw new AssertionError('There is no new transaction')
       }
@@ -180,11 +182,11 @@ describe('Happy paths', () => {
       const latestTxTime = await getLatestTxTime()
 
       await walletBottomPanel.stakingButton().click()
-      await driver.waitUntil(async () => await stakingDashboard.isDisplayed())
+      await driver.waitUntil(async () => stakingDashboard.isDisplayed())
       await stakingDashboard.goToStakingCenterButton().click()
       await enterNewValue(stakingCenter.nightlyPoolHashInput, STAKE_POOL_ID)
       await stakingCenter.nightlyDelegateButton().click()
-      await driver.waitUntil(async () => await confirmDelegationScreen.isDisplayed())
+      await driver.waitUntil(async () => confirmDelegationScreen.isDisplayed())
       const stakePoolHash = await confirmDelegationScreen.stakePoolHashText().getText()
       expect(stakePoolHash).to.be.equal(STAKE_POOL_ID)
       await enterNewValue(confirmDelegationScreen.spendingPasswordInput, SPENDING_PASSWORD)
@@ -193,13 +195,13 @@ describe('Happy paths', () => {
       await checkForErrors()
 
       try {
-        await driver.waitUntil(async () => await waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT + 60000))
+        await driver.waitUntil(async () => waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT + 60000))
       } catch (e) {
         throw new AssertionError('There is no new transaction')
       }
 
       await walletBottomPanel.stakingButton().click()
-      await driver.waitUntil(async () => await stakingDashboard.isDisplayed())
+      await driver.waitUntil(async () => stakingDashboard.isDisplayed())
       const availableFunds = await stakingDashboard.availableFundsText().getText()
       const totalDelegated = await stakingDashboard.userSummaryDelegatedText().getText()
 
@@ -211,13 +213,13 @@ describe('Happy paths', () => {
       const latestTxTime = await getLatestTxTime()
       await walletBottomPanel.stakingButton().click()
 
-      await driver.waitUntil(async () => await stakingDashboard.isDisplayed())
+      await driver.waitUntil(async () => stakingDashboard.isDisplayed())
       const availableFunds = await stakingDashboard.availableFundsText().getText()
       const totalDelegated = await stakingDashboard.userSummaryDelegatedText().getText()
       expect(availableFunds).to.be.equal(totalDelegated)
       await stakingDashboard.userSummaryWithdrawButton().click()
 
-      await driver.waitUntil(async () => await withdrawWarningScreen.isDisplayed())
+      await driver.waitUntil(async () => withdrawWarningScreen.isDisplayed())
       const [xPoint, yStartPoint] = await getCoordinateByPercents(50, 80)
       const [, yEndPoint] = await getCoordinateByPercents(50, 30)
 
@@ -226,7 +228,7 @@ describe('Happy paths', () => {
       await withdrawWarningScreen.iUnderstandCheckbox().click()
       await withdrawWarningScreen.deregisterButton().click()
 
-      await driver.waitUntil(async () => await confirmWithdrawTransactionScreen.isDisplayed())
+      await driver.waitUntil(async () => confirmWithdrawTransactionScreen.isDisplayed())
       const recoveredBalance = (await confirmWithdrawTransactionScreen.recoveredBalanceText().getText()).split(' ')[0]
       expect(parseFloat(recoveredBalance)).to.be.equal(2)
       await enterNewValue(confirmWithdrawTransactionScreen.walletPasswordInput, SPENDING_PASSWORD, false)
@@ -245,13 +247,13 @@ describe('Happy paths', () => {
       await checkForErrors()
 
       try {
-        await driver.waitUntil(async () => await waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
+        await driver.waitUntil(async () => waitForNewTransaction(latestTxTime, TWO_MINUTES_TIMEOUT))
       } catch (e) {
         throw new AssertionError('There is no new transaction')
       }
 
       await walletBottomPanel.stakingButton().click()
-      await driver.waitUntil(async () => await stakingDashboard.isDisplayed())
+      await driver.waitUntil(async () => stakingDashboard.isDisplayed())
       expect(await stakingDashboard.notDelegatedImage().isDisplayed()).to.be.true
     })
   })
