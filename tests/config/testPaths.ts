44005eb825f9fecaecf429e196f2040a7050bba1
Denis Nebytov <denis@emurgo.io>
2022-11-10 17:17:43 +0300

    Denis/test fixes for preprod (#2204)

--- tests/config/testPaths.ts
+++ tests/config/testPaths.ts
@@ -1,3 +1,4 @@
 export const artifactsDir = './tests/artifacts/'
 export const screenshotsDir = `${artifactsDir}screenshots/`
+export const screenRecordsDir = `${artifactsDir}screenRecords/`
 export const appiumLogsPath = `${artifactsDir}appium_info.log`
