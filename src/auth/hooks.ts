fbd7389f4fd1041cda260b4f5c2be1da772a4b07
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2022-11-15 14:38:23 -0500

    Update src/auth/hooks.ts
    Co-authored-by: Kevin Sullivan <kevin.sullivan@emurgo.io>

--- src/auth/hooks.ts
+++ src/auth/hooks.ts
@@ -98,7 +98,6 @@ export const useAuthOsWithEasyConfirmation = (
 ) => {
   const strings = useStrings()
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const alert = (error: unknown) => {
     if (error instanceof Keychain.Errors.CancelledByUser) return
     if (error instanceof Keychain.Errors.TooManyAttempts) return Alert.alert(strings.error, strings.tooManyAttempts)
