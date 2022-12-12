3a5bae679c9f54935a3558d9a26ea83eb49e4ac1
Javier Bueno <javierbueno@emurgo.io>
2022-12-12 16:51:49 +0100

    fix tx list item layout

--- src/TxHistory/TxHistoryList/TxHistoryListItem.tsx
+++ src/TxHistory/TxHistoryList/TxHistoryListItem.tsx
@@ -80,7 +80,7 @@ export const TxHistoryListItem = ({transaction}: Props) => {
         <Spacer height={4} />
 
         <Text secondary small testID="submittedAtText">
-          {totalAssets === 0 && submittedAt}
+          {submittedAt}
         </Text>
       </Middle>
 
@@ -92,11 +92,7 @@ export const TxHistoryListItem = ({transaction}: Props) => {
         )}
 
         {totalAssets !== 0 && (
-          <Row>
-            <Text secondary small testID="submittedAtText">
-              {submittedAt}
-            </Text>
-
+          <Row style={{justifyContent: 'space-between'}}>
             <Text testID="totalAssetsText">{strings.assets(totalAssets)}</Text>
           </Row>
         )}
