3a6e08fea9ad20640422d48866c1a0b6d53e5d0a
jemubm <105349292+jemubm@users.noreply.github.com>
2022-12-15 17:58:41 +0100

    Tx list screen issues fix improvement (#2274)
    * tx list screen bugs fix improvement
    
    * tx list screen bugs fix improvement 2

--- src/TxHistory/CollapsibleHeader.tsx
+++ src/TxHistory/CollapsibleHeader.tsx
@@ -3,11 +3,17 @@ import {InteractionManager, LayoutAnimation, View, ViewProps} from 'react-native
 
 export const CollapsibleHeader = ({expanded, children}: {expanded: boolean} & ViewProps) => {
   const [_expanded, setExpanded] = React.useState(expanded)
+  const firstRenderRef = React.useRef(true)
 
   React.useLayoutEffect(() => {
     // it fixes layout and blank screen issues
     // https://emurgo.atlassian.net/browse/YOMO-428
     // https://emurgo.atlassian.net/browse/YOMO-427
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false
+      return
+    }
+
     InteractionManager.runAfterInteractions(() => {
       LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
 
