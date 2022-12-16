5d91d4471db98ccb9553b7095178456b2b583373
Kevin Sullivan <kevin.sullivan@emurgo.io>
2022-12-16 12:48:36 -0600

    cleanup wallet restore/create errors (#2276)
    * cleanup wallet select
    
    * clean up wcreate wallet errors
    
    * chore: rem deprecated
    
    * chore: simpler mocks
    
    * Update src/WalletInit/SaveNanoX/SaveNanoXScreen.tsx
    
    Co-authored-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
    
    Co-authored-by: stackchain <30806844+stackchain@users.noreply.github.com>

--- src/WalletInit/WalletCredentials/WalletCredentialsScreen.stories.tsx
+++ src/WalletInit/WalletCredentials/WalletCredentialsScreen.stories.tsx
@@ -1,24 +1,70 @@
 import {NavigationRouteContext} from '@react-navigation/native'
+import {action} from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react-native'
 import React from 'react'
 
+import {NetworkError} from '../../legacy/errors'
+import {WalletManagerProvider} from '../../WalletManager'
+import {WalletManager, walletManager} from '../../yoroi-wallets'
 import {WalletCredentialsScreen} from './WalletCredentialsScreen'
 
-storiesOf('WalletCredentialsScreen', module).add('Default', () => {
-  const route = {
-    key: 'key',
-    name: 'name',
-    params: {
-      phrase: 'phrase',
-      networkId: 300,
-      walletImplementationId: 'haskell',
-      provider: 'provider',
-    },
-  }
+storiesOf('WalletCredentialsScreen', module)
+  .add('Default', () => {
+    const route = {
+      key: 'key',
+      name: 'name',
+      params: {
+        phrase: 'phrase',
+        networkId: 300,
+        walletImplementationId: 'haskell',
+        provider: 'provider',
+      },
+    }
 
-  return (
-    <NavigationRouteContext.Provider value={route}>
-      <WalletCredentialsScreen />
-    </NavigationRouteContext.Provider>
-  )
-})
+    return (
+      <NavigationRouteContext.Provider value={route}>
+        <WalletManagerProvider
+          walletManager={
+            {
+              ...walletManager,
+              createWallet: (...args) => {
+                action('create wallet')(...args)
+              },
+            } as unknown as WalletManager
+          }
+        >
+          <WalletCredentialsScreen />
+        </WalletManagerProvider>
+      </NavigationRouteContext.Provider>
+    )
+  })
+  .add('error, no network connection', () => {
+    const route = {
+      key: 'key',
+      name: 'name',
+      params: {
+        phrase: 'phrase',
+        networkId: 300,
+        walletImplementationId: 'haskell',
+        provider: 'provider',
+      },
+    }
+
+    return (
+      <NavigationRouteContext.Provider value={route}>
+        <WalletManagerProvider
+          walletManager={
+            {
+              ...walletManager,
+              createWallet: (...args) => {
+                action('create wallet')(...args)
+                throw new NetworkError()
+              },
+            } as unknown as WalletManager
+          }
+        >
+          <WalletCredentialsScreen />
+        </WalletManagerProvider>
+      </NavigationRouteContext.Provider>
+    )
+  })
