233174e0414f97c6129df09bcb257892ce08c7ec
Kevin Sullivan <wolverine.ks@gmail.com>
2022-12-22 16:14:58 -0600

    storage 2.0

---
+++ src/yoroi-wallets/storage.test.ts
@@ -0,0 +1,279 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import {createPrefixedStorage, debug, makeRootStorage, toAbsolutePath, toFileName, validatePath} from './storage'
+
+describe('storage', () => {
+  beforeEach(() => {
+    AsyncStorage.clear()
+  })
+
+  it('prefixes', async () => {
+    const storage0 = makeRootStorage()
+    expect(storage0.path).toBe('/')
+
+    const storage1 = createPrefixedStorage({storage: storage0, path: 'level1'})
+    expect(storage1.path).toBe('/level1')
+
+    const storage2 = createPrefixedStorage({storage: storage1, path: 'level2'})
+    expect(storage2.path).toBe('/level1/level2')
+
+    const storage3 = createPrefixedStorage({storage: storage2, path: 'level3'})
+    expect(storage3.path).toBe('/level1/level2/level3')
+
+    const storage4 = createPrefixedStorage({storage: storage3, path: 'level4'})
+    expect(storage4.path).toBe('/level1/level2/level3/level4')
+  })
+
+  it('create, setItem, getItem, getAllKeys', async () => {
+    // storage0
+    const storage0 = createPrefixedStorage()
+    expect(await storage0.getAllKeys()).toEqual([])
+    // add item
+    const item0 = 'item0'
+    await storage0.setItem('item0', item0)
+    expect(await storage0.getItem('item0')).toEqual(item0)
+    // add item
+    const item00 = 'item00'
+    await storage0.setItem('item00', item00)
+    expect(await storage0.getItem('item00')).toEqual(item00)
+    expect(await storage0.getAllKeys()).toEqual(['item0', 'item00'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item0": "item0",
+        "/item00": "item00",
+      }
+    `)
+
+    // storage1
+    const storage1 = createPrefixedStorage({storage: storage0, path: 'level1'})
+    expect(await storage1.getAllKeys()).toEqual([])
+    // add item
+    const item1 = 'item1'
+    await storage1.setItem('item1', item1)
+    expect(await storage1.getItem('item1')).toEqual(item1)
+    expect(await storage1.getAllKeys()).toEqual(['item1'])
+    // add item
+    const item11 = 'item11'
+    await storage1.setItem('item11', item11)
+    expect(await storage1.getItem('item11')).toEqual(item11)
+    expect(await storage1.getAllKeys()).toEqual(['item1', 'item11'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item0": "item0",
+        "/item00": "item00",
+        "/level1/item1": "item1",
+        "/level1/item11": "item11",
+      }
+    `)
+
+    // storage2
+    const storage2 = createPrefixedStorage({storage: storage1, path: 'level2'})
+    expect(await storage2.getAllKeys()).toEqual([])
+    // add item
+    const item2 = 'item2'
+    await storage2.setItem('item2', item2)
+    expect(await storage2.getItem('item2')).toEqual(item2)
+    expect(await storage2.getAllKeys()).toEqual(['item2'])
+    // add item
+    const item22 = 'item22'
+    await storage2.setItem('item22', item22)
+    expect(await storage2.getItem('item22')).toEqual(item22)
+    expect(await storage2.getAllKeys()).toEqual(['item2', 'item22'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item0": "item0",
+        "/item00": "item00",
+        "/level1/item1": "item1",
+        "/level1/item11": "item11",
+        "/level1/level2/item2": "item2",
+        "/level1/level2/item22": "item22",
+      }
+    `)
+
+    // storage3
+    const storage3 = createPrefixedStorage({storage: storage2, path: 'level3'})
+    expect(await storage3.getAllKeys()).toEqual([])
+    // add item
+    const item3 = 'item3'
+    await storage3.setItem('item3', item3)
+    expect(await storage3.getItem('item3')).toEqual(item3)
+    expect(await storage3.getAllKeys()).toEqual(['item3'])
+    // add item
+    const item33 = 'item33'
+    await storage3.setItem('item33', item33)
+    expect(await storage3.getItem('item33')).toEqual(item33)
+    expect(await storage3.getAllKeys()).toEqual(['item3', 'item33'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item0": "item0",
+        "/item00": "item00",
+        "/level1/item1": "item1",
+        "/level1/item11": "item11",
+        "/level1/level2/item2": "item2",
+        "/level1/level2/item22": "item22",
+        "/level1/level2/level3/item3": "item3",
+        "/level1/level2/level3/item33": "item33",
+      }
+    `)
+
+    // remove item
+    await storage0.removeItem('item0')
+    expect(await storage0.getItem('item0')).toEqual(null)
+    expect(await storage0.getAllKeys()).toEqual(['item00'])
+
+    // other levels unaffected
+    expect(await storage1.getAllKeys()).toEqual(['item1', 'item11'])
+    expect(await storage2.getAllKeys()).toEqual(['item2', 'item22'])
+    expect(await storage3.getAllKeys()).toEqual(['item3', 'item33'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item00": "item00",
+        "/level1/item1": "item1",
+        "/level1/item11": "item11",
+        "/level1/level2/item2": "item2",
+        "/level1/level2/item22": "item22",
+        "/level1/level2/level3/item3": "item3",
+        "/level1/level2/level3/item33": "item33",
+      }
+    `)
+
+    // remove item
+    await storage3.removeItem('item33')
+    expect(await storage3.getItem('item33')).toEqual(null)
+    expect(await storage3.getAllKeys()).toEqual(['item3'])
+
+    // other levels unaffected
+    expect(await storage0.getAllKeys()).toEqual(['item00'])
+    expect(await storage1.getAllKeys()).toEqual(['item1', 'item11'])
+    expect(await storage2.getAllKeys()).toEqual(['item2', 'item22'])
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item00": "item00",
+        "/level1/item1": "item1",
+        "/level1/item11": "item11",
+        "/level1/level2/item2": "item2",
+        "/level1/level2/item22": "item22",
+        "/level1/level2/level3/item3": "item3",
+      }
+    `)
+
+    expect(await storage0.removeItem('unknown')).toEqual(null)
+  })
+
+  it('create, multiSet, multiGet, multiRemove, clear', async () => {
+    // storage0
+    const storage0 = createPrefixedStorage()
+    // add items
+    const item0 = 'item0'
+    const item00 = 'item00'
+    const item000 = 'item000'
+    const item0000 = 'item0000'
+    await storage0.multiSet([
+      ['item0', item0],
+      ['item00', item00],
+      ['item000', item000],
+      ['item0000', item0000],
+    ])
+    expect(await storage0.getItem('item0')).toEqual(item0)
+    expect(await storage0.getItem('item00')).toEqual(item00)
+    expect(await storage0.getItem('item000')).toEqual(item000)
+    expect(await storage0.getItem('item0000')).toEqual(item0000)
+    expect(await storage0.getAllKeys()).toEqual(['item0', 'item00', 'item000', 'item0000'])
+    expect(await storage0.multiGet(['item0', 'item00', 'item000', 'item000'])).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "item0",
+          "item0",
+        ],
+        Array [
+          "item00",
+          "item00",
+        ],
+        Array [
+          "item000",
+          "item000",
+        ],
+        Array [
+          "item000",
+          "item000",
+        ],
+      ]
+    `)
+    expect(await debug()).toMatchInlineSnapshot(`
+      Object {
+        "/item0": "item0",
+        "/item00": "item00",
+        "/item000": "item000",
+        "/item0000": "item0000",
+      }
+    `)
+    await storage0.multiRemove(['item0', 'item000'])
+    expect(await storage0.multiGet(['item0', 'item00', 'item000', 'item0000'])).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "item0",
+          null,
+        ],
+        Array [
+          "item00",
+          "item00",
+        ],
+        Array [
+          "item000",
+          null,
+        ],
+        Array [
+          "item0000",
+          "item0000",
+        ],
+      ]
+    `)
+
+    await storage0.clear()
+    expect(await storage0.multiGet(['item0', 'item00', 'item000', 'item0000'])).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "item0",
+          null,
+        ],
+        Array [
+          "item00",
+          null,
+        ],
+        Array [
+          "item000",
+          null,
+        ],
+        Array [
+          "item0000",
+          null,
+        ],
+      ]
+    `)
+  })
+
+  it('validates path', () => {
+    expect(validatePath('valid')).toBe(true)
+
+    expect(validatePath('')).toBe(false)
+    expect(validatePath('/invalid')).toBe(false)
+    expect(validatePath('invalid/path')).toBe(false)
+    expect(validatePath('/invalid/path')).toBe(false)
+    expect(validatePath('/invalid/')).toBe(false)
+    expect(validatePath('//')).toBe(false)
+    expect(validatePath('invalid/')).toBe(false)
+  })
+
+  it('toAbsolutePath', () => {
+    expect(toAbsolutePath('/', '/', 'fileName')).toEqual('/fileName')
+    expect(toAbsolutePath('/', 'level1', 'fileName')).toEqual('/level1/fileName')
+    expect(toAbsolutePath('/level1', '/', 'fileName')).toEqual('/level1/fileName')
+    expect(toAbsolutePath('/level1', 'level2', 'fileName')).toEqual('/level1/level2/fileName')
+  })
+
+  it('toFileName', () => {
+    expect(toFileName('/fileName')).toBe('fileName')
+    expect(toFileName('/level1/fileName')).toBe('fileName')
+    expect(toFileName('/level1/level2/fileName')).toBe('fileName')
+  })
+})
--- src/yoroi-wallets/storage.ts
+++ src/yoroi-wallets/storage.ts
@@ -44,3 +44,111 @@ export const makeStorageWithPrefix = (prefix: string): Storage => {
     },
   }
 }
+
+interface PrefixedStorage extends Omit<AsyncStorageStatic, 'multiMerge' | 'mergeItem'> {
+  path: string
+  toAbsolutePath: (key: string) => string
+}
+
+export const makeRootStorage = (storage: AsyncStorageStatic = AsyncStorage) => {
+  return {
+    ...storage,
+    path: '/',
+    toAbsolutePath: (key: string) => `/${key}`,
+  }
+}
+
+export const debug = async () => {
+  const keys = await AsyncStorage.getAllKeys()
+  const items = await AsyncStorage.multiGet(keys)
+
+  const data = Object.fromEntries(items)
+
+  return data
+}
+
+export const createPrefixedStorage = (options?: {storage?: PrefixedStorage; path?: string}) => {
+  const {storage = makeRootStorage(), path = '/'} = options || {}
+  if (!validatePath(path)) throw new Error("invalid path: paths may not contain '/'")
+  const isRootStorage = storage.path === '/'
+  const isRootPath = path === '/'
+
+  const storagePath =
+    isRootStorage && isRootPath
+      ? '/'
+      : !isRootStorage && isRootPath
+      ? storage.path
+      : isRootStorage && !isRootPath
+      ? `${storage.path}${path}`
+      : `${storage.path}/${path}`
+
+  const prefixedStorage: PrefixedStorage = {
+    path: storagePath,
+    toAbsolutePath: (fileName: string) => toAbsolutePath(storage.path, path, fileName),
+    async getItem(fileName: string) {
+      const absolutePath = toAbsolutePath(storage.path, path, fileName)
+      return AsyncStorage.getItem(absolutePath)
+    },
+    async multiGet(fileNames: string[]) {
+      const absolutePaths = fileNames.map((fileName) => toAbsolutePath(storage.path, path, fileName))
+      const items = await AsyncStorage.multiGet(absolutePaths)
+      return items.map(([absolutePath, file]) => [toFileName(absolutePath), file])
+    },
+
+    async setItem(fileName: string, file: string) {
+      const absolutePath = toAbsolutePath(storage.path, path, fileName)
+      return AsyncStorage.setItem(absolutePath, file)
+    },
+    async multiSet(items: string[][]) {
+      const absolutePathItems = items.map(([fileName, file]) => [toAbsolutePath(storage.path, path, fileName), file])
+      return AsyncStorage.multiSet(absolutePathItems)
+    },
+
+    async removeItem(fileName: string) {
+      const absolutePath = toAbsolutePath(storage.path, path, fileName)
+      return AsyncStorage.removeItem(absolutePath)
+    },
+    async multiRemove(fileNames: string[]) {
+      const absolutePaths = fileNames.map((fileName) => toAbsolutePath(storage.path, path, fileName))
+      return AsyncStorage.multiRemove(absolutePaths)
+    },
+    async clear() {
+      const allKeys = await this.getAllKeys()
+      return this.multiRemove(allKeys)
+    },
+
+    async getAllKeys() {
+      const allKeys = await AsyncStorage.getAllKeys()
+      const regex = isRootStorage && isRootPath ? new RegExp(`^/[^/]*$`) : new RegExp(`^${storagePath}/[^/]*$`)
+
+      return allKeys.filter((absolutePath) => absolutePath.match(regex)).map(toFileName)
+    },
+  }
+
+  return prefixedStorage
+}
+
+export const validatePath = (path: string) => {
+  if (path === '/') return true
+  if (path.includes('/')) return false
+  if (path === '') return false
+  return true
+}
+
+export const toAbsolutePath = (storagePath: string, folderName: string, fileName: string) => {
+  const isRootStorage = storagePath === '/'
+  const isRootFolder = folderName === '/'
+
+  return isRootStorage && isRootFolder
+    ? `/${fileName}`
+    : !isRootStorage && isRootFolder
+    ? `${storagePath}/${fileName}`
+    : isRootStorage && !isRootFolder
+    ? `${storagePath}${folderName}/${fileName}`
+    : `${storagePath}/${folderName}/${fileName}`
+}
+
+export const toFileName = (absolutePath: string) => {
+  const lastSlashIndex = absolutePath.lastIndexOf('/')
+  return absolutePath.slice(lastSlashIndex + 1)
+}
