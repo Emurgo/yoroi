54e4fd5db7096841dccea206c26293b55dab630d
Michal Szorád <miso.szorad@gmail.com>
2023-01-06 11:12:35 +0100

    Add virtualized list

--- src/Nfts/ImageGallery/ImageGallery.tsx
+++ src/Nfts/ImageGallery/ImageGallery.tsx
@@ -1,5 +1,14 @@
 import React, {useEffect} from 'react'
-import {Dimensions, GestureResponderEvent, Image, ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native'
+import {
+  Dimensions,
+  GestureResponderEvent,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  VirtualizedList,
+} from 'react-native'
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder'
 
 import {Icon, Spacer, Text} from '../../components'
@@ -26,22 +35,48 @@ export const SkeletonGallery = ({amount}: {amount: number} = {amount: 3}) => {
 }
 
 export const ImageGallery = ({nfts = [], onSelect}: Props) => {
+  const rows = groupArray(nfts)
   return (
     <ScrollView bounces={false} contentContainerStyle={styles.galleryContainer}>
-      {nfts.map((nft, index) => {
-        const fingerprint = getAssetFingerprint(nft.metadata.policyId, nft.metadata.assetNameHex)
-        return (
-          <ModeratedImage
-            onPress={() => onSelect(index)}
-            image={nft.image}
-            fingerprint={fingerprint}
-            text={nft.name}
-            key={fingerprint}
-          />
-        )
-      })}
+      <VirtualizedList
+        data={rows}
+        initialNumToRender={4}
+        getItem={(array, index) => array[index]}
+        getItemCount={() => rows.length}
+        keyExtractor={(row: YoroiNFT[]) => getAssetFingerprint(row[0].metadata.policyId, row[0].metadata.assetNameHex)}
+        renderItem={({item: [nft1, nft2]}) => {
+          const nft1Fingerprint = getAssetFingerprint(nft1.metadata.policyId, nft1.metadata.assetNameHex)
+          const nft2Fingerprint = getAssetFingerprint(nft2.metadata.policyId, nft2.metadata.assetNameHex)
+          return (
+            <View style={styles.row}>
+              <ModeratedImage
+                onPress={() => onSelect(nfts.indexOf(nft1))}
+                image={nft1.image}
+                fingerprint={nft1Fingerprint}
+                text={nft1.name}
+                key={nft1Fingerprint}
+              />
+              <ModeratedImage
+                onPress={() => onSelect(nfts.indexOf(nft2))}
+                image={nft2.image}
+                fingerprint={nft2Fingerprint}
+                text={nft2.name}
+                key={nft2Fingerprint}
+              />
+            </View>
+          )
+        }}
+      />
     </ScrollView>
   )
+}
+
+function groupArray<T>(arr: T[]) {
+  const result: T[][] = []
+  for (let i = 0; i < arr.length; i += 2) {
+    result.push([arr[i], arr[i + 1]])
+  }
+  return result
 }
 
 interface ModeratedImageProps {
@@ -160,6 +195,12 @@ const styles = StyleSheet.create({
   textTop: {
     fontSize: 14,
     lineHeight: 22,
+  },
+  row: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
   },
 })
 
