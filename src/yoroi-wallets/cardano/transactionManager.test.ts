875403b51fc75ac19469beaff0f103c6160bf383
Javier Bueno <javierbueno@emurgo.io>
2023-01-18 21:33:31 +0100

    memo storage refactor

--- src/yoroi-wallets/cardano/transactionManager.test.ts
+++ src/yoroi-wallets/cardano/transactionManager.test.ts
@@ -1,39 +1,21 @@
 import {storage as rootStorage} from '../storage'
 import {makeTransactionManager} from './transactionManager'
 
-describe('memo storage', () => {
-  it('works', async () => {
+describe('transaction manager', () => {
+  it('memo storage', async () => {
     const storage = rootStorage.join('tx-manager/')
-    const {memo} = makeTransactionManager({storage})
+    const transactionManager = makeTransactionManager({storage})
 
-    // saving one memo and removing it
-    await memo.save('fake-tx-id-1', 'Send money to my friend')
-    await memo.read('fake-tx-id-1').then((memo) => {
+    // saving two memos
+    await transactionManager.saveMemo('fake-tx-id-1', 'Send money to my friend')
+    await transactionManager.saveMemo('fake-tx-id-2', 'Send money to my girlfriend')
+
+    // reading two memos
+    await transactionManager.readMemo('fake-tx-id-1').then((memo) => {
       return expect(memo).toEqual('Send money to my friend')
     })
-    await memo.remove('fake-tx-id-1')
-    await memo.read('fake-tx-id-1').then((memo) => {
-      return expect(memo).toEqual(null)
-    })
-
-    // saving two memos and clearing storage
-    await memo.save('fake-tx-id-1', 'Send money to my friend')
-    await memo.save('fake-tx-id-2', 'Send money to my girlfriend')
-
-    await memo.read('fake-tx-id-1').then((memo) => {
-      return expect(memo).toEqual('Send money to my friend')
-    })
-    await memo.read('fake-tx-id-2').then((memo) => {
+    await transactionManager.readMemo('fake-tx-id-2').then((memo) => {
       return expect(memo).toEqual('Send money to my girlfriend')
-    })
-
-    await memo.clear()
-
-    await memo.read('fake-tx-id-1').then((memo) => {
-      return expect(memo).toEqual(null)
-    })
-    await memo.read('fake-tx-id-2').then((memo) => {
-      return expect(memo).toEqual(null)
     })
   })
 })
--- src/yoroi-wallets/cardano/transactionManager.ts
+++ src/yoroi-wallets/cardano/transactionManager.ts
@@ -1,18 +1,10 @@
 import {Storage} from '../storage'
 
 export const makeTransactionManager = ({storage}: {storage: Storage}) => {
-  const memo = makeMemoStorage(storage.join('memos/'))
+  const memosStorage = storage.join('memos/')
 
   return {
-    memo,
-  }
-}
-
-export const makeMemoStorage = (storage: Storage) => {
-  return {
-    save: (txId: string, memo: string) => storage.setItem(txId, memo),
-    read: (txId: string) => storage.getItem(txId),
-    remove: (txId: string) => storage.removeItem(txId),
-    clear: () => storage.clear(),
+    saveMemo: (txId: string, memo: string) => memosStorage.setItem(txId, memo),
+    readMemo: (txId: string) => memosStorage.getItem(txId),
   }
 }
