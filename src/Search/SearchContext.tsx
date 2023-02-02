87fd610f65bd49cb5555646b198378a99dea3926
Michal Szorád <miso.szorad@gmail.com>
2023-02-02 14:35:23 +0000

    Use reducer in search context

--- src/Search/SearchContext.tsx
+++ src/Search/SearchContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, ReactNode, useContext, useState} from 'react'
+import React, {createContext, ReactNode, useContext, useReducer} from 'react'
 
 interface SearchContextApi {
   search: string
@@ -17,10 +17,33 @@ export const useSearch = () => {
 }
 
 export const SearchProvider = ({children, value}: {children: ReactNode; value?: Partial<SearchContextApi>}) => {
-  const [search, setSearch] = useState(DEFAULT_SEARCH_VALUE)
-  const clearSearch = () => setSearch(DEFAULT_SEARCH_VALUE)
+  const [search, dispatch] = useReducer(searchReducer, DEFAULT_SEARCH_VALUE)
+  const clearSearch = () => dispatch({type: 'CLEAR'})
+  const setSearch = (search: string) => dispatch({type: 'SET', payload: search})
   const defaultValue = {search, setSearch, clearSearch}
   return <SearchContext.Provider value={{...defaultValue, ...value}}>{children}</SearchContext.Provider>
+}
+
+type ClearSearchAction = {
+  type: 'CLEAR'
+}
+
+type SetSearchAction = {
+  type: 'SET'
+  payload: string
+}
+
+type SearchAction = ClearSearchAction | SetSearchAction
+
+function searchReducer(state: string, action: SearchAction) {
+  switch (action.type) {
+    case 'CLEAR':
+      return DEFAULT_SEARCH_VALUE
+    case 'SET':
+      return action.payload
+    default:
+      return state
+  }
 }
 
 const DEFAULT_SEARCH_VALUE = ''
