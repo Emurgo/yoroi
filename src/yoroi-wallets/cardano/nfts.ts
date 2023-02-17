a4dc6110144c25bc086cddb33ddf8caae1e5c654
Michal Szorád <miso.szorad@gmail.com>
2023-02-17 16:48:23 +0000

    Improve NFTs parsing

--- src/yoroi-wallets/cardano/nfts.ts
+++ src/yoroi-wallets/cardano/nfts.ts
@@ -2,17 +2,23 @@ import {getAssetFingerprint} from '../../legacy/format'
 import {AssetMetadata, NftMetadata, YoroiNft} from '../types'
 import {asciiToHex} from './api/utils'
 
-export const convertNft = (assetMetadata: AssetMetadata, storageUrl: string): YoroiNft => {
+export const convertNfts = (assetMetadata: AssetMetadata, storageUrl: string): YoroiNft[] => {
   const policyId = Object.keys(assetMetadata)[0]
-  const assetNameKey = Object.keys(assetMetadata[policyId])[0]
-  const metadata: NftMetadata = assetMetadata[policyId][assetNameKey]
-  const assetNameHex = asciiToHex(assetNameKey)
+  const allMetadataKeys = Object.keys(assetMetadata[policyId])
+  return allMetadataKeys.map((name) => {
+    const metadata = assetMetadata[policyId][name]
+    return convertNft(metadata, storageUrl, policyId, name)
+  })
+}
+
+const convertNft = (metadata: NftMetadata, storageUrl: string, policyId: string, shortName: string): YoroiNft => {
+  const assetNameHex = asciiToHex(shortName)
   const fingerprint = getAssetFingerprint(policyId, assetNameHex)
   const description = Array.isArray(metadata.description) ? metadata.description.join(' ') : metadata.description
 
   const id = `${policyId}.${assetNameHex}`
   return {
-    id: id,
+    id,
     fingerprint,
     name: metadata.name,
     description: description ?? '',
