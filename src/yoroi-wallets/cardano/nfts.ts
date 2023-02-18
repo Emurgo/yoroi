91fc01ae5010f6cdb81d7a93d2cabeb242150752
Michal Szorád <miso.szorad@gmail.com>
2023-02-18 17:51:42 +0000

    Do not show NFTs that do not belong to the wallet anymore

--- src/yoroi-wallets/cardano/nfts.ts
+++ src/yoroi-wallets/cardano/nfts.ts
@@ -1,17 +1,13 @@
 import {getAssetFingerprint} from '../../legacy/format'
-import {AssetMetadata, NftMetadata, YoroiNft} from '../types'
+import {NftMetadata, YoroiNft} from '../types'
 import {asciiToHex} from './api/utils'
 
-export const convertNfts = (assetMetadata: AssetMetadata, storageUrl: string): YoroiNft[] => {
-  const [policyId] = Object.keys(assetMetadata)
-  const allMetadataKeys = Object.keys(assetMetadata[policyId])
-  return allMetadataKeys.map((name) => {
-    const metadata = assetMetadata[policyId][name]
-    return convertNft(metadata, storageUrl, policyId, name)
-  })
-}
-
-const convertNft = (metadata: NftMetadata, storageUrl: string, policyId: string, shortName: string): YoroiNft => {
+export const convertNft = (
+  metadata: NftMetadata,
+  storageUrl: string,
+  policyId: string,
+  shortName: string,
+): YoroiNft => {
   const assetNameHex = asciiToHex(shortName)
   const fingerprint = getAssetFingerprint(policyId, assetNameHex)
   const description = Array.isArray(metadata.description) ? metadata.description.join(' ') : metadata.description
