a9cca667670cf9fbce97aa51351a8d6b9df8d461
Kevin Sullivan <wolverine.ks@gmail.com>
2023-03-21 14:01:05 -0500

    WIP

--- src/yoroi-wallets/types/types.ts
+++ src/yoroi-wallets/types/types.ts
@@ -73,4 +73,9 @@ export type YoroiNft = {
   }
 }
 
+export type YoroiTarget = {
+  receiver: string
+  entry: YoroiEntry
+}
+
 export type YoroiNftModerationStatus = 'consent' | 'blocked' | 'approved' | 'pending' | 'manual_review'
