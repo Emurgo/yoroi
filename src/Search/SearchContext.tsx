3c1c27b65859894687973aa2ed61f0cfded77a0d
Javier Bueno <javierbueno@emurgo.io>
2023-03-24 18:46:32 +0000

    Search header logic refactor 2

--- src/Search/SearchContext.tsx
+++ src/Search/SearchContext.tsx
@@ -18,6 +18,18 @@ export const useSearch = () => {
   return value
 }
 
+export const useSearchResult = ({target, searchBy, sort = true}) => {
+  const {search} = useSearch()
+  const searchTermLowerCase = search.toLowerCase()
+  const filteredTarget =
+    searchTermLowerCase.length > 0 && target.length > 0
+      ? target.filter((n) => n[searchBy].toLowerCase().includes(searchTermLowerCase))
+      : target
+  const searchResult = sort ? filteredTarget.sort((a, b) => a[searchBy].localeCompare(b[searchBy])) : filteredTarget
+
+  return {searchResult}
+}
+
 export const SearchProvider = ({
   children,
   initialState,
