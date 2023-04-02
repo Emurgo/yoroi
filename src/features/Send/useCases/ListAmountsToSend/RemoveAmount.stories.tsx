770648e80f25a7f903dca32cf9a14db53604bb17
Javier Bueno <javierbueno@emurgo.io>
2023-04-02 22:42:40 +0200

    rebase

--- src/features/Send/useCases/ListAmountsToSend/RemoveAmount.stories.tsx
+++
@@ -1,7 +0,0 @@
-import {action} from '@storybook/addon-actions'
-import {storiesOf} from '@storybook/react-native'
-import React from 'react'
-
-import {RemoveAmountButton} from './RemoveAmount'
-
-storiesOf('Remove Amount Button', module).add('initial', () => <RemoveAmountButton onPress={() => action(`onPress`)} />)
