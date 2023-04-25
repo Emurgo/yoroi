50eb8e6996e18f1eaf78eb3f7468191f47114c98
stackchain <30806844+stackchain@users.noreply.github.com>
2023-04-25 18:23:08 -0400

    chore: entry - index.js

--- apps/yoroi-mobile/index.js
+++ apps/yoroi-mobile/index.js
@@ -3,9 +3,3 @@ import 'react-native-gesture-handler' // required by react-navigation
 import './global'
 import './src/i18n/polyfills' // https://formatjs.io/docs/polyfills
 import './src/index'
-
-import {AppRegistry} from 'react-native'
-import App from './src/App'
-import {name as appName} from './app.json'
-
-AppRegistry.registerComponent(appName, () => App)
