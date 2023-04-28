678dfeae62b32f9a895133b4111aa25a68730f35
Javier Bueno <javierbueno@emurgo.io>
2023-04-28 18:49:39 +0200

    update signed tx story

--- src/features/Send/useCases/SignedTx/SignedTxScreen.stories.tsx
+++ src/features/Send/useCases/SignedTx/SignedTxScreen.stories.tsx
@@ -1,19 +1,8 @@
 import {storiesOf} from '@storybook/react-native'
 import React from 'react'
 
-import {RouteProvider} from '../../../../../.storybook'
-import {SelectedWalletProvider} from '../../../../SelectedWallet'
-import {mocks} from '../../../../yoroi-wallets/mocks'
 import {SignedTxScreen} from './SignedTxScreen'
 
 storiesOf('Signed Tx Screen', module).add('initial', () => {
-  return (
-    <RouteProvider params={{id: mockTransaction.id}}>
-      <SelectedWalletProvider wallet={mocks.wallet}>
-        <SignedTxScreen />
-      </SelectedWalletProvider>
-    </RouteProvider>
-  )
+  return <SignedTxScreen />
 })
-
-const mockTransaction = Object.values(mocks.wallet.transactions)[0]
