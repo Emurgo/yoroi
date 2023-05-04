bd0c87e295757935605194af5999a5bc70eaaee4
jemubm <105349292+jemubm@users.noreply.github.com>
2023-05-04 15:09:01 +0200

    CR: update

--- src/features/Send/useCases/SignedTx/SignedTxScreen.stories.tsx
+++
@@ -1,8 +0,0 @@
-import {storiesOf} from '@storybook/react-native'
-import React from 'react'
-
-import {SignedTxScreen} from './SignedTxScreen'
-
-storiesOf('Signed Tx Screen', module).add('initial', () => {
-  return <SignedTxScreen />
-})
