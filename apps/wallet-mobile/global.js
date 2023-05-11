e411a05db2e879801763f6843fac206f4bfa8764
jemubm <105349292+jemubm@users.noreply.github.com>
2023-05-11 22:32:03 +0200

    fix(packages): bluetooth / buffer fix (#2465)
    Co-authored-by: stackchain <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/global.js
+++ apps/wallet-mobile/global.js
@@ -1,8 +1,9 @@
+import {Buffer} from '@craftzdog/react-native-buffer'
 import {decode, encode} from 'base-64'
 
-if (!global.btoa) {  global.btoa = encode }
+if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
-global.Buffer = require('safe-buffer').Buffer
+global.Buffer = Buffer
 global.process = require('process')
 global.crypto = require('react-native-crypto')
