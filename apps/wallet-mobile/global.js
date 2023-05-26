d0f236a395c2d7061abad9b386ac1d40152c0296
Michal <miso.szorad@gmail.com>
2023-05-26 21:51:10 +0100

    fix(debugger): Override buffer's subarray and remove react-native-buffer (#2479)

--- apps/wallet-mobile/global.js
+++ apps/wallet-mobile/global.js
@@ -1,8 +1,12 @@
-import {Buffer} from '@craftzdog/react-native-buffer'
+import {Buffer} from 'buffer'
 import {decode, encode} from 'base-64'
 
-if (!global.btoa) { global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+if (!global.btoa) {
+  global.btoa = encode
+}
+if (!global.atob) {
+  global.atob = decode
+}
 
 global.Buffer = Buffer
 global.process = require('process')
