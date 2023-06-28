c83cbb90cdf03a2a74719d325ef4efbd04659fb4
jemubm <105349292+jemubm@users.noreply.github.com>
2023-06-28 14:31:37 +0200

    fix: QR reader for Read-Only Wallet (#2516)
    Signed-off-by: jemubm <105349292+jemubm@users.noreply.github.com>

---
+++ apps/wallet-mobile/src/components/QRCodeScanner/QRCodeScanner.tsx
@@ -0,0 +1,35 @@
+import {BarCodeScanner} from 'expo-barcode-scanner'
+import {Camera} from 'expo-camera'
+import * as React from 'react'
+import {StyleSheet} from 'react-native'
+
+export const QRCodeScanner = ({onRead}: {onRead: ({data}: {data: string}) => void}) => {
+  const [status, requestPermissions] = Camera.useCameraPermissions()
+  const granted = status && status.granted
+
+  React.useEffect(() => {
+    if (!granted) {
+      requestPermissions()
+    }
+  }, [granted, requestPermissions])
+
+  const handleBarCodeScanned = (event) => {
+    onRead(event)
+  }
+
+  if (!granted) {
+    return null
+  }
+
+  return (
+    // expo-barcode-scanner issue in android https://github.com/expo/expo/issues/5212
+    <Camera
+      style={StyleSheet.absoluteFill}
+      ratio="16:9"
+      barCodeScannerSettings={{
+        barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+      }}
+      onBarCodeScanned={handleBarCodeScanned}
+    />
+  )
+}
--- apps/wallet-mobile/src/features/Send/useCases/StartMultiTokenTx/InputReceiver/ReadQRCodeScreen.tsx
+++ apps/wallet-mobile/src/features/Send/useCases/StartMultiTokenTx/InputReceiver/ReadQRCodeScreen.tsx
@@ -1,9 +1,7 @@
 import {useNavigation} from '@react-navigation/native'
-import {BarCodeScanner} from 'expo-barcode-scanner'
-import {Camera} from 'expo-camera'
 import * as React from 'react'
-import {StyleSheet} from 'react-native'
 
+import {QRCodeScanner} from '../../../../../components'
 import {TxHistoryRouteNavigation} from '../../../../../navigation'
 import {useSelectedWallet} from '../../../../../SelectedWallet'
 import {Quantity} from '../../../../../yoroi-wallets/types'
@@ -48,35 +46,4 @@ const getParams = (params: string) => {
     result[item[0]] = decodeURIComponent(item[1])
   })
   return result
-}
-
-const QRCodeScanner = ({onRead}: {onRead: ({data}: {data: string}) => void}) => {
-  const [status, requestPermissions] = Camera.useCameraPermissions()
-  const granted = status && status.granted
-
-  React.useEffect(() => {
-    if (!granted) {
-      requestPermissions()
-    }
-  }, [granted, requestPermissions])
-
-  const handleBarCodeScanned = ({data}) => {
-    onRead({data})
-  }
-
-  if (!granted) {
-    return null
-  }
-
-  return (
-    // expo-barcode-scanner issue in android https://github.com/expo/expo/issues/5212
-    <Camera
-      style={StyleSheet.absoluteFill}
-      ratio="16:9"
-      barCodeScannerSettings={{
-        barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
-      }}
-      onBarCodeScanned={handleBarCodeScanned}
-    />
-  )
 }
