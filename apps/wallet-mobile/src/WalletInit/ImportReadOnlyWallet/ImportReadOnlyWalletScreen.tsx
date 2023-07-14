c9d132ba5565d6f049880f9040de12ea7f3c3453
jemubm <105349292+jemubm@users.noreply.github.com>
2023-07-14 14:46:14 +0200

    Update apps/wallet-mobile/src/WalletInit/ImportReadOnlyWallet/ImportReadOnlyWalletScreen.tsx
    Co-authored-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
    Signed-off-by: jemubm <105349292+jemubm@users.noreply.github.com>

--- apps/wallet-mobile/src/WalletInit/ImportReadOnlyWallet/ImportReadOnlyWalletScreen.tsx
+++ apps/wallet-mobile/src/WalletInit/ImportReadOnlyWallet/ImportReadOnlyWalletScreen.tsx
@@ -41,7 +41,7 @@ export const ImportReadOnlyWalletScreen = () => {
       <StatusBar translucent backgroundColor="transparent" />
 
       <View style={styles.cameraContainer}>
-        <QRCodeScanner onRead={onRead} maskEnabled={false} />
+        <QRCodeScanner onRead={onRead} />
       </View>
 
       <ScrollView style={styles.scrollView}>
