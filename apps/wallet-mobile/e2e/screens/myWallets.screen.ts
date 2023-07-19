bf5681b677f81fcbe1c6263403f78fda7fe0e9dc
Denis <nebytov.d@gmail.com>
2023-07-19 13:15:53 +0300

    updated locators

--- apps/wallet-mobile/e2e/screens/myWallets.screen.ts
+++ apps/wallet-mobile/e2e/screens/myWallets.screen.ts
@@ -3,7 +3,7 @@ import { by, element } from 'detox'
 export const pageTitle = () => element(by.text('My wallets'))
 export const addWalletButton = () => element(by.id('addWalletOnHaskellShelleyButton'))
 export const addWalletByronButton = () => element(by.id('addWalletOnByronButton'))
-export const addWalletTestnetButton = () => element(by.id('addWalletButtonShelleyTestnet'))
+export const addWalletTestnetButton = () => element(by.id('addWalletPreprodShelleyButton'))
 export const walletByNameButton = (walletName: string) => element(by.text(walletName))
 export const createWalletButton = () => element(by.id('createWalletButton'))
 export const restoreWalletButton = () => element(by.id('restoreWalletButton'))
