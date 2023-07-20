0cc116e3a12c0e1aa85ccc7c4f0295bc0f687b33
Javier Bueno <javierbueno@emurgo.io>
2023-07-20 11:16:06 +0200

    fix tsc

--- apps/wallet-mobile/src/TxHistory/PairedBalance.stories.tsx
+++ apps/wallet-mobile/src/TxHistory/PairedBalance.stories.tsx
@@ -55,7 +55,7 @@ storiesOf('PairedBalance', module)
         >
           <View style={{flex: 1, justifyContent: 'center'}}>
             <View style={{borderWidth: 1}}>
-              <PairedBalance amount={{quantity: '2', tokenId: mocks.wallet.primaryTokenInfo.id}} privacy />
+              <PairedBalance amount={{quantity: '2', tokenId: mocks.wallet.primaryTokenInfo.id}} privacyMode="HIDDEN" />
             </View>
           </View>
         </SelectedWalletProvider>
