af21175e31af959ea6da76e012850d7cad1e7cae
Javier Bueno <javierbueno@emurgo.io>
2023-07-25 11:47:07 +0200

    Rebuilding history when after send tx submit fail

--- apps/wallet-mobile/src/features/Send/common/navigation.ts
+++ apps/wallet-mobile/src/features/Send/common/navigation.ts
@@ -31,7 +31,7 @@ export const useNavigateTo = () => {
                       {
                         name: 'history',
                         state: {
-                          routes: [{name: 'send-start-tx'}],
+                          routes: [{name: 'history-list'}, {name: 'send-start-tx'}],
                         },
                       },
                     ],
