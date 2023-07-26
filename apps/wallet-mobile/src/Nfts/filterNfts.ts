d029d640b14362c449d6abfabba38bcb8c920d93
jorbuedo <jorbuedo@users.noreply.github.com>
2023-07-26 15:03:05 +0200

    chore: Added analytics (feature flag) (#2532)
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
    Co-authored-by: stackchain <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/src/Nfts/filterNfts.ts
+++ apps/wallet-mobile/src/Nfts/filterNfts.ts
@@ -1,3 +1,6 @@
+import React from 'react'
+
+import {useMetrics} from '../metrics/metricsManager'
 import {TokenInfo} from '../yoroi-wallets/types'
 
 export const filterNfts = (searchTerm: string, nfts: TokenInfo[]): TokenInfo[] => {
@@ -5,4 +8,19 @@ export const filterNfts = (searchTerm: string, nfts: TokenInfo[]): TokenInfo[] =
   const filteredNfts =
     searchTermLowerCase.length > 0 ? nfts.filter((nft) => nft.name?.toLowerCase().includes(searchTermLowerCase)) : nfts
   return filteredNfts
+}
+
+export const useTrackNftGallerySearchActivated = (searchTerm: string, resultCount: number) => {
+  const {track} = useMetrics()
+
+  React.useEffect(() => {
+    if (!searchTerm) return
+
+    const debounce = setTimeout(
+      () => track.nftGallerySearchActivated({nft_search_term: searchTerm, nft_count: resultCount}),
+      500,
+    )
+
+    return () => clearTimeout(debounce)
+  }, [searchTerm, resultCount, track])
 }
