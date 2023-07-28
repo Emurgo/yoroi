ef616834e941ce3521199fa4b8d123bf35131010
SorinC6 <kish.sorin@yahoo.com>
2023-07-28 15:37:03 +0300

    removed unused import

--- apps/wallet-mobile/src/features/Swap/useCases/ConfirmationOrderScreen/ConfirmationOrderScreen.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/ConfirmationOrderScreen/ConfirmationOrderScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {StyleSheet, TextInput as RNTextInput, TouchableOpacity, View, ViewProps} from 'react-native'
 
 import {Button, Icon, Spacer, Text, TextInput} from '../../../../components'
