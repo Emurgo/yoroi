e26a20bba35802be409c134a33577507e9131fdf
stackchain <30806844+stackchain@users.noreply.github.com>
2023-08-02 14:56:41 +0100

    chore: swap mocks

--- packages/react-swap/src/translators/swapManager.mocks.ts
+++ packages/react-swap/src/translators/swapManager.mocks.ts
@@ -20,11 +20,101 @@ const createOrderResponse: Swap.CreateOrderResponse = {
   contractAddress: 'some-address',
 }
 const cancelOrderResponse: string = 'cbor'
-const listOrdersByStatusOpenResponse: Swap.OpenOrder[] = []
+const listOrdersByStatusOpenResponse: Swap.OpenOrder[] = [
+  {
+    provider: 'minswap',
+    deposit: {
+      quantity: '0',
+      tokenId: '',
+    },
+    from: {
+      quantity: '0',
+      tokenId: '',
+    },
+    to: {
+      quantity: '0',
+      tokenId: '',
+    },
+    utxo: 'utxo',
+  },
+]
 
-const listPoolsByPairResponse: Swap.Pool[] = []
+const listPoolsByPairResponse: Swap.Pool[] = [
+  {
+    poolId: 'pool-id',
+    tokenA: {
+      quantity: '0',
+      tokenId: '',
+    },
+    tokenB: {
+      quantity: '0',
+      tokenId: '',
+    },
+    provider: 'minswap',
+    lastUpdate: '1690983915581',
+    price: 1.1234456789,
+    batcherFee: {
+      quantity: '0',
+      tokenId: '',
+    },
+    deposit: {
+      quantity: '0',
+      tokenId: '',
+    },
+    fee: {
+      quantity: '0',
+      tokenId: '',
+    },
+    lpToken: {
+      quantity: '0',
+      tokenId: '',
+    },
+  },
+]
 
-const listPairsByTokenResponse: Balance.Token[] = []
+const listPairsByTokenResponse: Balance.Token[] = [
+  {
+    info: {
+      name: 'some-name',
+      decimals: 6,
+      symbol: 'some-symbol',
+      description: 'some-description',
+      fingerprint: 'some-fingerprint',
+      group: 'some-group',
+      icon: 'some-icon',
+      id: '.',
+      image: 'some-image',
+      kind: 'ft',
+      metadatas: {},
+      ticker: 'some-ticker',
+    },
+    status: 'verified',
+    price: {
+      askPrice: 1.2,
+      bidPrice: 1.3,
+      baseDecimalPlaces: 6,
+      price: 1.4,
+      price10d: [1.2, 1.3],
+      priceChange: {
+        '24h': 1.5,
+        '7d': 1.6,
+      },
+      quoteDecimalPlaces: 6,
+      volume: {
+        base: 1.7,
+        quote: 1.8,
+      },
+      volumeChange: {
+        base: 1.9,
+        quote: 2.0,
+      },
+    },
+    supply: {
+      circulating: '1.1',
+      total: '1.2',
+    },
+  },
+]
 
 // API FUNCTIONS
 const createOrder = {
