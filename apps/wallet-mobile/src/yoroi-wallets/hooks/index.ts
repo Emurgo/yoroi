94d3ecf45e22d67ae04f7f70829e0c83e9a826f4
Michal <miso.szorad@gmail.com>
2023-08-04 23:17:25 +0100

    fix(sentry): Restore sentry (#2588)

--- apps/wallet-mobile/src/yoroi-wallets/hooks/index.ts
+++ apps/wallet-mobile/src/yoroi-wallets/hooks/index.ts
@@ -14,7 +14,7 @@ import {
   UseQueryOptions,
 } from 'react-query'
 
-import {isNightly} from '../../legacy/config'
+import {CONFIG} from '../../legacy/config'
 import {useWalletManager} from '../../WalletManager'
 import {calcLockedDeposit} from '../cardano/assetUtils'
 import {generateShelleyPlateFromKey} from '../cardano/shelley/plate'
@@ -38,9 +38,8 @@ import {WalletManager, WalletMeta} from '../walletManager'
 const crashReportsStorageKey = 'sendCrashReports'
 
 export const getCrashReportsEnabled = async (storage: AsyncStorageStatic = AsyncStorage) => {
-  if (isNightly()) return true
-
   const data = await storage.getItem(crashReportsStorageKey)
+  if (CONFIG.FORCE_CRASH_REPORTS) return true
   return parseBoolean(data) ?? false
 }
 
