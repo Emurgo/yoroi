4c2f93151a99b6a63c9019c3e79e7d64937f57cc
Javier Bueno <javierbueno@emurgo.io>
2023-08-11 13:16:37 +0200

    CR: update

--- apps/wallet-mobile/src/features/Initialization/InitialScreen/InitialScreen.tsx
+++ apps/wallet-mobile/src/features/Initialization/InitialScreen/InitialScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {StyleSheet, Text, TouchableOpacity} from 'react-native'
+import {Platform, StyleSheet, Text, TouchableOpacity} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {Button, Icon, Spacer, StatusBar, TextInput, YoroiLogo} from '../../../components'
@@ -98,15 +98,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#242838',
   },
-  input: {
-    color: '#6B7384',
-    fontWeight: '400',
-    paddingLeft: 8,
-    borderColor: '#A7AFC0',
-    borderWidth: 1,
-    borderRadius: 10,
-    justifyContent: 'center',
-  },
+  input: Platform.select({
+    android: {
+      color: '#6B7384',
+      fontWeight: '400',
+      paddingLeft: 8,
+      justifyContent: 'center',
+      borderColor: '#A7AFC0',
+      borderWidth: 1,
+      borderRadius: 10,
+    },
+    default: {
+      color: '#6B7384',
+      fontWeight: '400',
+      paddingLeft: 8,
+      justifyContent: 'center',
+    },
+  }),
+
   checkboxText: {
     fontFamily: 'Rubik',
     fontSize: 16,
