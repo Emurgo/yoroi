1093c368f147eadbe98d375d6ff432fa3de7ba3d
Michal <miso.szorad@gmail.com>
2023-08-14 12:08:52 +0100

    Add story

---
+++ apps/wallet-mobile/src/features/ToggleAnalyticsSettings/ToggleAnalyticsSettings.stories.tsx
@@ -0,0 +1,5 @@
+import {storiesOf} from '@storybook/react-native'
+import React from 'react'
+import {ToggleAnalyticsSettings} from './ToggleAnalyticsSettings'
+
+storiesOf('ToggleAnalyticsSettings', module).add('Default', () => <ToggleAnalyticsSettings />)
