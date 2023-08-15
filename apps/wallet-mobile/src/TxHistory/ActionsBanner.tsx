9ad423c01dd94876ad35a22431b06baac0b8d598
Javier Bueno <javierbueno@emurgo.io>
2023-08-15 13:54:41 +0200

    action banner buttons revamp

--- apps/wallet-mobile/src/TxHistory/ActionsBanner.tsx
+++ apps/wallet-mobile/src/TxHistory/ActionsBanner.tsx
@@ -63,7 +63,7 @@ export const ActionsBanner = ({disabled = false}: {disabled: boolean}) => {
           {Boolean(features.showSwapButton) && (
             <View style={styles.centralized}>
               <TouchableOpacity
-                style={styles.actionIconNoBackground}
+                style={styles.actionIcon}
                 onPress={navigateTo.swap}
                 testID="swapButton"
                 disabled={disabled}
@@ -118,13 +118,6 @@ const styles = StyleSheet.create({
     width: 42,
     borderRadius: 20,
     backgroundColor: '#3154CB',
-  },
-  actionIconNoBackground: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 42,
-    width: 42,
-    borderRadius: 20,
   },
   actionLabel: {
     paddingTop: 8,
