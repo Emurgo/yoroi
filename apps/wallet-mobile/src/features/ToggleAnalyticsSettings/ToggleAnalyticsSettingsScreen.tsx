cb3460d479bd2ba394c40698ac63d7b5d0afb268
Javier Bueno <javierbueno@emurgo.io>
2023-08-15 12:14:46 +0200

    analytics on read more link

--- apps/wallet-mobile/src/features/ToggleAnalyticsSettings/ToggleAnalyticsSettingsScreen.tsx
+++ apps/wallet-mobile/src/features/ToggleAnalyticsSettings/ToggleAnalyticsSettingsScreen.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
-import {StyleSheet} from 'react-native'
+import {Linking, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {Analytics, StatusBar} from '../../components'
 
 export const ToggleAnalyticsSettingsScreen = () => {
+  const onReadMore = () => {
+    Linking.openURL('https://emurgohelpdesk.zendesk.com/hc/en-us/articles/7594394140303-What-s-user-insights-')
+  }
+
   return (
     <SafeAreaView edges={['left', 'right', 'bottom']} style={styles.container}>
       <StatusBar type="dark" />
 
-      <Analytics type="settings" />
+      <Analytics type="settings" onReadMore={onReadMore} />
     </SafeAreaView>
   )
 }
