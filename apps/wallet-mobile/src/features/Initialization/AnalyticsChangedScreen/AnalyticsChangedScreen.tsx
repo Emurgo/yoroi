dec5a8cc9861eb2c9dd2c3941577ea4888cda8a2
banklesss <105349292+banklesss@users.noreply.github.com>
2023-08-15 12:30:52 +0200

    chore: Added analytics "on read more" link (#2606)

--- apps/wallet-mobile/src/features/Initialization/AnalyticsChangedScreen/AnalyticsChangedScreen.tsx
+++ apps/wallet-mobile/src/features/Initialization/AnalyticsChangedScreen/AnalyticsChangedScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {StyleSheet} from 'react-native'
+import {Linking, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {Analytics, StatusBar} from '../../../components'
@@ -8,7 +8,7 @@ import {useAgreeWithLegal} from '../common'
 export const AnalyticsChangedScreen = () => {
   const {agree} = useAgreeWithLegal()
   const onReadMore = () => {
-    // TODO: Add agreement link
+    Linking.openURL('https://emurgohelpdesk.zendesk.com/hc/en-us/articles/7594394140303-What-s-user-insights-')
   }
 
   const handleClose = () => {
