438f9e0d7ce13588523cf7ab12d66339dd6b54a5
SorinC6 <kish.sorin@yahoo.com>
2023-08-18 16:42:57 +0300

    add experimental fee calculation

--- apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/EditPool/ShowPoolActions.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/EditPool/ShowPoolActions.tsx
@@ -1,6 +1,9 @@
 import {useSwap} from '@yoroi/swap'
 import React from 'react'
 
+import {useSelectedWallet} from '../../../../../../SelectedWallet'
+import {useTokenInfo} from '../../../../../../yoroi-wallets/hooks'
+import {Quantities} from '../../../../../../yoroi-wallets/utils'
 import {useNavigateTo} from '../../../../common/navigation'
 import {ExpandableInfoCard} from '../../../../common/SelectPool/ExpendableCard/ExpandableInfoCard'
 import {useStrings} from '../../../../common/strings'
@@ -9,23 +12,30 @@ export const ShowPoolActions = () => {
   const navigate = useNavigateTo()
   const strings = useStrings()
   const {createOrder} = useSwap()
-  const {selectedPool} = createOrder
+  const {selectedPool, amounts} = createOrder
+  const wallet = useSelectedWallet()
+  const buyTokenInfo = useTokenInfo({wallet, tokenId: amounts.buy.tokenId})
+  const sellTokenInfo = useTokenInfo({wallet, tokenId: amounts.sell.tokenId})
+  const tokenName = buyTokenInfo.ticker ?? buyTokenInfo.name
 
   if (selectedPool === undefined) {
     return <></>
   }
 
+  const totalAmount = Quantities.denominated(amounts.buy.quantity, buyTokenInfo.decimals ?? 0)
   const protocolCapitalize = selectedPool.provider[0].toUpperCase() + selectedPool.provider.substring(1)
+  const calculatedFee = (Number(selectedPool?.fee) / 100) * Number(createOrder.amounts.sell.quantity)
+  const poolFee = Quantities.denominated(`${calculatedFee}`, sellTokenInfo.decimals ?? 0)
 
   return (
     <ExpandableInfoCard
       label={`${protocolCapitalize} (auto)`}
-      mainInfo={[{label: 'Total 11 ADA ?'}]}
+      mainInfo={[{label: `Total ${totalAmount} ${tokenName} `}]}
       navigateTo={() => navigate.selectPool()}
       hiddenInfo={[
         {
           label: 'Min ADA',
-          value: '2 ADA', // TODO add real value
+          value: '2 ADA',
           info: strings.swapMinAda,
         },
         {
@@ -35,8 +45,7 @@ export const ShowPoolActions = () => {
         },
         {
           label: 'Fees',
-          value: selectedPool?.fee, // waiting for more clarification if show the fee or the ada value
-          info: strings.swapFees,
+          value: String(poolFee),
         },
       ]}
     />
