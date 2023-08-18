9b9e6c2a680a6df3b7a220c5cd6a14ac3b5d3039
Michal <miso.szorad@gmail.com>
2023-08-18 09:38:27 +0100

    Fix agreement reset in dev and update agreement date

--- apps/wallet-mobile/src/legacy/DeveloperScreen.tsx
+++ apps/wallet-mobile/src/legacy/DeveloperScreen.tsx
@@ -22,6 +22,8 @@ import {NetworkError} from '../yoroi-wallets/cardano/errors'
 import {generateAdaMnemonic} from '../yoroi-wallets/cardano/mnemonic'
 import {useCreateWallet} from '../yoroi-wallets/hooks'
 import {NetworkId} from '../yoroi-wallets/types'
+import {useLegalAgreement, useResetLegalAgreement} from '../features/Initialization/common'
+import {CONFIG} from './config'
 
 const routes: Array<{label: string; path: keyof AppRoutes}> = [
   {label: 'Storybook', path: 'storybook'},
@@ -71,7 +73,9 @@ export const DeveloperScreen = () => {
   })
   const [wallet] = useSelectedWalletContext()
   const [addresses, setAddresses] = React.useState('')
-  const {resetConsent, isConsentRequested} = useMetrics()
+  const agreement = useLegalAgreement()
+  const {reset: resetLegalAgreement} = useResetLegalAgreement()
+  const agreedToLegal = agreement?.latestAcceptedAgreementsDate === CONFIG.AGREEMENT_DATE
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -114,10 +118,10 @@ export const DeveloperScreen = () => {
         />
 
         <Button
-          title={`Reset Analytics Consent (${isConsentRequested})`}
+          title={`Reset Legal Agreement (Agreed: ${agreedToLegal})`}
           style={styles.button}
           onPress={() => {
-            resetConsent()
+            resetLegalAgreement()
           }}
           testID="btnResetAnalyticsConsent"
         />
