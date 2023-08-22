044cd1c2db528ef4a207e464d5a7c054d64d58fd
Javier Bueno <javierbueno@emurgo.io>
2023-08-22 10:40:33 +0200

    CR: update

--- apps/wallet-mobile/src/yoroi-wallets/cardano/getMinAmounts.test.ts
+++ apps/wallet-mobile/src/yoroi-wallets/cardano/getMinAmounts.test.ts
@@ -7,88 +7,44 @@ import {CardanoMobile} from '../wallets'
 import {getMinAmounts, withMinAmounts, withPrimaryToken} from './getMinAmounts'
 import {cardanoValueFromMultiToken, normalizeToAddress} from './utils'
 
-jest.mock('../wallets', () => ({
-  CardanoMobile: {
-    BigNum: {
-      fromStr: jest.fn(),
-    },
-    TransactionOutput: {
-      new: jest.fn(),
-    },
-    DataCost: {
-      newCoinsPerByte: jest.fn(),
-    },
-    minAdaForOutput: jest.fn(),
-  },
-}))
-
-jest.mock('./utils', () => ({
-  cardanoValueFromMultiToken: jest.fn(),
-  normalizeToAddress: jest.fn(),
-}))
-
 describe('withMinAmounts()', () => {
   beforeEach(jest.resetAllMocks)
 
   it('should returns the min amount quantity', async () => {
-    const MOCKED_MIN_AMOUNT = '28282828'
-    const MOCKED_INPUT_AMOUNT = '123'
-
     const address =
       'addr_test1qrrdv3uxj8shu27ea9djvnn3rl4w3lvh3cyck6yc36mvf6ctlqxj9g0azvpycncr9u600p6t556qhc3psk06uzzw6saq4kvdpq'
     const amounts: Balance.Amounts = {
-      [walletMocks.wallet.primaryToken.identifier]: MOCKED_INPUT_AMOUNT,
-      fakeToken1: '123',
-      fakeToken2: '123',
+      [walletMocks.wallet.primaryToken.identifier]: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
 
-    // @ts-ignore
-    normalizeToAddress.mockResolvedValue({address: 'address'})
-    // @ts-ignore
-    CardanoMobile.minAdaForOutput.mockResolvedValue({toStr: jest.fn().mockReturnValue(MOCKED_MIN_AMOUNT)})
-    // @ts-ignore
-    cardanoValueFromMultiToken.mockResolvedValue('fake-value')
-
     expect(await withMinAmounts(address, amounts, walletMocks.wallet.primaryToken)).toEqual({
-      '': MOCKED_MIN_AMOUNT,
-      fakeToken1: '123',
-      fakeToken2: '123',
+      '': '1301620',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     })
   })
 
   it('should returns the input amount quantity', async () => {
-    const MOCKED_MIN_AMOUNT = '123'
-    const MOCKED_INPUT_AMOUNT = '28282828'
-
     const address =
       'addr_test1qrrdv3uxj8shu27ea9djvnn3rl4w3lvh3cyck6yc36mvf6ctlqxj9g0azvpycncr9u600p6t556qhc3psk06uzzw6saq4kvdpq'
     const amounts: Balance.Amounts = {
-      [walletMocks.wallet.primaryToken.identifier]: MOCKED_INPUT_AMOUNT,
-      fakeToken1: '123',
-      fakeToken2: '123',
+      [walletMocks.wallet.primaryToken.identifier]: '1234432556466',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
 
-    // @ts-ignore
-    normalizeToAddress.mockResolvedValue({address: 'address'})
-    // @ts-ignore
-    CardanoMobile.minAdaForOutput.mockResolvedValue({toStr: jest.fn().mockReturnValue(MOCKED_MIN_AMOUNT)})
-    // @ts-ignore
-    cardanoValueFromMultiToken.mockResolvedValue('fake-value')
-
-    expect(await withMinAmounts(address, amounts, walletMocks.wallet.primaryToken)).toEqual({
-      '': MOCKED_INPUT_AMOUNT,
-      fakeToken1: '123',
-      fakeToken2: '123',
-    })
+    expect(await withMinAmounts(address, amounts, walletMocks.wallet.primaryToken)).toEqual(amounts)
   })
 })
 
 describe('withPrimaryToken()', () => {
   it('should returns the input amounts', () => {
     const amounts: Balance.Amounts = {
-      [walletMocks.wallet.primaryToken.identifier]: '1234',
-      fakeToken1: '123',
-      fakeToken2: '123',
+      [walletMocks.wallet.primaryToken.identifier]: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
 
     expect(withPrimaryToken(amounts, walletMocks.wallet.primaryToken)).toEqual(amounts)
@@ -96,14 +52,14 @@ describe('withPrimaryToken()', () => {
 
   it('should returns the input amounts plus primary token as 0', () => {
     const amounts: Balance.Amounts = {
-      fakeToken1: '123',
-      fakeToken2: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
 
     expect(withPrimaryToken(amounts, walletMocks.wallet.primaryToken)).toEqual({
       [walletMocks.wallet.primaryToken.identifier]: '0',
-      fakeToken1: '123',
-      fakeToken2: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     })
   })
 })
@@ -112,34 +68,27 @@ describe('getMinAmounts()', () => {
   beforeEach(jest.resetAllMocks)
 
   it('should return the mocked min amount', async () => {
-    const MOCKED_MIN_AMOUNT_RESULT = '123'
     const amounts: Balance.Amounts = {
-      [walletMocks.wallet.primaryToken.identifier]: '321',
+      [walletMocks.wallet.primaryToken.identifier]: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
     const address =
       'addr_test1qrrdv3uxj8shu27ea9djvnn3rl4w3lvh3cyck6yc36mvf6ctlqxj9g0azvpycncr9u600p6t556qhc3psk06uzzw6saq4kvdpq'
 
-    // @ts-ignore
-    normalizeToAddress.mockResolvedValue({address: 'address'})
-    // @ts-ignore
-    CardanoMobile.minAdaForOutput.mockResolvedValue({toStr: jest.fn().mockReturnValue(MOCKED_MIN_AMOUNT_RESULT)})
-    // @ts-ignore
-    cardanoValueFromMultiToken.mockResolvedValue('fake-value')
-
     expect(await getMinAmounts(address, amounts, walletMocks.wallet.primaryToken)).toEqual({
-      [walletMocks.wallet.primaryToken.identifier]: MOCKED_MIN_AMOUNT_RESULT,
+      [walletMocks.wallet.primaryToken.identifier]: '1301620',
     })
   })
 
   it('should throw an error', async () => {
-    // @ts-ignore
-    normalizeToAddress.mockResolvedValue(undefined)
-
     const amounts: Balance.Amounts = {
-      [walletMocks.wallet.primaryToken.identifier]: '321',
+      [walletMocks.wallet.primaryToken.identifier]: '123',
+      '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
+      '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
     }
 
-    const address = 'really-bad-address' // normalizeToAddress is mocked so this string does nothing
+    const address = 'really-bad-address' 
 
     const primaryToken = walletMocks.wallet.primaryToken
 
