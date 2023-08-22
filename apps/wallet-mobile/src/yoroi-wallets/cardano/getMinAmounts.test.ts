670b4b536ad131f1fb10fab889fe7f6df8b6e6bf
Javier Bueno <javierbueno@emurgo.io>
2023-08-22 10:46:45 +0200

    CR: update

--- apps/wallet-mobile/src/yoroi-wallets/cardano/getMinAmounts.test.ts
+++ apps/wallet-mobile/src/yoroi-wallets/cardano/getMinAmounts.test.ts
@@ -6,7 +6,7 @@ import {mocks as walletMocks} from '../mocks'
 import {getMinAmounts, withMinAmounts, withPrimaryToken} from './getMinAmounts'
 
 describe('withMinAmounts()', () => {
-  it('should returns the min amount quantity', async () => {
+  it('should return the min amount quantity', async () => {
     const address =
       'addr_test1qrrdv3uxj8shu27ea9djvnn3rl4w3lvh3cyck6yc36mvf6ctlqxj9g0azvpycncr9u600p6t556qhc3psk06uzzw6saq4kvdpq'
     const amounts: Balance.Amounts = {
@@ -22,7 +22,7 @@ describe('withMinAmounts()', () => {
     })
   })
 
-  it('should returns the input amount quantity', async () => {
+  it('should return the input amount quantity', async () => {
     const address =
       'addr_test1qrrdv3uxj8shu27ea9djvnn3rl4w3lvh3cyck6yc36mvf6ctlqxj9g0azvpycncr9u600p6t556qhc3psk06uzzw6saq4kvdpq'
     const amounts: Balance.Amounts = {
@@ -36,7 +36,7 @@ describe('withMinAmounts()', () => {
 })
 
 describe('withPrimaryToken()', () => {
-  it('should returns the input amounts', () => {
+  it('should return the input amounts', () => {
     const amounts: Balance.Amounts = {
       [walletMocks.wallet.primaryToken.identifier]: '123',
       '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
@@ -46,7 +46,7 @@ describe('withPrimaryToken()', () => {
     expect(withPrimaryToken(amounts, walletMocks.wallet.primaryToken)).toEqual(amounts)
   })
 
-  it('should returns the input amounts plus primary token as 0', () => {
+  it('should return the input amounts plus primary token as 0', () => {
     const amounts: Balance.Amounts = {
       '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
       '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e': '55',
@@ -61,7 +61,7 @@ describe('withPrimaryToken()', () => {
 })
 
 describe('getMinAmounts()', () => {
-  it('should return the mocked min amount', async () => {
+  it('should return the min amount', async () => {
     const amounts: Balance.Amounts = {
       [walletMocks.wallet.primaryToken.identifier]: '123',
       '698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950': '12',
