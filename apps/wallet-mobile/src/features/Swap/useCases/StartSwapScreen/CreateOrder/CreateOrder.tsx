1a18fe040d21985e9a11a4b841804f840c1d0695
Javier Bueno <javierbueno@emurgo.io>
2023-09-21 15:51:08 +0200

    fix(swap): scrolls

--- apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/CreateOrder.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/CreateOrder.tsx
@@ -176,7 +176,7 @@ export const CreateOrder = () => {
 
   return (
     <View style={styles.root}>
-      <ScrollView style={{paddingHorizontal: 16}}>
+      <ScrollView style={styles.scroll}>
         <View style={styles.container}>
           <LimitPriceWarning
             open={showLimitPriceWarning}
@@ -229,6 +229,9 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     backgroundColor: COLORS.WHITE,
+  },
+  scroll: {
+    paddingHorizontal: 16,
   },
   container: {
     flex: 1,
