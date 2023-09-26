6c4e4eb689db9af08deaf71c4e2b5370f13d840c
jorbuedo <jorbuedo@gmail.com>
2023-09-26 12:44:38 +0200

    Fix slippage

--- packages/swap/src/helpers/orders/getQuantityWithSlippage.ts
+++ packages/swap/src/helpers/orders/getQuantityWithSlippage.ts
@@ -1,13 +1,16 @@
+import {Balance} from '@yoroi/types'
 import {ceilDivision} from '../../utils/ceilDivision'
+import {asQuantity} from '../../utils/asQuantity'
 
 export const getQuantityWithSlippage = (
-  quantity: bigint,
-  slippage: bigint,
-): bigint => {
+  quantity: Balance.Quantity,
+  slippage: number,
+): Balance.Quantity => {
+  const q = BigInt(quantity)
   const slippageAmount = ceilDivision(
-    BigInt(1000) * slippage * quantity,
-    BigInt(100 * 1000),
+    BigInt(1000 * Math.floor(100 * slippage)) * q,
+    BigInt(100 * 100 * 1000),
   )
 
-  return quantity - slippageAmount
+  return asQuantity(`${q - slippageAmount}`)
 }
