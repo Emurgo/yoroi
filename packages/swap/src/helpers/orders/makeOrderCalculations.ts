5b434634164fcbdf60c1e6db93167f7892b886d3
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2023-10-02 22:33:23 +0100

    wip: frontend fee + lptoken

--- packages/swap/src/helpers/orders/makeOrderCalculations.ts
+++ packages/swap/src/helpers/orders/makeOrderCalculations.ts
@@ -6,17 +6,20 @@ import {
 import {getQuantityWithSlippage} from './getQuantityWithSlippage'
 import {Quantities} from '../../utils/quantities'
 import {getLiquidityProviderFee} from './getLiquidityProviderFee'
+import {getFrontendFee} from './getFrontendFee'
 
 export const makeOrderCalculations = ({
   orderData,
   pools,
   primaryTokenId,
   action,
+  lpTokenHeld,
 }: Readonly<{
   orderData: Swap.CreateOrderData
   pools: Array<Swap.Pool>
   sellAmount: Balance.Amount
   buyAmount: Balance.Amount
+  lpTokenHeld: Balance.Amount
   slippage: number
   primaryTokenId: Balance.TokenInfo['id']
   action:
@@ -31,28 +34,37 @@ export const makeOrderCalculations = ({
   const result: Array<SwapOrderCalulation> = []
 
   pools.forEach((pool) => {
-    let orderCalculation: Writable<SwapOrderCalulation>
+    const buyAmountWithSlippage: Balance.Amount = {
+      quantity: getQuantityWithSlippage(
+        orderData.amounts.buy.quantity,
+        orderData.slippage,
+      ),
+      tokenId: orderData.amounts.buy.tokenId,
+    }
 
-    orderCalculation = {
+    const liquidityFee: Balance.Amount = getLiquidityProviderFee(
+      pool.fee,
+      orderData.amounts.sell,
+    )
+
+    const frontendFeeInfo = getFrontendFee({
+      sell: orderData.amounts.sell,
+      buy: orderData.amounts.buy,
+      lpTokenHeld,
+      primaryTokenId: primaryTokenId,
+      // TODO: implement after adding the pair to ADA in the state
+      // sellInPrimaryTokenValue: orderData.sellInPrimaryTokenValue,
+      // buyInPrimaryTokenValue: orderData.buyInPrimaryTokenValue,
+    })
+
+    const orderCalculation: SwapOrderCalulation = {
       cost: {
         batcherFee: pool.batcherFee,
         deposit: pool.deposit,
-        frontendFeeInfo: {
-          fee: {
-            tokenId: primaryTokenId,
-            quantity: Quantities.zero,
-          },
-          tier: undefined,
-        },
-        liquidityFee: getLiquidityProviderFee(pool.fee, orderData.amounts.sell),
+        frontendFeeInfo,
+        liquidityFee,
       },
-      buyAmountWithSlippage: {
-        quantity: getQuantityWithSlippage(
-          orderData.amounts.buy.quantity,
-          orderData.slippage,
-        ),
-        tokenId: orderData.amounts.buy.tokenId,
-      },
+      buyAmountWithSlippage,
       sell: {
         price: '',
         priceDifference: '',
