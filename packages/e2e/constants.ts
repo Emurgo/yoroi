39f18d4d0bfb71bbd318b7e2dad1f9888193fb8e
Rahul <rahulnair87@gmail.com>
2023-10-16 12:38:40 +0530

    refactor e2e package

--- packages/e2e/constants.ts
+++ packages/e2e/constants.ts
@@ -1,6 +1,6 @@
 export enum WalletType {
-  NormalWallet,
-  DaedalusWallet,
+ NormalWallet,
+ DaedalusWallet,
 }
 
 /**
@@ -10,10 +10,10 @@ export enum WalletType {
  * @property {WalletType} type  a 15-word wallet or a 24-word wallet
  */
 type RestoredWallet = {
-  checksum: string
-  name: string
-  phrase: string[]
-  type: WalletType
+ checksum: string
+ name: string
+ phrase: string[]
+ type: WalletType
 }
 
 export const valid_Pin = '123456'
@@ -22,60 +22,64 @@ export const ledger_Wallet_Name = 'Test Ledger'
 export const spending_Password = '1234567890'
 export const ada_Token = 'ADA'
 export const tada_Token = 'TADA'
-export const stake_Pool_Id = 'fe662c24cf56fb98626161f76d231ac50ab7b47dd83986a30c1d4796'
+export const stake_Pool_Id =
+ 'fe662c24cf56fb98626161f76d231ac50ab7b47dd83986a30c1d4796'
 export const app_Id = 'com.emurgo.nightly'
 export const app_Id_Parent = 'com.emurgo.*'
 export const normal_15_Word_Wallet: RestoredWallet = {
-  checksum: 'CONL-2085',
-  name: 'RTW-15-word',
-  phrase: [
-    'ritual',
-    'nerve',
-    'sweet',
-    'social',
-    'level',
-    'pioneer',
-    'cream',
-    'artwork',
-    'material',
-    'three',
-    'thumb',
-    'melody',
-    'zoo',
-    'fence',
-    'east',
-  ],
-  type: WalletType.NormalWallet,
+ checksum: 'CONL-2085',
+ name: 'RTW-15-word',
+ phrase: [
+  'ritual',
+  'nerve',
+  'sweet',
+  'social',
+  'level',
+  'pioneer',
+  'cream',
+  'artwork',
+  'material',
+  'three',
+  'thumb',
+  'melody',
+  'zoo',
+  'fence',
+  'east',
+ ],
+ type: WalletType.NormalWallet,
 }
 export const normal_24_Word_Wallet: RestoredWallet = {
-  name: 'RTW-24-word',
-  checksum: 'CCPL-3231',
-  phrase: [
-    'like',
-    'project',
-    'body',
-    'tribe',
-    'track',
-    'wheat',
-    'noble',
-    'blur',
-    'reflect',
-    'tomorrow',
-    'beach',
-    'document',
-    'market',
-    'enforce',
-    'clever',
-    'submit',
-    'gorilla',
-    'hockey',
-    'can',
-    'surge',
-    'fossil',
-    'asthma',
-    'salmon',
-    'cry',
-  ],
-  type: WalletType.DaedalusWallet,
+ name: 'RTW-24-word',
+ checksum: 'CCPL-3231',
+ phrase: [
+  'like',
+  'project',
+  'body',
+  'tribe',
+  'track',
+  'wheat',
+  'noble',
+  'blur',
+  'reflect',
+  'tomorrow',
+  'beach',
+  'document',
+  'market',
+  'enforce',
+  'clever',
+  'submit',
+  'gorilla',
+  'hockey',
+  'can',
+  'surge',
+  'fossil',
+  'asthma',
+  'salmon',
+  'cry',
+ ],
+ type: WalletType.DaedalusWallet,
 }
-export const RESTORED_WALLETS: RestoredWallet[] = [normal_15_Word_Wallet, normal_24_Word_Wallet]
+export const RESTORED_WALLETS: RestoredWallet[] = [
+ normal_15_Word_Wallet,
+ normal_24_Word_Wallet,
+]
--- packages/e2e/tests/_android/restore-wallet.test.ts
+++ packages/e2e/tests/_android/restore-wallet.test.ts
@@ -6,43 +6,55 @@ import * as restoreWalletFlow from '../../screens/restoreWalletFlow.screen'
 import * as utils from '../../utils'
 
 describe('Restore a wallet', () => {
-  let platform: string
+ let platform: string
 
-  beforeAll(async () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const vars: any = await utils.initialize()
-    platform = vars.platform
-    await device.launchApp({newInstance: true})
-    await utils.prepareApp(constants.valid_Pin)
-  })
+ beforeAll(async () => {
+  const vars: any = await utils.initialize()
+  platform = vars.platform
+  await device.launchApp({newInstance: true})
+  await utils.prepareApp(constants.valid_Pin)
+ })
 
-  it('should be able to initiate the "restore wallet" process from home screen', async () => {
-    await utils.takeScreenshot('Home Screen')
-    await myWalletsScreen.addWalletTestnetButton().tap()
-    await myWalletsScreen.restoreWalletButton().tap()
-    await restoreWalletFlow.restoreNormalWalletButton().tap()
-  })
+ it('should be able to initiate the "restore wallet" process from home screen', async () => {
+  await utils.takeScreenshot('Home Screen')
+  await myWalletsScreen.addWalletTestnetButton().tap()
+  await myWalletsScreen.restoreWalletButton().tap()
+  await restoreWalletFlow.restoreNormalWalletButton().tap()
+ })
 
-  it('should be able to enter the 15-word recovery phrase', async () => {
-    await utils.enterRecoveryPhrase(constants.normal_15_Word_Wallet.phrase, platform)
-    await utils.takeScreenshot('Recovery Phrase entered')
-    await restoreWalletFlow.mnemonicRestoreWalletButton().tap()
+ it('should be able to enter the 15-word recovery phrase', async () => {
+  await utils.enterRecoveryPhrase(
+   constants.normal_15_Word_Wallet.phrase,
+   platform,
+  )
+  await utils.takeScreenshot('Recovery Phrase entered')
+  await restoreWalletFlow.mnemonicRestoreWalletButton().tap()
 
-    await expect(restoreWalletFlow.walletChecksumText()).toBeVisible()
-    await expect(restoreWalletFlow.walletChecksumText()).toHaveText(constants.normal_15_Word_Wallet.checksum)
-    await restoreWalletFlow.verifyWalletContinueButton().tap()
-    await expect(restoreWalletFlow.credentialsView()).toBeVisible()
-  })
+  await expect(restoreWalletFlow.walletChecksumText()).toBeVisible()
+  await expect(restoreWalletFlow.walletChecksumText()).toHaveText(
+   constants.normal_15_Word_Wallet.checksum,
+  )
+  await restoreWalletFlow.verifyWalletContinueButton().tap()
+  await expect(restoreWalletFlow.credentialsView()).toBeVisible()
+ })
 
-  it('should be able to set the spending password', async () => {
-    await restoreWalletFlow.walletNameInput().typeText(constants.normal_15_Word_Wallet.name)
-    await device.disableSynchronization()
-    await restoreWalletFlow.spendingPasswordInput().typeText(constants.spending_Password)
-    await restoreWalletFlow.repeatSpendingPasswordInput().typeText(`${constants.spending_Password}\n`)
-    await device.enableSynchronization()
-    await restoreWalletFlow.credentialsContinueButton().tap()
-    await expect(myWalletsScreen.pageTitle()).toBeVisible()
-    await expect(myWalletsScreen.walletByNameButton(constants.normal_15_Word_Wallet.name)).toBeVisible()
-    await utils.takeScreenshot(`Wallet "${constants.wallet_Name} is added.`)
-  })
+ it('should be able to set the spending password', async () => {
+  await restoreWalletFlow
+   .walletNameInput()
+   .typeText(constants.normal_15_Word_Wallet.name)
+  await device.disableSynchronization()
+  await restoreWalletFlow
+   .spendingPasswordInput()
+   .typeText(constants.spending_Password)
+  await restoreWalletFlow
+   .repeatSpendingPasswordInput()
+   .typeText(`${constants.spending_Password}\n`)
+  await device.enableSynchronization()
+  await restoreWalletFlow.credentialsContinueButton().tap()
+  await expect(myWalletsScreen.pageTitle()).toBeVisible()
+  await expect(
+   myWalletsScreen.walletByNameButton(constants.normal_15_Word_Wallet.name),
+  ).toBeVisible()
+  await utils.takeScreenshot(`Wallet "${constants.wallet_Name} is added.`)
+ })
 })
