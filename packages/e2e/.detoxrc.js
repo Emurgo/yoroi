3acce0542e2e1cdc78736a99633bf52882634817
Rahul <rahulnair87@gmail.com>
2023-10-17 23:55:32 +0530

    set up android emulator

--- packages/e2e/.detoxrc.js
+++ packages/e2e/.detoxrc.js
@@ -66,7 +66,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3_e2e'
+        avdName: 'Pixel_6' // Make sure to use API level 31 while creating emulator on Android Studio 
       }
     }
   },
