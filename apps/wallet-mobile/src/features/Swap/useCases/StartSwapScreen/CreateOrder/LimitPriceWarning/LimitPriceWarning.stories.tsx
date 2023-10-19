4a630d6ea8e4757ac576d71b0162f0f45a49ba51
Javier Bueno <javierbueno@emurgo.io>
2023-10-19 05:32:00 +0200

    refactor(swap): limit price warning modal

--- apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/LimitPriceWarning/LimitPriceWarning.stories.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/LimitPriceWarning/LimitPriceWarning.stories.tsx
@@ -1,7 +1,10 @@
 import {storiesOf} from '@storybook/react-native'
 import {mockSwapManager, SwapProvider} from '@yoroi/swap'
 import React from 'react'
+import {Button, StyleSheet, View} from 'react-native'
 
+import {useModal} from '../../../../../../components'
+import {useStrings} from '../../../../../../features/Swap/common/strings'
 import {SelectedWalletProvider} from '../../../../../../SelectedWallet'
 import {mocks as walletMocks} from '../../../../../../yoroi-wallets/mocks'
 import {SwapFormProvider} from '../../../../common/SwapFormProvider'
@@ -10,11 +13,20 @@ import {LimitPriceWarning} from './LimitPriceWarning'
 storiesOf('LimitPriceWarning', module).add('Initial', () => <Initial />)
 
 const Initial = () => {
+  const {openModal, content, closeModal} = useModal()
+  const strings = useStrings()
   return (
     <SelectedWalletProvider wallet={walletMocks.wallet}>
       <SwapProvider swapManager={mockSwapManager}>
         <SwapFormProvider>
-          <LimitPriceWarning open />
+          <View style={{...StyleSheet.absoluteFillObject}}>
+            <Button
+              title="Open Modal"
+              onPress={() => {
+                content !== undefined ? closeModal() : openModal(strings.limitPriceWarningTitle, <LimitPriceWarning />)
+              }}
+            />
+          </View>
         </SwapFormProvider>
       </SwapProvider>
     </SelectedWalletProvider>
