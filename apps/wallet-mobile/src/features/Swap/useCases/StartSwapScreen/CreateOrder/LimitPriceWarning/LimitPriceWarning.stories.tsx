392a6108195b396c0043f552f9ef50da9a705d3e
Javier Bueno <javierbueno@emurgo.io>
2023-10-26 18:38:59 +0200

    fix(swap): modals

--- apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/LimitPriceWarning/LimitPriceWarning.stories.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/LimitPriceWarning/LimitPriceWarning.stories.tsx
@@ -7,14 +7,16 @@ import {useModal} from '../../../../../../components'
 import {useStrings} from '../../../../../../features/Swap/common/strings'
 import {SelectedWalletProvider} from '../../../../../../SelectedWallet'
 import {mocks as walletMocks} from '../../../../../../yoroi-wallets/mocks'
+import {mocks} from '../../../../common/mocks'
 import {SwapFormProvider} from '../../../../common/SwapFormProvider'
 import {LimitPriceWarning} from './LimitPriceWarning'
 
-storiesOf('LimitPriceWarning', module).add('Initial', () => <Initial />)
+storiesOf('Swap Limit Price Warning', module).add('Initial', () => <Initial />)
 
 const Initial = () => {
   const {openModal, content, closeModal} = useModal()
   const strings = useStrings()
+  const {orderData} = mocks.confirmTx
   return (
     <SelectedWalletProvider wallet={walletMocks.wallet}>
       <SwapProvider swapManager={mockSwapManager}>
@@ -23,7 +25,14 @@ const Initial = () => {
             <Button
               title="Open Modal"
               onPress={() => {
-                content !== undefined ? closeModal() : openModal(strings.limitPriceWarningTitle, <LimitPriceWarning />)
+                content !== undefined
+                  ? closeModal()
+                  : openModal(
+                      strings.limitPriceWarningTitle,
+                      <SelectedWalletProvider wallet={walletMocks.wallet}>
+                        <LimitPriceWarning orderData={orderData} />
+                      </SelectedWalletProvider>,
+                    )
               }}
             />
           </View>
