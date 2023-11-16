cf95d1859a29932ea1052ee8a8a509853a3c2c4e
Michal <miso.szorad@gmail.com>
2023-11-16 14:35:13 +0000

    Add basic test

--- packages/staking/src/governance/manager.test.ts
+++ packages/staking/src/governance/manager.test.ts
@@ -1,9 +1,24 @@
 import {describe, it, expect} from '@jest/globals'
 
 import {createGovernanceManager} from './manager'
+import {init} from '@emurgo/cross-csl-nodejs'
 
 describe('createGovernanceManager', () => {
+  const cardano = init('global')
+  const options = {networkId: 1, cardano}
+
   it('should return a manager', () => {
-    expect(createGovernanceManager({networkId: 1})).toBeDefined()
+    expect(createGovernanceManager(options)).toBeDefined()
+  })
+
+  describe('validateDrepKey', () => {
+    const governanceManager = createGovernanceManager(options)
+    const invalid = 'drep1abc'
+
+    it('should result with invalid status for an invalid drep key string', async () => {
+      expect(await governanceManager.validateDrepKey(invalid)).toMatchObject({
+        status: 'invalid',
+      })
+    })
   })
 })
--- packages/staking/src/governance/manager.ts
+++ packages/staking/src/governance/manager.ts
@@ -1,17 +1,38 @@
 import {YoroiUnsignedTx} from '@yoroi/wallet-mobile/src/yoroi-wallets/types'
 import {DRep, DRepCredential} from './types'
+import {CardanoTypes} from '../cardanoMobile'
+import {assertIsBech32WithPrefix} from '@cardano-sdk/util'
 
 type Config = {
   networkId: number
+  cardano: CardanoTypes.Wasm
 }
 
 type GovernanceManager = {
   getDReps: () => Promise<DRep[]>
+  validateDrepKey: (
+    drepKey: string,
+  ) => Promise<{status: 'valid'} | {status: 'invalid'; reason: string}>
   delegateToDrep: (drep: DRepCredential) => Promise<YoroiUnsignedTx>
   voteAbstain: (drep: DRepCredential) => Promise<YoroiUnsignedTx>
   voteNoConfidence: (drep: DRepCredential) => Promise<YoroiUnsignedTx>
 }
 
 export const createGovernanceManager = (_config: Config): GovernanceManager => {
-  return {} as any
+  const validateDrepKey = async (drepKey: string) => {
+    try {
+      assertIsBech32WithPrefix(drepKey, 'drep')
+    } catch (e) {
+      console.log(e)
+      return {
+        status: 'invalid',
+        reason: 'Drep key is not a valid bech32 address',
+      }
+    }
+
+    return {status: 'valid'}
+
+    // needs blockchain check as well
+  }
+  return {validateDrepKey} as any
 }
