60999b9abf0822b420a821362aabfb014a0671de
Michal <miso.szorad@gmail.com>
2023-11-19 12:42:26 +0000

    Add options

--- packages/staking/src/governance/translators/react/hooks.ts
+++ packages/staking/src/governance/translators/react/hooks.ts
@@ -1,27 +1,38 @@
-import {useQuery} from 'react-query'
+import {UseMutationOptions, useQuery, UseQueryOptions} from 'react-query'
 import {useGovernance} from './context'
 import {GovernanceAction} from '../../manager'
 import {useMutationWithInvalidations} from '@yoroi/common'
 
-export const useIsValidDRepID = (id: string) => {
+export const useIsValidDRepID = (
+  id: string,
+  options: UseQueryOptions<void, Error> = {},
+) => {
   const {manager} = useGovernance()
-  return useQuery(
-    ['governanceIsValidDRepID', id],
-    async () => await manager.validateDRepID(id),
-  )
+  return useQuery({
+    queryKey: ['governanceIsValidDRepID', id],
+    queryFn: async () => await manager.validateDRepID(id),
+    ...options,
+  })
 }
 
-export const useLatestGovernanceAction = () => {
+export const useLatestGovernanceAction = (
+  options: UseQueryOptions<GovernanceAction | null, Error> = {},
+) => {
   const {manager} = useGovernance()
-  return useQuery(
-    'governanceLatestGovernanceAction',
-    async () => await manager.getLatestGovernanceAction(),
-  )
+
+  return useQuery({
+    queryKey: 'governanceLatestGovernanceAction',
+    queryFn: async () => await manager.getLatestGovernanceAction(),
+    ...options,
+  })
 }
 
-export const useUpdateLatestGovernanceAction = () => {
+export const useUpdateLatestGovernanceAction = (
+  options: UseMutationOptions<void, Error, GovernanceAction> = {},
+) => {
   const {manager} = useGovernance()
   return useMutationWithInvalidations({
+    ...options,
     mutationFn: async (action: GovernanceAction) =>
       await manager.setLatestGovernanceAction(action),
     invalidateQueries: ['governanceLatestGovernanceAction'],
