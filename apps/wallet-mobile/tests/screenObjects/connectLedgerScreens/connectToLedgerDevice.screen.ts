8066f223af5b82dde28690d8c6cf504699931566
Rahul <rahulnair87@gmail.com>
2023-11-23 00:20:52 +0530

    remove appium tests

--- apps/wallet-mobile/tests/screenObjects/connectLedgerScreens/connectToLedgerDevice.screen.ts
+++
@@ -1,9 +0,0 @@
-export const connectLedgerTitle = () => driver.$('//*[@text="Connect to Ledger Device"]')
-export const continueButton = () => driver.$('//*[@resource-id="continueButton"]')
-
-export const allowUsingLocation = () => driver.$('//*[@resource-id="com.android.permissioncontroller:id/permission_allow_foreground_only_button"]')
-export const scanningTitle = () => driver.$('//*[@text="Scanning bluetooth devices..."]')
-export const getDevices = () => driver.$$('//android.widget.ScrollView')
-
-export const walletNameInput = () => driver.$('//android.widget.EditText')
-export const saveWalletButton = () => driver.$('//*[@resource-id="saveWalletButton"]')
