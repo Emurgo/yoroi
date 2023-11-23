683323c36332b985de52dab2c4bd2bf1a45d1acf
Rahul <62512215+rahulnr7@users.noreply.github.com>
2023-11-23 16:55:16 +0530

    Remove Appium tests from app folder (#2917)

--- apps/wallet-mobile/tests/screenObjects/pinCode.screen.ts
+++
@@ -1,2 +0,0 @@
-export const getPinKey = (pinNumber: string) => driver.$(`//*[@resource-id="pinKey${pinNumber}"]`)
-export const backspaceButton = () => driver.$('//*[@resource-id="pinKey⌫"]')
--- apps/wallet-mobile/tests/screenObjects/stakingScreens/stakingCenter.screen.ts
+++
@@ -1,3 +0,0 @@
-// nightly
-export const nightlyPoolHashInput = () => driver.$('//*[@resource-id="nightlyPoolHashInput"]').$('android.widget.EditText')
-export const nightlyDelegateButton = () => driver.$('//*[@resource-id="nightlyDelegateButton"]')
