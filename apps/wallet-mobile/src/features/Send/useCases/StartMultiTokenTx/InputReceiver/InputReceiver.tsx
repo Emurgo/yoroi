2a9a137dc2e468d68741c3ab4119aa17fb7532c3
Javier Bueno <javierbueno@emurgo.io>
2023-12-05 16:40:48 +0100

    feature(resolver): frontend update

--- apps/wallet-mobile/src/features/Send/useCases/StartMultiTokenTx/InputReceiver/InputReceiver.tsx
+++ apps/wallet-mobile/src/features/Send/useCases/StartMultiTokenTx/InputReceiver/InputReceiver.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Animated, StyleSheet} from 'react-native'
-import {Defs, Path, RadialGradient, Stop, Svg} from 'react-native-svg'
+import {Animated, Easing, StyleSheet} from 'react-native'
+import {Defs, LinearGradient, Path, Stop, Svg} from 'react-native-svg'
 
 import {Icon, TextInput, TextInputProps} from '../../../../../components'
 import {useNavigateTo} from '../../../common/navigation'
@@ -21,7 +21,7 @@ export const InputReceiver = ({
         isLoading ? (
           <Loading />
         ) : isValid ? (
-          <Icon.Check size={30} color="#08C29D" />
+          <Icon.Check size={25} color="#08C29D" />
         ) : (
           <ScannerButton onPress={navigateTo.reader} />
         )
@@ -45,13 +45,16 @@ export const InputReceiver = ({
 const Loading = (props) => {
   const spin = React.useRef(new Animated.Value(0)).current
 
-  Animated.loop(
-    Animated.timing(spin, {
-      toValue: 1,
-      duration: 1500,
-      useNativeDriver: true,
-    }),
-  ).start()
+  React.useEffect(() => {
+    Animated.loop(
+      Animated.timing(spin, {
+        toValue: 1,
+        duration: 1500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start()
+  }, [spin])
 
   const getRotationStyle = () => {
     const rotate = spin.interpolate({
@@ -68,22 +71,22 @@ const Loading = (props) => {
       <Svg width={20} height={20} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <Path
           d="M1 10c-.552 0-1.005.449-.95.998A10 10 0 1010.998.05C10.448-.005 10 .448 10 1c0 .552.45.994.997 1.062a8 8 0 11-8.935 8.935C1.994 10.45 1.552 10 1 10z"
-          fill="url(#paint0_angular_12263_40263)"
+          fill="url(#paint0_linear_12263_40263)"
         />
 
         <Defs>
-          <RadialGradient
-            id="paint0_angular_12263_40263"
-            cx={0}
-            cy={0}
-            r={1}
+          <LinearGradient
+            id="paint0_linear_12263_40263"
+            x1={1.5}
+            y1={11}
+            x2={11.5}
+            y2={1.5}
             gradientUnits="userSpaceOnUse"
-            gradientTransform="matrix(-9 1.49995 -1.49995 -9 10 10)"
           >
-            <Stop offset={0.032392} stopColor="#3154CB" />
+            <Stop stopColor="#3154CB" />
 
-            <Stop offset={0.279782} stopColor="#3154CB" stopOpacity={0.3} />
-          </RadialGradient>
+            <Stop offset={1} stopColor="#3154CB" stopOpacity={0.5} />
+          </LinearGradient>
         </Defs>
       </Svg>
     </Animated.View>
