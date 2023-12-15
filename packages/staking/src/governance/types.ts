a3e3a59889df5c3d54d839bf58902862eba442c1
Michal <miso.szorad@gmail.com>
2023-12-15 19:34:53 +0000

    Improve types

--- packages/staking/src/governance/types.ts
+++ packages/staking/src/governance/types.ts
@@ -26,7 +26,7 @@ export type StakingKeyState = {
     (
       | {action: 'no-confidence'}
       | {action: 'abstain'}
-      | {action: 'drep'; drepID: string}
+      | {action: 'drep'; drepID: DRepId}
     )
 }
 
