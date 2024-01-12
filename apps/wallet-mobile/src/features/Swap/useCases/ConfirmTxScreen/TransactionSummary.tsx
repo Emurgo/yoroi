287e3e62da3aad4a047e0731c74ecfe128234ab8
Sorin Chis <kish.sorin@yahoo.com>
2024-01-12 14:34:35 +0200

    fix(swap): add specific order type on summary (#2991)

--- apps/wallet-mobile/src/features/Swap/useCases/ConfirmTxScreen/TransactionSummary.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/ConfirmTxScreen/TransactionSummary.tsx
@@ -25,7 +25,7 @@ export const TransactionSummary = () => {
   const {
     limitPrice: {displayValue: limitDisplayValue},
   } = useSwapForm()
-  const {amounts, selectedPoolCalculation: calculation} = orderData
+  const {amounts, selectedPoolCalculation: calculation, type} = orderData
   const {openModal} = useModal()
 
   // should never happen
@@ -73,17 +73,14 @@ export const TransactionSummary = () => {
       value: `${Quantities.format(cost.deposit.quantity, wallet.primaryTokenInfo.decimals ?? 0)} ${
         wallet.primaryTokenInfo.ticker
       }`,
-      info: strings.swapMinAda,
     },
     {
       label: strings.swapFeesTitle,
       value: formattedFeeText,
-      info: strings.swapFees,
     },
     {
       label: strings.swapLiqProvFee,
       value: liqFeeQuantityFormatted,
-      info: strings.swapLiquidityFeeInfo(pool.fee),
     },
     {
       label: strings.swapMinReceivedTitle,
@@ -99,10 +96,9 @@ export const TransactionSummary = () => {
           </Text>
         </View>
       ),
-      info: strings.swapMinReceived,
     },
     {
-      label: strings.marketPrice,
+      label: `${capitalize(type)} ${strings.price}`,
       value: `${limitDisplayValue} ${tokenToSellName}/${tokenToBuyName}`,
       info: strings.marketPriceInfo,
     },
