a7931b06d7f4615a56bbc0736166e7837c48879f
jorbuedo <jorbuedo@users.noreply.github.com>
2024-01-17 12:43:52 +0100

    fix(cdn): Only show load skeleton first time (#3013)

--- apps/wallet-mobile/src/yoroi-wallets/hooks/index.ts
+++ apps/wallet-mobile/src/yoroi-wallets/hooks/index.ts
@@ -1022,7 +1022,7 @@ export const useNativeAssetImage = ({
   const queryClient = useQueryClient()
 
   const [isError, setError] = React.useState(false)
-  const [isLoading, setLoading] = React.useState(false)
+  const [isLoading, setLoading] = React.useState(responseType === 'binary')
 
   const queryKey = ['native-asset-img', policy, name, responseType, `${width}x${height}`, contentFit]
 
@@ -1036,7 +1036,6 @@ export const useNativeAssetImage = ({
       const requestUrl = `https://${network}.processed-media.yoroiwallet.com/${policy}/${name}?width=${width}&height=${height}&kind=${kind}&fit=${contentFit}${cache}`
 
       if (responseType === 'binary') {
-        setLoading(true)
         setError(false)
         return requestUrl
       }
