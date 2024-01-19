53fa9dd44f39d71a3bebc4684064145e76294914
SorinC6 <kish.sorin@yahoo.com>
2024-01-19 14:27:09 +0200

    wip

--- packages/theme/src/typography.ts
+++ packages/theme/src/typography.ts
@@ -2,12 +2,27 @@ import {TextStyle} from 'react-native'
 
 import {Typography} from './types'
 
-const weight: Record<string, TextStyle> = {
+const fontFamily: Record<string, TextStyle> = {
   regular: {
     fontFamily: 'Rubik',
   },
   medium: {
     fontFamily: 'Rubik-Medium',
+  },
+}
+
+const fontScale: Record<string, TextStyle> = {
+  small: {
+    fontWeight: '300',
+  },
+  normall: {
+    fontWeight: '400',
+  },
+  big: {
+    fontWeight: '500',
+  },
+  large: {
+    fontWeight: '600',
   },
 }
 
@@ -33,15 +48,15 @@ const size: Record<string, TextStyle> = {
     lineHeight: 24,
     letterSpacing: 0.6,
   },
-  body1: {
+  large: {
     fontSize: 16,
     lineHeight: 24,
   },
-  body2: {
+  normal: {
     fontSize: 14,
     lineHeight: 22,
   },
-  body3: {
+  small: {
     fontSize: 12,
     lineHeight: 18,
   },
@@ -53,126 +68,132 @@ const size: Record<string, TextStyle> = {
 
 export const typography: Typography = {
   'heading-1-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.h1,
   },
   'heading-1-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.h1,
   },
   'heading-2-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.h2,
   },
   'heading-2-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.h2,
   },
   'heading-3-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.h3,
   },
   'heading-3-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.h3,
   },
   'heading-4-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.h4,
   },
   'heading-4-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.h4,
   },
   'heading-5-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.h5,
     textTransform: 'uppercase',
   },
   'heading-5-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.h5,
     textTransform: 'uppercase',
   },
-  'body-1-medium': {
-    ...weight.medium,
-    ...size.body1,
+  'body-small-medium': {
+    ...fontFamily.medium,
+    ...size.small,
+    // font scale addded here,
   },
-  'body-1-regular': {
-    ...weight.regular,
-    ...size.body1,
+  'body-small-regular': {
+    ...fontFamily.regular,
+    ...size.small,
+    // font scale addded here,
   },
-  'body-2-medium': {
-    ...weight.medium,
-    ...size.body2,
+  'body-normal-medium': {
+    ...fontFamily.medium,
+    ...size.normal,
+    // font scale addded here,
   },
-  'body-2-regular': {
-    ...weight.regular,
-    ...size.body2,
+  'body-normal-regular': {
+    ...fontFamily.regular,
+    ...size.normal,
+    // font scale addded here,
   },
-  'body-3-medium': {
-    ...weight.medium,
-    ...size.body3,
+  'body-large-medium': {
+    ...fontFamily.medium,
+    ...size.large,
+    // font scale addded here,
   },
-  'body-3-regular': {
-    ...weight.regular,
-    ...size.body3,
+  'body-large-regular': {
+    ...fontFamily.regular,
+    ...size.large,
+    // font scale addded here,
   },
-  'button-1': {
-    ...weight.medium,
+  'button-large': {
+    ...fontFamily.medium,
     fontSize: 16,
     lineHeight: 24,
     letterSpacing: 0.5,
     textTransform: 'uppercase',
   },
-  'button-2': {
-    ...weight.medium,
+  'button-normal': {
+    ...fontFamily.medium,
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: 0.5,
     textTransform: 'uppercase',
   },
-  'button-3': {
-    ...weight.medium,
+  'button-small': {
+    ...fontFamily.medium,
     fontSize: 12,
     lineHeight: 19,
     letterSpacing: 0.3,
     textTransform: 'uppercase',
   },
-  'link-1': {
-    ...weight.regular,
+  'link-normal': {
+    ...fontFamily.regular,
     fontSize: 16,
     lineHeight: 22,
   },
-  'link-1-underline': {
-    ...weight.regular,
+  'link-normal-underline': {
+    ...fontFamily.regular,
     fontSize: 16,
     lineHeight: 24,
     textDecorationLine: 'underline',
   },
-  'link-2': {
-    ...weight.regular,
+  'link-small': {
+    ...fontFamily.regular,
     fontSize: 14,
     lineHeight: 20,
   },
-  'link-2-underline': {
-    ...weight.regular,
+  'link-small-underline': {
+    ...fontFamily.regular,
     fontSize: 14,
     lineHeight: 22,
     textDecorationLine: 'underline',
   },
   'overline': {
-    ...weight.regular,
+    ...fontFamily.regular,
     fontSize: 10,
     lineHeight: 18,
     textTransform: 'uppercase',
   },
   'caption-medium': {
-    ...weight.medium,
+    ...fontFamily.medium,
     ...size.caption,
   },
   'caption-regular': {
-    ...weight.regular,
+    ...fontFamily.regular,
     ...size.caption,
   },
 }
