2f5a56d12767d6e2f69e565cb083719aadcbea72
Javier Bueno <javierbueno@emurgo.io>
2024-01-20 00:08:10 +0100

    fix(resolver): update unstoppable supported tlds and manage errors

--- packages/resolver/src/utils/isResolvableDomain.test.ts
+++ packages/resolver/src/utils/isResolvableDomain.test.ts
@@ -3,22 +3,28 @@ import {isResolvableDomain} from './isResolvableDomain'
 describe('isResolvableDomain', () => {
   it.each`
     resolve             | expected
-    ${'ud.com'}         | ${true}
-    ${'ud.eth'}         | ${true}
-    ${'ud.crypto'}      | ${true}
-    ${'ud.zil'}         | ${true}
-    ${'ud.bitcoin'}     | ${true}
-    ${'ud.blockchain'}  | ${true}
-    ${'ud.go'}          | ${true}
-    ${'ud.888'}         | ${true}
-    ${'ud.dao'}         | ${true}
-    ${'ud.polygon'}     | ${true}
-    ${'ud.wallet'}      | ${true}
-    ${'ud.nft'}         | ${true}
     ${'ud.x'}           | ${true}
-    ${'ud.unstoppable'} | ${true}
+    ${'ud.polygon'}     | ${true}
+    ${'ud.nft'}         | ${true}
+    ${'ud.crypto'}      | ${true}
+    ${'ud.blockchain'}  | ${true}
+    ${'ud.bitcoin'}     | ${true}
+    ${'ud.dao'}         | ${true}
+    ${'ud.888'}         | ${true}
+    ${'ud.wallet'}      | ${true}
+    ${'ud.binanceus'}   | ${true}
+    ${'ud.hi'}          | ${true}
+    ${'ud.klever'}      | ${true}
+    ${'ud.kresus'}      | ${true}
+    ${'ud.anime'}       | ${true}
+    ${'ud.manga'}       | ${true}
+    ${'ud.go'}          | ${true}
+    ${'ud.zil'}         | ${true}
+    ${'ud.eth'}         | ${true}
     ${'$adahandle'}     | ${true}
     ${'cns.ada'}        | ${true}
+    ${'ud.com'}         | ${false}
+    ${'ud.unstoppable'} | ${false}
     ${'other.uk'}       | ${false}
     ${'$'}              | ${false}
   `('should return $expected for $resolve', ({resolve, expected}) => {
