a3996ea64136f31f9d7c7b09c0f99cfdd5a2c06c
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-01-24 10:46:17 +0000

    Update packages/types/src/resolver/errors.ts
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- packages/types/src/resolver/errors.ts
+++ packages/types/src/resolver/errors.ts
@@ -3,4 +3,4 @@ export class ResolverErrorInvalidDomain extends Error {}
 export class ResolverErrorNotFound extends Error {}
 export class ResolverErrorUnsupportedTld extends Error {}
 export class ResolverErrorExpiredDomain extends Error {}
-export class ResolverErrorInvalidBlockchain extends Error {}
+export class ResolverErrorWrongBlockchain extends Error {}
