8113a8b013a64b0fff46a8bbdc3eaace315d3120
Vinicius Santiago <vinicius.eduardo@kognit.com.br>
2024-02-08 18:57:42 +0000

    Fix (Receive): Change navigator to History and add safeareaview in the screens. Also fixed HistoryNavigator based on commit 14aa4a7

--- apps/wallet-mobile/src/features/Receive/useCases/SpecificAmountScreen.tsx
+++ apps/wallet-mobile/src/features/Receive/useCases/SpecificAmountScreen.tsx
@@ -72,7 +72,7 @@ export const SpecificAmountScreen = () => {
             <View style={styles.screen}>
               <Text style={styles.textAddressDetails}>{strings.specificAmountDescription}</Text>
 
-              <TextInput label={strings.ADALabel} keyboardType="numeric" onChangeText={setAmount} maxLength={4} />
+              <TextInput label={strings.ADALabel} keyboardType="number-pad" onChangeText={setAmount} maxLength={4} />
 
               <View style={styles.textSection}>
                 <Text style={[styles.textAddressDetails, {color: colors.gray}]}>{strings.address}</Text>
