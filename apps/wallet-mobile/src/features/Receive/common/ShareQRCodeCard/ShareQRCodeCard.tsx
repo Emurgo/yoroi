764b91af3984141f017d5c2480d7c72ef5d31835
Vinicius Santiago <60615218+euvini@users.noreply.github.com>
2024-02-09 10:26:52 +0000

    Update apps/wallet-mobile/src/features/Receive/common/ShareQRCodeCard/ShareQRCodeCard.tsx
    Co-authored-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
    Signed-off-by: Vinicius Santiago <60615218+euvini@users.noreply.github.com>

--- apps/wallet-mobile/src/features/Receive/common/ShareQRCodeCard/ShareQRCodeCard.tsx
+++ apps/wallet-mobile/src/features/Receive/common/ShareQRCodeCard/ShareQRCodeCard.tsx
@@ -28,7 +28,7 @@ type AddressDetailsProps = {
 }
 
 export function ShareQRCodeCard({address, title, isCopying, onLongPress}: ShareProps) {
-  const [isSharing, setIsSharing] = useState<boolean>(false)
+  const [isSharing, setIsSharing] = React.useState<boolean>(false)
   const strings = useStrings()
   const ref: RefObject<ViewShot> = React.useRef(null)
 
