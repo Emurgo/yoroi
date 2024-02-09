ea1da0b6dc571fcaf3ab06e56003d4c0198b5ba4
Michal <miso.szorad@gmail.com>
2024-02-09 14:25:34 +0000

    Tabs

--- apps/wallet-mobile/src/features/DappExplorer/useCases/HomeScreen.tsx
+++ apps/wallet-mobile/src/features/DappExplorer/useCases/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {Platform, Share, StyleSheet, TouchableOpacity, View} from 'react-native'
+import {Platform, Share, StyleSheet, TouchableOpacity, View, Text} from 'react-native'
 import {WebView} from 'react-native-webview'
 import React from 'react'
 import {WebViewProgressEvent} from 'react-native-webview/lib/WebViewTypes'
@@ -10,17 +10,45 @@ import {
 } from '../common/icons'
 import {useConnectWalletToWebView} from '../WebViewConnector'
 import {useSelectedWallet} from '../../../SelectedWallet'
+import {NavigationContainer} from '@react-navigation/native'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
 
 const DAPP_URL = 'https://muesliswap.com/swap'
 
 const ENABLED_BUTTON_COLOR = '#383E54'
 const DISABLED_BUTTON_COLOR = '#8A92A3'
+const Tab = createMaterialTopTabNavigator()
 
 export const HomeScreen = () => {
   const ref = React.useRef<WebView | null>(null)
   const [canGoBack, setCanGoBack] = React.useState(false)
   const [canGoForward, setCanGoForward] = React.useState(false)
   const wallet = useSelectedWallet()
+
+  const [tabs, setTabs] = React.useState([
+    {
+      name: 'tab-1',
+      component: TabScreen,
+    },
+    {
+      name: 'tab-2',
+      component: TabScreen,
+    },
+  ])
+
+  const addNewTab = () => {
+    setTabs((tabs) => [
+      ...tabs,
+      {
+        name: 'tab-' + (parseInt(tabs.pop()?.name.replace('tab-', '') ?? '0') + 1),
+        component: TabScreen,
+      },
+    ])
+  }
+
+  const remove = (route: any) => {
+    setTabs((tabs) => tabs.filter((tab) => tab.name !== route.name))
+  }
 
   const handleReloadPress = () => {
     ref.current?.reload()
@@ -47,32 +75,15 @@ export const HomeScreen = () => {
     setCanGoForward(e.nativeEvent.canGoForward)
   }
 
-  const {handleEvent, initScript} = useConnectWalletToWebView(wallet, ref.current)
-
   return (
     <View style={styles.root}>
-      <WebView
-        source={{uri: DAPP_URL}}
-        ref={ref}
-        injectedJavaScript={initScript}
-        onLoadProgress={handleLoadProgress}
-        onMessage={handleEvent}
-      />
-
-      <View style={styles.navigationContainer}>
-        <TouchableOpacity onPress={handleBackPress} style={styles.navigationButton} disabled={!canGoBack}>
-          <NavigationArrowLeftIcon color={canGoBack ? ENABLED_BUTTON_COLOR : DISABLED_BUTTON_COLOR} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleForwardPress} style={styles.navigationButton} disabled={!canGoForward}>
-          <NavigationArrowRightIcon color={canGoForward ? ENABLED_BUTTON_COLOR : DISABLED_BUTTON_COLOR} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleSharePress} style={styles.navigationButton}>
-          <NavigationShareIcon color={ENABLED_BUTTON_COLOR} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleReloadPress} style={styles.navigationButton}>
-          <NavigationRefreshIcon color={ENABLED_BUTTON_COLOR} />
-        </TouchableOpacity>
-      </View>
+      <NavigationContainer independent={true}>
+        <Tab.Navigator style={{backgroundColor: 'red'}}>
+          {tabs.map((tab) => (
+            <Tab.Screen key={tab.name} name={tab.name} component={tab.component} />
+          ))}
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   )
 }
@@ -97,3 +108,60 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
+
+function TabScreen() {
+  const ref = React.useRef<WebView | null>(null)
+  const wallet = useSelectedWallet()
+  const {handleEvent, initScript} = useConnectWalletToWebView(wallet, ref.current)
+
+  const [canGoBack, setCanGoBack] = React.useState(false)
+  const [canGoForward, setCanGoForward] = React.useState(false)
+
+  const handleReloadPress = () => {
+    ref.current?.reload()
+  }
+
+  const handleBackPress = () => {
+    ref.current?.goBack()
+  }
+
+  const handleForwardPress = () => {
+    ref.current?.goForward()
+  }
+
+  const handleSharePress = () => {
+    if (Platform.OS === 'android') {
+      Share.share({message: DAPP_URL})
+    } else {
+      Share.share({url: DAPP_URL})
+    }
+  }
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <WebView
+          source={{uri: DAPP_URL}}
+          ref={ref}
+          injectedJavaScript={initScript}
+          onMessage={handleEvent}
+          id={wallet.id}
+          nativeID={wallet.id}
+        />
+      </View>
+      <View style={styles.navigationContainer}>
+        <TouchableOpacity onPress={handleBackPress} style={styles.navigationButton} disabled={!canGoBack}>
+          <NavigationArrowLeftIcon color={canGoBack ? ENABLED_BUTTON_COLOR : DISABLED_BUTTON_COLOR} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleForwardPress} style={styles.navigationButton} disabled={!canGoForward}>
+          <NavigationArrowRightIcon color={canGoForward ? ENABLED_BUTTON_COLOR : DISABLED_BUTTON_COLOR} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSharePress} style={styles.navigationButton}>
+          <NavigationShareIcon color={ENABLED_BUTTON_COLOR} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleReloadPress} style={styles.navigationButton}>
+          <NavigationRefreshIcon color={ENABLED_BUTTON_COLOR} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
