362c8c206d85d96c2a26de402f53f507617ca876
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-02-13 12:22:51 +0000

    chore(common,resolver,wallet-mobile,staking): sync storage update and rootStorage as client

--- apps/wallet-mobile/src/auth/CreatePinScreen/CreatePinScreen.stories.tsx
+++ apps/wallet-mobile/src/auth/CreatePinScreen/CreatePinScreen.stories.tsx
@@ -1,18 +1,19 @@
 import {action} from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react-native'
-import {rootStorage, StorageProvider} from '@yoroi/common'
+import {AsyncStorageProvider} from '@yoroi/common'
 import React from 'react'
 
+import {rootStorage} from '../../yoroi-wallets/storage/rootStorage'
 import {CreatePinScreen} from './CreatePinScreen'
 
 storiesOf('CreatePinScreen', module).add('Default', () => {
   return (
-    <StorageProvider
+    <AsyncStorageProvider
       storage={{
         ...rootStorage,
       }}
     >
       <CreatePinScreen onDone={() => action('onDone')} />
-    </StorageProvider>
+    </AsyncStorageProvider>
   )
 })
