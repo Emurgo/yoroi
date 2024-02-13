2519a2624128af6fd20ceddd353da6957ec3d0ad
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-02-13 12:15:28 +0000

    chore(types): updated multi/simple storage

--- packages/types/src/app/storage.ts
+++ packages/types/src/app/storage.ts
@@ -1,27 +1,31 @@
+import {MaybePromise} from '../helpers/types'
+
 export type AppStorageFolderName = `${string}/`
 
-export interface AppStorage {
-  join: (folderName: AppStorageFolderName) => AppStorage
+export interface AppStorage<IsAsync extends boolean = true> {
+  join: (folderName: AppStorageFolderName) => AppStorage<IsAsync>
   getItem: <T = unknown>(
     key: string,
     parse?: (item: string | null) => T,
-  ) => Promise<T>
+  ) => MaybePromise<T, IsAsync>
   multiGet: <T = unknown>(
     keys: ReadonlyArray<string>,
     parse?: (item: string | null) => T,
-  ) => Promise<ReadonlyArray<[string, T]>>
+  ) => MaybePromise<ReadonlyArray<[string, T]>, IsAsync>
   setItem: <T = unknown>(
     key: string,
     value: T,
     stringify?: (data: T) => string,
-  ) => Promise<void>
+  ) => MaybePromise<void, IsAsync>
   multiSet: (
     tuples: ReadonlyArray<[key: string, value: unknown]>,
     stringify?: (data: unknown) => string,
-  ) => Promise<void>
-  removeItem: (key: string) => Promise<void>
-  removeFolder: (folderName: AppStorageFolderName) => Promise<void>
-  multiRemove: (keys: ReadonlyArray<string>) => Promise<void>
-  getAllKeys: () => Promise<ReadonlyArray<string>>
-  clear: () => Promise<void>
+  ) => MaybePromise<void, IsAsync>
+  removeItem: (key: string) => MaybePromise<void, IsAsync>
+  removeFolder: (
+    folderName: AppStorageFolderName,
+  ) => MaybePromise<void, IsAsync>
+  multiRemove: (keys: ReadonlyArray<string>) => MaybePromise<void, IsAsync>
+  getAllKeys: () => MaybePromise<ReadonlyArray<string>, IsAsync>
+  clear: () => MaybePromise<void, IsAsync>
 }
