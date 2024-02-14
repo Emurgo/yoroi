91b8066f3cd37661a7d8cf4d902c13c3c619930a
Vinicius Santiago <vinicius.eduardo@kognit.com.br>
2024-02-14 17:56:44 +0000

    remove folder openswap

--- packages/openswap/dist/price.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ApiDeps, PriceAddress, PriceResponse } from './types';
-export declare function getPrice(deps: ApiDeps, args: {
-    baseToken: PriceAddress;
-    quoteToken: PriceAddress;
-}): Promise<PriceResponse>;
--- packages/openswap/dist/token-pairs.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ApiDeps, TokenPairsResponse } from './types';
-export declare function getTokenPairs(deps: ApiDeps, { policyId, assetName }?: {
-    policyId?: string | undefined;
-    assetName?: string | undefined;
-}): Promise<TokenPairsResponse>;
