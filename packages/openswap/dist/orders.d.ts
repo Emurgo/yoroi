584d37ad8a067ddba9ec7fc2d9a501f2a927c8be
Sorin Chis <kish.sorin@yahoo.com>
2024-02-14 11:02:34 +0200

    remove transation and openswap package

--- packages/openswap/dist/orders.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ApiDeps, CancelOrderRequest, CreateOrderRequest, CreateOrderResponse, CompletedOrderResponse, OpenOrderResponse } from './types';
-export declare function createOrder(deps: ApiDeps, args: CreateOrderRequest): Promise<CreateOrderResponse>;
-export declare function cancelOrder(deps: ApiDeps, args: CancelOrderRequest): Promise<string>;
-export declare function getOrders(deps: ApiDeps, args: {
-    stakeKeyHash: string;
-}): Promise<OpenOrderResponse>;
-export declare function getCompletedOrders(deps: ApiDeps, args: {
-    stakeKeyHash: string;
-}): Promise<CompletedOrderResponse>;
--- packages/openswap/dist/tokens.d.ts
+++
@@ -1,2 +0,0 @@
-import type { ApiDeps, ListTokensResponse } from './types';
-export declare function getTokens(deps: ApiDeps): Promise<ListTokensResponse>;
