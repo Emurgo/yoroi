1979d3872d04a36a85dc19fb0568becf73348f22
Sorin Chis <kish.sorin@yahoo.com>
2024-02-14 14:15:01 +0200

    add default prop

--- apps/wallet-mobile/src/components/Icon/Chevron.tsx
+++ apps/wallet-mobile/src/components/Icon/Chevron.tsx
@@ -4,10 +4,12 @@ import Svg, {Path} from 'react-native-svg'
 
 export const Chevron = ({
   direction,
+  color,
   size = 40,
 }: {
   size?: number
   backgroundColor?: string
+  color?: string
   direction: 'left' | 'right' | 'up' | 'down'
 }) => {
   const {theme} = useTheme()
@@ -19,7 +21,7 @@ export const Chevron = ({
         fillRule="evenodd"
         clipRule="evenodd"
         d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683417 14.0976 1.31658 13.7071 1.70711L7.70711 7.70711C7.31658 8.09763 6.68342 8.09763 6.29289 7.70711L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z"
-        fill={theme.color.gray.max}
+        fill={color ?? theme.color.gray.max}
       />
     </Svg>
   )
