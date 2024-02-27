fb647161cfcbbf541fb16b58275244410d9754ec
Michal <miso.szorad@gmail.com>
2024-02-27 15:38:49 +0000

    Refactor connector

--- packages/dapp-connector/src/connector.js
+++ packages/dapp-connector/src/connector.js
@@ -4,23 +4,34 @@ export const connectWallet = (props) => {
   return `(${connectWallet2.toString()})(${JSON.stringify(props)})`
 }
 
-const connectWallet2 = ({iconUrl, apiVersion, walletName, supportedExtensions, sessionId}) => {
+const connectWallet2 = ({
+  iconUrl,
+  apiVersion,
+  walletName,
+  supportedExtensions,
+  sessionId,
+}) => {
   // https://github.com/facebook/hermes/issues/114
   // https://github.com/facebook/hermes/issues/612
   'show source please'
   if (typeof iconUrl !== 'string') throw new Error('iconUrl must be a string')
-  if (typeof apiVersion !== 'string') throw new Error('apiVersion must be a string')
-  if (typeof walletName !== 'string') throw new Error('walletName must be a string')
-  if (!Array.isArray(supportedExtensions)) throw new Error('supportedExtensions must be an array')
+  if (typeof apiVersion !== 'string')
+    throw new Error('apiVersion must be a string')
+  if (typeof walletName !== 'string')
+    throw new Error('walletName must be a string')
+  if (!Array.isArray(supportedExtensions))
+    throw new Error('supportedExtensions must be an array')
 
-  const postMessage = (data) => window.ReactNativeWebView.postMessage(JSON.stringify(data))
+  const postMessage = (data) =>
+    window.ReactNativeWebView.postMessage(JSON.stringify(data))
   const promisesMap = new Map()
 
-  const callExternalMethod = (method, params) => {
+  const callExternalMethod = (method, args) => {
     const requestId = Math.random().toString(36).substr(2, 9) // id needs to be unique
     const promise = new Promise((resolve, reject) => {
       promisesMap.set(requestId, {resolve, reject})
     })
+    const params = {args, browserContext: getContext()}
     postMessage({id: requestId, method, params})
     return promise
   }
@@ -33,34 +44,36 @@ const connectWallet2 = ({iconUrl, apiVersion, walletName, supportedExtensions, s
   }
 
   window.addEventListener('error', (event) => {
-    // callExternalMethod('log_message', 'Unhandled error:' + serializeError(event.error))
+    callExternalMethod(
+      'log_message',
+      'Unhandled error:' + serializeError(event.error),
+    )
   })
 
   window.addEventListener('unhandledrejection', (event) => {
-    // callExternalMethod('log_message', 'Unhandled rejection:' + serializeError(event.reason))
+    callExternalMethod(
+      'log_message',
+      'Unhandled rejection:' + serializeError(event.reason),
+    )
   })
 
   window.addEventListener('message', (event) => {
-    // alert('got message' + JSON.stringify(event.data))
-    // callExternalMethod('log_message', 'GOT EVENT' + JSON.stringify(event.data))
     const {id, result, error} = event.data
     const promise = promisesMap.get(id)
-    // if (!promise) alert('promise not found' + id)
-    if (promise) {
-      // alert('promise found' + id + JSON.stringify(event.data))
-      promisesMap.delete(id)
-      if (error) {
-        // callExternalMethod('log_message', 'WebView received error:' + JSON.stringify(error))
-        promise.reject(handleError(error))
-      } else {
-        // callExternalMethod('log_message', 'WebView received response:' + JSON.stringify(result))
-        promise.resolve(result)
-      }
+    if (!promise) return
+    promisesMap.delete(id)
+    if (error) {
+      promise.reject(handleError(error))
+    } else {
+      promise.resolve(result)
     }
   })
 
   const createApi = (cardanoEnableResponse) => {
-    callExternalMethod('log_message', 'cardanoEnableResponse:' + JSON.stringify(cardanoEnableResponse))
+    callExternalMethod(
+      'log_message',
+      'cardanoEnableResponse:' + JSON.stringify(cardanoEnableResponse),
+    )
     if (!cardanoEnableResponse) {
       alert('not enabled wallet')
       throw {code: -3, info: 'User Rejected'}
@@ -69,19 +82,19 @@ const connectWallet2 = ({iconUrl, apiVersion, walletName, supportedExtensions, s
 
     enabling = false
     return {
-      getBalance: (...args) => callExternalMethod('api.getBalance', {args, browserContext: getContext()}),
-      getChangeAddress: (...args) => callExternalMethod('api.getChangeAddress', {args, browserContext: getContext()}),
-      getNetworkId: (...args) => callExternalMethod('api.getNetworkId', {args, browserContext: getContext()}),
+      getBalance: (...args) => callExternalMethod('api.getBalance', args),
+      getChangeAddress: (...args) =>
+        callExternalMethod('api.getChangeAddress', args),
+      getNetworkId: (...args) => callExternalMethod('api.getNetworkId', args),
       getRewardAddresses: (...args) =>
-        callExternalMethod('api.getRewardAddresses', {args, browserContext: getContext()}),
-      getUsedAddresses: (...args) => callExternalMethod('api.getUsedAddresses', {args, browserContext: getContext()}),
+        callExternalMethod('api.getRewardAddresses', args),
+      getUsedAddresses: (...args) =>
+        callExternalMethod('api.getUsedAddresses', args),
     }
   }
 
   const getContext = () => {
-    return {
-      origin: window.location.origin,
-    }
+    return {origin: window.location.origin}
   }
 
   let enabling = false
@@ -95,13 +108,13 @@ const connectWallet2 = ({iconUrl, apiVersion, walletName, supportedExtensions, s
       return false
     }
 
-    return callExternalMethod('cardano_is_enabled', {browserContext: getContext()})
+    return callExternalMethod('cardano_is_enabled')
   }
 
-  const enable = () => {
+  const enable = (...args) => {
     enabling = true
     localStorage.setItem('yoroi-session-id', sessionId)
-    return callExternalMethod('cardano_enable', {browserContext: getContext()})
+    return callExternalMethod('cardano_enable', args)
       .then(createApi)
       .catch((e) => handleError(e))
   }
