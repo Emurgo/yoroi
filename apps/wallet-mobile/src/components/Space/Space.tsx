7fa0d1586f439af2cc6161111123b6bb63e27a6a
Sorin Chis <kish.sorin@yahoo.com>
2024-02-28 12:43:58 +0200

    export types from package

--- apps/wallet-mobile/src/components/Space/Space.tsx
+++ apps/wallet-mobile/src/components/Space/Space.tsx
@@ -1,24 +1,12 @@
+import {baseSpace, SpacingSize} from '@yoroi/theme'
 import React from 'react'
 import {View, ViewStyle} from 'react-native'
-
-// TODO import this from theme dirrectly if is approved
-type SpacingKey = 'none' | 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl'
-const baseSpace: Record<SpacingKey, number> = {
-  none: 0,
-  xxs: 2,
-  xs: 4,
-  s: 8,
-  m: 12,
-  l: 16,
-  xl: 24,
-  xxl: 32,
-}
 
 const debugStyle = {backgroundColor: 'red', opacity: 0.2}
 
 type Props = {
-  height?: SpacingKey
-  width?: SpacingKey
+  height?: SpacingSize
+  width?: SpacingSize
   fill?: boolean
   style?: ViewStyle
   debug?: boolean
