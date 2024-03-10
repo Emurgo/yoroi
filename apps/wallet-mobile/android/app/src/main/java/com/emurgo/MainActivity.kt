bddefbd91f42800d78f71768e223b34bef0e462f
lisicky <lisicky@inbox.ru>
2024-03-11 02:16:27 +0700

    bump packages for the latest rn and expo

--- apps/wallet-mobile/android/app/src/main/java/com/emurgo/MainActivity.kt
+++ apps/wallet-mobile/android/app/src/main/java/com/emurgo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.emurgo
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +21,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
+  }
 }
