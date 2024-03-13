e4ab8944d74ff49e96aab377b8bfe554eaa8367e
jorbuedo <jorbuedo@gmail.com>
2024-03-13 15:44:31 +0100

    Removes tx history animate down

--- apps/wallet-mobile/src/TxHistory/useAnimatedTxHistory.ts
+++ apps/wallet-mobile/src/TxHistory/useAnimatedTxHistory.ts
@@ -1,4 +1,3 @@
-import {useNavigation} from '@react-navigation/native'
 import React from 'react'
 import {useAnimatedStyle, useSharedValue, withSpring} from 'react-native-reanimated'
 
@@ -10,8 +9,6 @@ const animatedConfig = {
 }
 
 const useAnimatedTxHistory = () => {
-  const navigation = useNavigation()
-
   const translateYOffset = useSharedValue(initialTranslateYOffset)
   const translateStyles = useAnimatedStyle(() => ({
     transform: [{translateY: translateYOffset.value}],
@@ -20,19 +17,6 @@ const useAnimatedTxHistory = () => {
   React.useLayoutEffect(() => {
     if (translateYOffset.value !== 0) {
       translateYOffset.value = withSpring(0, animatedConfig)
-    }
-
-    const cleanUpFocus = navigation.addListener('focus', () => {
-      translateYOffset.value = withSpring(0, animatedConfig)
-    })
-
-    const cleanUpBlur = navigation.addListener('blur', () => {
-      translateYOffset.value = withSpring(initialTranslateYOffset, animatedConfig)
-    })
-
-    return () => {
-      cleanUpFocus()
-      cleanUpBlur()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
