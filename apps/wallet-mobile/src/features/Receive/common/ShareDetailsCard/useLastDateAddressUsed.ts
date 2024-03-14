7497c9b500c33990c77b44c021e6628d9f27bb2e
jorbuedo <jorbuedo@gmail.com>
2024-03-14 14:53:23 +0100

    Memo last used format

--- apps/wallet-mobile/src/features/Receive/common/ShareDetailsCard/useLastDateAddressUsed.ts
+++ apps/wallet-mobile/src/features/Receive/common/ShareDetailsCard/useLastDateAddressUsed.ts
@@ -6,23 +6,22 @@ import {useSelectedWallet} from '../../../../SelectedWallet'
 export const useLastDateAddressUsed = (address: string) => {
   const wallet = useSelectedWallet()
   const intl = useIntl()
-  const lastUsed = useMemo(
-    () =>
-      Object.values(wallet.transactions).reduce((currentLast, tx) => {
-        const {inputs, outputs} = tx
-        const isRelevant = inputs.some((v) => address === v.address) || outputs.some((v) => address === v.address)
-        if (!isRelevant) return currentLast
-        const lastUpdatedAt = new Date(tx.lastUpdatedAt).getTime()
+  return useMemo(() => {
+    const lastUsed = Object.values(wallet.transactions).reduce((currentLast, tx) => {
+      const {inputs, outputs} = tx
+      const isRelevant = inputs.some((v) => address === v.address) || outputs.some((v) => address === v.address)
+      if (!isRelevant) return currentLast
+      const lastUpdatedAt = new Date(tx.lastUpdatedAt).getTime()
 
-        return Math.max(currentLast, lastUpdatedAt)
-      }, 0),
-    [address, wallet.transactions],
-  )
-  return lastUsed
-    ? intl.formatDate(new Date(lastUsed), {
-        dateStyle: 'short',
-        timeStyle: 'short',
-        hour12: false,
-      })
-    : null
+      return Math.max(currentLast, lastUpdatedAt)
+    }, 0)
+
+    return lastUsed
+      ? intl.formatDate(new Date(lastUsed), {
+          dateStyle: 'short',
+          timeStyle: 'short',
+          hour12: false,
+        })
+      : null
+  }, [address, intl, wallet.transactions])
 }
