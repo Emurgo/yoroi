97e295914086939b24590ff52898a535adbffa4f
Michal <miso.szorad@gmail.com>
2024-03-15 17:49:51 +0000

    Update storage.ts

--- packages/dapp-connector/src/storage.ts
+++ packages/dapp-connector/src/storage.ts
@@ -2,16 +2,19 @@ import {App} from '@yoroi/types'
 
 export const connectionStorageMaker = (storage: App.Storage): Storage => {
   const listAllConnections = async () => (await storage.getItem<DappConnection[]>('connections')) || []
+
   const removeConnection = async (connection: DappConnection) => {
     const connections = await listAllConnections()
     const newConnections = connections.filter((c) => !areConnectionsEqual(c, connection))
     await storage.setItem('connections', newConnections)
   }
+
   const addConnection = async (connection: DappConnection) => {
     const connections = await listAllConnections()
     const newConnections = [...connections, connection]
     await storage.setItem('connections', newConnections)
   }
+
   return {
     listAllConnections,
     removeConnection,
