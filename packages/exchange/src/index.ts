f585d4680b1c6b2b4a8ee524a6e73a8b54084145
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-03-17 21:29:01 +0000

    fix: deps (#3141)
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- packages/exchange/src/index.ts
+++ packages/exchange/src/index.ts
@@ -1,8 +1,12 @@
-export {exchangeApiMaker} from './adapters/api'
-export {useCreateReferralLink} from './translators/reactjs/hooks/useCreateReferralLink'
-export {useExchangeProvidersByOrderType} from './translators/reactjs/hooks/useExchangeProvidersByOrderType'
-export {useExchange} from './translators/reactjs/hooks/useExchange'
-export {ExchangeProvider} from './translators/reactjs/provider/ExchangeProvider'
-export {exchangeManagerMaker} from './manager'
+export * from './adapters/banxa/api'
+export * from './adapters/encryptus/api'
+export * from './adapters/api'
+
+export * from './translators/reactjs/hooks/useCreateReferralLink'
+export * from './translators/reactjs/hooks/useExchangeProvidersByOrderType'
+export * from './translators/reactjs/hooks/useExchange'
+export * from './translators/reactjs/provider/ExchangeProvider'
 export * from './translators/reactjs/state/state'
+
+export * from './manager'
 export * from './manager.mocks'
