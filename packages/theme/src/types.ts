a3237a00878f7fd9b8992051708785220ef2cd0c
hoailp <phuochoaileqn@gmail.com>
2024-03-19 10:20:05 +0700

    feat: init new gradient color

--- packages/theme/src/types.ts
+++ packages/theme/src/types.ts
@@ -69,6 +69,7 @@ export type Palette = {
     'green': Gradient
     'blue': Gradient
     'light': Gradient
+    'green-blue': [HexColor, HexColor, HexColor, HexColor, HexColor, HexColor]
   }
   'bottom-sheet-background': HexColor
   'overlay-extension': {hex: HexColor; opacity: number}
