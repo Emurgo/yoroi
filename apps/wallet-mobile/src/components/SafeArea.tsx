f8a630601c260c21ef1fa0b0518f5ea467c1828b
Michal <miso.szorad@gmail.com>
2024-03-21 04:50:37 +0000

    Use theme

--- apps/wallet-mobile/src/components/SafeArea.tsx
+++ apps/wallet-mobile/src/components/SafeArea.tsx
@@ -1,10 +1,10 @@
+import {useTheme} from '@yoroi/theme'
 import React from 'react'
 import {StyleSheet} from 'react-native'
 import {type SafeAreaViewProps, SafeAreaView} from 'react-native-safe-area-context'
 
-import {COLORS} from '../theme'
-
 export const SafeArea = ({children, ...rest}: SafeAreaViewProps) => {
+  const styles = useStyles()
   return (
     <SafeAreaView edges={safeAreaEdges} {...rest} style={[styles.root, rest.style]}>
       {children}
@@ -13,9 +13,12 @@ export const SafeArea = ({children, ...rest}: SafeAreaViewProps) => {
 }
 const safeAreaEdges = ['bottom', 'left', 'right', 'bottom'] as const
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: COLORS.WHITE,
-  },
-})
+const useStyles = () => {
+  const {theme} = useTheme()
+  return StyleSheet.create({
+    root: {
+      flex: 1,
+      backgroundColor: theme.color['white-static'],
+    },
+  })
+}
