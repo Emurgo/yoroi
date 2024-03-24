ec79c90736cf07821bc8f81d09062dab9fec6bbb
Javier Bueno <javierbueno@emurgo.io>
2024-03-24 01:58:19 +0100

    feature(exchange): loading and error screens

--- apps/wallet-mobile/.storybook/storybook.requires.js
+++ apps/wallet-mobile/.storybook/storybook.requires.js
@@ -89,7 +89,6 @@ const getStories = () => {
     "./src/components/LanguagePicker/LanguagePickerWarning.stories.tsx": require("../src/components/LanguagePicker/LanguagePickerWarning.stories.tsx"),
     "./src/components/Link/Link.stories.tsx": require("../src/components/Link/Link.stories.tsx"),
     "./src/components/Loading/Loading.stories.tsx": require("../src/components/Loading/Loading.stories.tsx"),
-    "./src/components/LoadingOverlay/LoadingOverlay.stories.tsx": require("../src/components/LoadingOverlay/LoadingOverlay.stories.tsx"),
     "./src/components/ModalError/ModalError.stories.tsx": require("../src/components/ModalError/ModalError.stories.tsx"),
     "./src/components/ModalScreenWrapper/ModalScreenWrapper.stories.tsx": require("../src/components/ModalScreenWrapper/ModalScreenWrapper.stories.tsx"),
     "./src/components/NftImageGallery/NftImageGallery.stories.tsx": require("../src/components/NftImageGallery/NftImageGallery.stories.tsx"),
