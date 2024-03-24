502f266de951eb0f768bcf07ea5be7635d36e5ef
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-03-24 17:25:29 +0000

    Update apps/wallet-mobile/src/features/Links/useCases/RequestedAdaPaymentWithLinkScreen/RequestedAdaPaymentWithLink.stories.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/src/features/Links/useCases/RequestedAdaPaymentWithLinkScreen/RequestedAdaPaymentWithLink.stories.tsx
+++ apps/wallet-mobile/src/features/Links/useCases/RequestedAdaPaymentWithLinkScreen/RequestedAdaPaymentWithLink.stories.tsx
@@ -1,5 +1,5 @@
 import {storiesOf} from '@storybook/react-native'
-import React from 'react'
+import * as React from 'react'
 import {StyleSheet, View} from 'react-native'
 
 import {RequestedAdaPaymentWithLinkScreen} from './RequestedAdaPaymentWithLinkScreen'
