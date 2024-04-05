fea2654291f6abf9335158f159f7cdce94c21fbe
Javier Bueno <javierbueno@emurgo.io>
2024-04-05 22:41:07 +0200

    feature(add-wallet): auth out init screen

--- apps/wallet-mobile/src/features/Receive/common/SingleOrMultipleAddressesModal/SingleOrMultipleAddressesModal.stories.tsx
+++ apps/wallet-mobile/src/features/Receive/common/SingleOrMultipleAddressesModal/SingleOrMultipleAddressesModal.stories.tsx
@@ -3,9 +3,9 @@ import {storiesOf} from '@storybook/react-native'
 import {AsyncStorageProvider} from '@yoroi/common'
 import * as React from 'react'
 
-import {SelectedWalletProvider} from '../../../../SelectedWallet'
 import {mocks} from '../../../../yoroi-wallets/mocks'
 import {rootStorage} from '../../../../yoroi-wallets/storage/rootStorage'
+import {SelectedWalletProvider} from '../../../Wallet/common/Context'
 import {SingleOrMultipleAddressesModal} from './SingleOrMultipleAddressesModal'
 
 storiesOf('Receive SingleOrMultipleAddressesModal', module).add('default', () => (
