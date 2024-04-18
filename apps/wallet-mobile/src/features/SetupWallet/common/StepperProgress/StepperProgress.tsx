1b2538d1f8b7de0f92652d6cb7e3ef69c247cdfa
Javier Bueno <javierbueno@emurgo.io>
2024-04-18 16:26:59 +0200

    fix(wallet-setup): stepper numbers alignment

--- apps/wallet-mobile/src/features/SetupWallet/common/StepperProgress/StepperProgress.tsx
+++ apps/wallet-mobile/src/features/SetupWallet/common/StepperProgress/StepperProgress.tsx
@@ -4,6 +4,13 @@ import {StyleSheet, ViewStyle} from 'react-native'
 import Animated, {Layout} from 'react-native-reanimated'
 
 import {CheckIllustration} from '../../illustrations/CheckIllustration'
+import {Number1} from './Number1'
+import {Number2} from './Number2'
+import {Number2Empty} from './Number2Empty'
+import {Number3} from './Number3'
+import {Number3Empty} from './Number3Empty'
+import {Number4} from './Number4'
+import {Number4Empty} from './Number4Empty'
 
 type StepProps = {
   currentStep: number
@@ -14,17 +21,16 @@ type StepProps = {
 const Step = ({currentStep, currentStepTitle, isNext, isPrevious}: StepProps) => {
   const {styles} = useStyles()
   const shouldDisplayStepTitle = !isNext && !isPrevious && currentStepTitle !== undefined
+
+  const StepLogo = !isPrevious ? getStepperLogo(currentStep, isNext) : CheckIllustration
+
   return (
     <Animated.View style={styles.root}>
       <Animated.View
         layout={Layout}
         style={[styles.step, isNext && styles.isNext, isPrevious && styles.isPrevious, styles.markedStep]}
       >
-        {!isPrevious ? (
-          <Animated.Text style={[styles.stepNumber, isNext && styles.isNextNumber]}>{currentStep}</Animated.Text>
-        ) : (
-          <CheckIllustration />
-        )}
+        {StepLogo && <StepLogo />}
       </Animated.View>
 
       {shouldDisplayStepTitle && (
@@ -64,6 +70,38 @@ export const StepperProgress = ({currentStep, currentStepTitle, totalSteps, styl
   )
 }
 
+const getStepperLogo = (step: number, empty: boolean) => {
+  if (step === 1) {
+    return Number1
+  }
+
+  if (step === 2 && empty) {
+    return Number2Empty
+  }
+
+  if (step === 2) {
+    return Number2
+  }
+
+  if (step === 3 && empty) {
+    return Number3Empty
+  }
+
+  if (step === 3) {
+    return Number3
+  }
+
+  if (step === 4 && empty) {
+    return Number4Empty
+  }
+
+  if (step === 4) {
+    return Number4
+  }
+
+  return null
+}
+
 const useStyles = () => {
   const {theme} = useTheme()
   const styles = StyleSheet.create({
@@ -94,13 +132,6 @@ const useStyles = () => {
     },
     isPrevious: {
       backgroundColor: theme.color.primary[300],
-    },
-    stepNumber: {
-      ...theme.typography['body-2-m-medium'],
-      color: theme.color.primary[100],
-    },
-    isNextNumber: {
-      color: theme.color.gray[400],
     },
     currentStepTitle: {
       ...theme.typography['body-1-l-medium'],
