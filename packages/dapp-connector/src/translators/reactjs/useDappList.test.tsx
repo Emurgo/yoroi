cca9342c33a19a0f01e36e95174445a251272bf4
Michal <miso.szorad@gmail.com>
2024-04-25 12:51:14 +0200

    Use queryClientFixture

--- packages/dapp-connector/src/translators/reactjs/useDappList.test.tsx
+++ packages/dapp-connector/src/translators/reactjs/useDappList.test.tsx
@@ -1,21 +1,12 @@
 import {useDappList} from './useDappList'
 import {renderHook, waitFor} from '@testing-library/react-native'
 import * as React from 'react'
-import {QueryClient, QueryClientProvider} from 'react-query'
+import {QueryClientProvider} from 'react-query'
+import {queryClientFixture} from '@yoroi/common'
 
 describe('useDappList', () => {
   it('should return list of dapps and filters', async () => {
-    const client = new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-          cacheTime: 0,
-        },
-        mutations: {
-          retry: false,
-        },
-      },
-    })
+    const client = queryClientFixture()
 
     const wrapper = (props: React.PropsWithChildren) => <QueryClientProvider {...props} client={client} />
     const {result} = renderHook(() => useDappList(), {wrapper})
