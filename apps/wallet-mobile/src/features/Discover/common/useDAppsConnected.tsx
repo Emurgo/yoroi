a33ec7109f93563fe89b9d35843d4d3df1d363c7
Michal <miso.szorad@gmail.com>
2024-04-25 12:50:08 +0200

    refactor select

--- apps/wallet-mobile/src/features/Discover/common/useDAppsConnected.tsx
+++ apps/wallet-mobile/src/features/Discover/common/useDAppsConnected.tsx
@@ -1,9 +1,10 @@
 import {useDappConnector} from '@yoroi/dapp-connector'
+import {DappConnection} from '@yoroi/dapp-connector/src/adapters/async-storage'
 import {useQuery, UseQueryOptions} from 'react-query'
 
 import {useSelectedWallet} from '../../../features/WalletManager/Context'
 
-export const useDAppsConnected = (options?: UseQueryOptions<string[], Error, string[], [string, string]>) => {
+export const useDAppsConnected = (options?: UseQueryOptions<DappConnection[], Error, string[], [string, string]>) => {
   const wallet = useSelectedWallet()
   const {manager} = useDappConnector()
 
@@ -11,8 +12,8 @@ export const useDAppsConnected = (options?: UseQueryOptions<string[], Error, str
     suspense: true,
     ...options,
     queryKey: [wallet.id, 'dappsConnected'],
-    queryFn: async () => {
-      const connections = await manager.listAllConnections()
+    queryFn: () => manager.listAllConnections(),
+    select: (connections) => {
       const currentWalletConnections = connections.filter((c) => c.walletId === wallet.id)
       return currentWalletConnections.map((c) => c.dappOrigin)
     },
