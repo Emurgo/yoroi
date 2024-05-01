44c01410194cd0ef34627a09548c61e78adf1192
banklesss <105349292+banklesss@users.noreply.github.com>
2024-05-01 10:15:11 +0200

    fix(wallet-setup): mnemonic input (#3222)

--- apps/wallet-mobile/src/features/SetupWallet/useCases/RestoreWallet/RestoreWalletScreen.tsx
+++ apps/wallet-mobile/src/features/SetupWallet/useCases/RestoreWallet/RestoreWalletScreen.tsx
@@ -80,6 +80,7 @@ export const RestoreWalletScreen = () => {
 
     if (mnemonicWordsComplete && !isValid) {
       setIsValidPhrase(false)
+      setMnemonic(newWords.join(' '))
       track.restoreWalletEnterPhraseStepStatus({recovery_prhase_status: false})
 
       return
