7ad0dca264d822f3d876e321d3932c31cc12e613
Sorin Chis <kish.sorin@yahoo.com>
2024-05-07 13:27:51 +0300

    add dark theme announcement theme screen

---
+++ apps/wallet-mobile/src/features/SetupWallet/useCases/DarkThemeAnnouncement/DarkThemeAnnouncement.stories.tsx
@@ -0,0 +1,26 @@
+import {storiesOf} from '@storybook/react-native'
+import {useTheme} from '@yoroi/theme'
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+import {DarkThemeAnnouncement} from './DarkThemeAnnouncement'
+
+storiesOf('Inital DarkThemeAnnouncementScreen', module)
+  .addDecorator((story) => {
+    const styles = useStyles()
+    return <View style={styles.container}>{story()}</View>
+  })
+  .add('initial', () => <DarkThemeAnnouncement />)
+
+const useStyles = () => {
+  const {color} = useTheme()
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: color.gray_cmin,
+    },
+  })
+
+  return styles
+}
---
+++ apps/wallet-mobile/src/features/SetupWallet/useCases/DarkThemeAnnouncement/DarkThemeAnnouncement.tsx
@@ -0,0 +1,120 @@
+import {useTheme} from '@yoroi/theme'
+import React from 'react'
+import {defineMessages, useIntl} from 'react-intl'
+import {StyleSheet, Switch, View} from 'react-native'
+import {ScrollView} from 'react-native-gesture-handler'
+
+import {Button, Text} from '../../../../components'
+import {Space} from '../../../../components/Space/Space'
+import {ThemeIlustration} from '../../illustrations/ThemeIlustration'
+
+export const DarkThemeAnnouncement = () => {
+  const styles = useStyles()
+  const strings = useStrings()
+  const {isLight, selectThemeName} = useTheme()
+
+  const scrollViewRef = React.useRef<ScrollView | null>(null)
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      scrollViewRef.current?.flashScrollIndicators()
+    }, 500)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <ScrollView bounces={false} ref={scrollViewRef} persistentScrollbar={true} showsVerticalScrollIndicator={true}>
+        <View style={styles.content}>
+          <Space height="md" />
+
+          <ThemeIlustration />
+
+          <Space height="_2xl" />
+
+          <Text style={styles.title}>{strings.header}</Text>
+
+          <Text style={styles.description}>{strings.description}</Text>
+
+          <View style={styles.toggle}>
+            <Switch
+              value={isLight === false}
+              onValueChange={() => selectThemeName(isLight === true ? 'default-dark' : 'default-light')}
+            />
+          </View>
+
+          <Text style={styles.caption}>{strings.changeTheme}</Text>
+        </View>
+      </ScrollView>
+
+      <Button title={strings.continue} shelleyTheme />
+    </View>
+  )
+}
+
+const useStyles = () => {
+  const {color, atoms} = useTheme()
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: color.gray_cmin,
+    },
+    content: {
+      alignItems: 'center',
+      paddingTop: 60,
+      ...atoms.body_1_lg_regular,
+    },
+    title: {
+      ...atoms.heading_3_medium,
+      textAlign: 'center',
+    },
+    description: {
+      ...atoms.body_1_lg_regular,
+      color: color.el_gray_medium,
+      textAlign: 'center',
+      ...atoms.py_sm,
+    },
+    caption: {
+      color: color.el_gray_medium,
+      textAlign: 'center',
+      ...atoms.body_3_sm_regular,
+    },
+
+    toggle: {
+      paddingBottom: 44,
+    },
+  })
+
+  return styles
+}
+
+const useStrings = () => {
+  const intl = useIntl()
+  return {
+    header: intl.formatMessage(messages.header),
+    description: intl.formatMessage(messages.description),
+    changeTheme: intl.formatMessage(messages.changeTheme),
+    continue: intl.formatMessage(messages.continue),
+  }
+}
+
+const messages = defineMessages({
+  header: {
+    id: 'walletinit.theme.tryDarkTheme',
+    defaultMessage: '!!!Try Yoroi dark theme',
+  },
+  description: {
+    id: 'walletinit.theme.description',
+    defaultMessage:
+      '!!!Press the theme switcher and dive into the new stylish theme crafted to enhance your Cardano wallet experience',
+  },
+  changeTheme: {
+    id: 'walletinit.theme.chanageTheme',
+    defaultMessage: '!!!Anonymous analytics data',
+  },
+  continue: {
+    id: 'components.walletinit.walletform.continueButton',
+    defaultMessage: '!!!Continue',
+  },
+})
