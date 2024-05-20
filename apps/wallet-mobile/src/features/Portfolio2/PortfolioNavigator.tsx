6bf7b5b58caa343fd63b9e6996b6290baacd413e
hoailp <phuochoaileqn@gmail.com>
2024-05-20 15:46:09 +0700

    feat: init portfolio navigator

---
+++ apps/wallet-mobile/src/features/Portfolio2/PortfolioNavigator.tsx
@@ -0,0 +1,32 @@
+import {createStackNavigator} from '@react-navigation/stack'
+import {useTheme} from '@yoroi/theme'
+import * as React from 'react'
+
+import {defaultStackNavigationOptions, Portfolio2Routes} from '../../navigation'
+import {PortfolioDashboardScreen} from './useCases/PortfolioDashboard/PortfolioDashboardScreen'
+import {PortfolioTokenDetailsScreen} from './useCases/PortfolioTokenDetails/PortfolioTokenDetailsScreen'
+import {PortfolioTokensListScreen} from './useCases/PortfolioTokensList/PortfolioTokensListScreen'
+
+const Stack = createStackNavigator<Portfolio2Routes>()
+
+export const PortfolioNavigator = () => {
+  const {atoms, color} = useTheme()
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        ...defaultStackNavigationOptions(atoms, color),
+        headerLeft: () => null,
+        detachPreviousScreen: false /* https://github.com/react-navigation/react-navigation/issues/9883 */,
+        gestureEnabled: true,
+      }}
+      initialRouteName="portfolio-dashboard"
+    >
+      <Stack.Screen name="portfolio-dashboard" component={PortfolioDashboardScreen} />
+
+      <Stack.Screen name="portfolio-tokens-list" component={PortfolioTokensListScreen} />
+
+      <Stack.Screen name="portfolio-token-details" component={PortfolioTokenDetailsScreen} />
+    </Stack.Navigator>
+  )
+}
