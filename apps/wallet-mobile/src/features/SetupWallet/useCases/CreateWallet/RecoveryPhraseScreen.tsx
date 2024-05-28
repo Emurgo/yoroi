98ffde503f95700740acf2a3d3e3be18c8de2731
Rahul <rahulnair87@gmail.com>
2024-05-28 16:05:16 +0530

    lint

--- apps/wallet-mobile/src/features/SetupWallet/useCases/CreateWallet/RecoveryPhraseScreen.tsx
+++ apps/wallet-mobile/src/features/SetupWallet/useCases/CreateWallet/RecoveryPhraseScreen.tsx
@@ -71,7 +71,7 @@ export const RecoveryPhraseScreen = () => {
             closeModal()
             showCreateWalletInfoModalChanged(false)
           }}
-          testId='setup-step2-continue-button'
+          testId="setup-step2-continue-button"
         />
 
         <Space height="_2xl" />
@@ -107,7 +107,7 @@ export const RecoveryPhraseScreen = () => {
         <Text style={styles.title}>
           {strings.recoveryPhraseTitle(bold)}
 
-          <Info onPress={handleOnShowModal} testId='step2-info-icon' />
+          <Info onPress={handleOnShowModal} testId="step2-info-icon" />
         </Text>
 
         <View style={styles.mnemonicWords}>
@@ -135,7 +135,12 @@ export const RecoveryPhraseScreen = () => {
           ))}
         </View>
 
-        <TouchableOpacity activeOpacity={0.5} style={styles.blurButton} onPress={() => setIsBlur(!isBlur)} testID="step2-show_hide-recovery-phrase-button">
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.blurButton}
+          onPress={() => setIsBlur(!isBlur)}
+          testID="step2-show_hide-recovery-phrase-button"
+        >
           {isBlur ? <EyeOpenIllustration /> : <EyeClosedIllustration />}
 
           <Text style={styles.blurTextButton}>
@@ -154,7 +159,7 @@ export const RecoveryPhraseScreen = () => {
           mnemonicChanged(mnemonic)
           navigation.navigate('setup-wallet-verify-recovery-phrase-mnemonic')
         }}
-        testId='setup-step2-next-button'
+        testId="setup-step2-next-button"
       />
 
       <Space height="lg" />
@@ -162,12 +167,12 @@ export const RecoveryPhraseScreen = () => {
   )
 }
 
-const Info = ({onPress, testId}: {onPress: () => void, testId?:string}) => {
+const Info = ({onPress, testId}: {onPress: () => void; testId?: string}) => {
   const {styles} = useStyles()
   return (
     <TouchableOpacity style={styles.info} onPress={onPress}>
-      <View style={styles.infoIcon} testID={testId} >
-        <InfoIcon size={24}/>
+      <View style={styles.infoIcon} testID={testId}>
+        <InfoIcon size={24} />
       </View>
     </TouchableOpacity>
   )
