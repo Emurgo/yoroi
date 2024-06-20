27660a2377ae7e639ef8691496bad7de33b87037
hoailp <phuochoaileqn@gmail.com>
2024-06-20 14:40:28 +0700

    feat: handle navigate from portfolio to send tokens screen and swap tokens screen

--- apps/wallet-mobile/src/features/Portfolio/common/useNavigateTo.tsx
+++ apps/wallet-mobile/src/features/Portfolio/common/useNavigateTo.tsx
@@ -12,6 +12,8 @@ export const useNavigateTo = () => {
     tokenDetail: (params: PortfolioTokenDetailParams) =>
       navigation.navigate('portfolio-token-details', {id: params.id}),
     nftsList: () => navigation.navigate('nfts'),
+    send: () => navigation.navigate('history', {screen: 'send-start-tx'}),
+    swap: () => navigation.navigate('history', {screen: 'swap-start-swap', params: {screen: 'token-swap'}}),
   } as const).current
 }
 
