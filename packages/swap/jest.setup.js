36a49af60785f1e1c43fd279694aac25c4b06a55
banklesss <105349292+banklesss@users.noreply.github.com>
2024-06-27 01:14:40 +0200

    fix(swap): increase test timeout (#3334)

--- packages/swap/jest.setup.js
+++ packages/swap/jest.setup.js
@@ -2,4 +2,4 @@ jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 )
 
-jest.setTimeout(30000)
+jest.setTimeout(80000)
