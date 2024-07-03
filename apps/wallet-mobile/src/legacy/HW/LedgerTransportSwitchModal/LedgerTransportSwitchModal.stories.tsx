62948e8ce7d203904dcd625c829102a682cffe6a
jorbuedo <jorbuedo@gmail.com>
2024-07-03 20:00:41 +0200

    Fix story

--- apps/wallet-mobile/src/legacy/HW/LedgerTransportSwitchModal/LedgerTransportSwitchModal.stories.tsx
+++ apps/wallet-mobile/src/legacy/HW/LedgerTransportSwitchModal/LedgerTransportSwitchModal.stories.tsx
@@ -2,17 +2,12 @@ import {storiesOf} from '@storybook/react-native'
 import React from 'react'
 
 import {WithModalProps} from '../../../../.storybook/decorators'
-import {LedgerTransportSwitchModal} from './LedgerTransportSwitchModal'
+import {LedgerTransportSwitch} from './LedgerTransportSwitchModal'
 
 storiesOf('Ledger Transport Switch Modal', module).add('default', () => (
   <WithModalProps>
     {({onPress, ...props}) => (
-      <LedgerTransportSwitchModal
-        {...props}
-        showCloseIcon
-        onSelectUSB={onPress('USB selected')}
-        onSelectBLE={onPress('BLE selected')}
-      />
+      <LedgerTransportSwitch {...props} onSelectUSB={onPress('USB selected')} onSelectBLE={onPress('BLE selected')} />
     )}
   </WithModalProps>
 ))
