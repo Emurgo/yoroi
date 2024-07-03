2185043b3125b8597f06a3e3c605b07516ed1de3
Javier Bueno <javierbueno@emurgo.io>
2024-07-03 20:25:14 +0200

    fix(dashboard): timer alignment

--- apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/EditPool/SelectPoolFromListScreen/SelectPoolFromListScreen.tsx
+++ apps/wallet-mobile/src/features/Swap/useCases/StartSwapScreen/CreateOrder/EditPool/SelectPoolFromListScreen/SelectPoolFromListScreen.tsx
@@ -1,6 +1,7 @@
 import {useSwap} from '@yoroi/swap'
+import {useTheme} from '@yoroi/theme'
 import React from 'react'
-import {ScrollView} from 'react-native'
+import {ScrollView, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {Counter} from '../../../../../common/Counter/Counter'
@@ -10,13 +11,14 @@ import {useStrings} from '../../../../../common/strings'
 export const SelectPoolFromListScreen = () => {
   const strings = useStrings()
   const {orderData} = useSwap()
+  const styles = useStyles()
 
   const {pools} = orderData
 
   const poolCounter = Array.isArray(pools) ? pools.length : 0
 
   return (
-    <SafeAreaView style={{flex: 1}} edges={['left', 'right', 'bottom']}>
+    <SafeAreaView style={styles.root} edges={['left', 'right', 'bottom']}>
       <ScrollView>
         <SelectPoolFromList pools={pools} />
       </ScrollView>
@@ -24,4 +26,17 @@ export const SelectPoolFromListScreen = () => {
       <Counter counter={poolCounter} unitsText={strings.pools(poolCounter)} closingText={strings.available} />
     </SafeAreaView>
   )
+}
+
+const useStyles = () => {
+  const {color} = useTheme()
+
+  const styles = StyleSheet.create({
+    root: {
+      flex: 1,
+      backgroundColor: color.gray_cmin,
+    },
+  })
+
+  return styles
 }
