38147646834c58767087ac9b49c15bc41ceb7b6e
Javier Bueno <javierbueno@emurgo.io>
2024-07-08 18:12:21 +0200

    fix(swap): pool card shador

--- apps/wallet-mobile/src/features/Swap/common/SelectPool/SelectPoolFromList/SelectPoolFromList.tsx
+++ apps/wallet-mobile/src/features/Swap/common/SelectPool/SelectPoolFromList/SelectPoolFromList.tsx
@@ -30,7 +30,6 @@ export const SelectPoolFromList = ({pools = []}: Props) => {
   const navigate = useNavigateTo()
   const {track} = useMetrics()
   const {styles, colors} = useStyles()
-  const {isDark} = useTheme()
 
   const sellTokenInfo = useTokenInfo({wallet, tokenId: orderData.amounts.sell.tokenId})
   const buyTokenInfo = useTokenInfo({wallet, tokenId: orderData.amounts.buy.tokenId})
@@ -68,7 +67,7 @@ export const SelectPoolFromList = ({pools = []}: Props) => {
           <View key={pool.poolId}>
             <Spacer height={16} />
 
-            <View style={[isDark ? undefined : styles.shadowProp]}>
+            <View style={[isSelectedPool ? undefined : styles.shadowProp]}>
               <LinearGradient
                 colors={isSelectedPool ? colors.gradientColor : [colors.white, colors.white]}
                 style={styles.linearGradient}
