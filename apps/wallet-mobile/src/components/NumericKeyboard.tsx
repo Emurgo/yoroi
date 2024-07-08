adb8cfe1fc73a1a74683a2ce23764de8b8cab677
banklesss <105349292+banklesss@users.noreply.github.com>
2024-07-08 10:11:55 +0200

    refactor(login): keyboard redesign (#3374)

--- apps/wallet-mobile/src/components/NumericKeyboard.tsx
+++ apps/wallet-mobile/src/components/NumericKeyboard.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable react/jsx-newline */
 import {useTheme} from '@yoroi/theme'
 import React from 'react'
 import {StyleSheet, TouchableHighlight, View, ViewProps} from 'react-native'
 
 import {Backspace} from './Icon/Backspace'
+import {Spacer} from './Spacer'
 import {Text} from './Text'
 
 export const BACKSPACE = 'BACKSPACE'
@@ -14,25 +14,55 @@ export const NumericKeyboard = ({onKeyDown}: {onKeyDown: (key: string) => void})
     <View style={styles.keyboard}>
       <Row>
         <Key value="1" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="2" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="3" onPress={onKeyDown} />
       </Row>
 
+      <Spacer height={7} />
+
       <Row>
         <Key value="4" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="5" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="6" onPress={onKeyDown} />
       </Row>
 
+      <Spacer height={7} />
+
       <Row>
         <Key value="7" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="8" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <Key value="9" onPress={onKeyDown} />
       </Row>
 
+      <Spacer height={7} />
+
       <Row>
         <EmptyKey />
+
+        <Spacer width={5} />
+
         <Key value="0" onPress={onKeyDown} />
+
+        <Spacer width={5} />
+
         <BackspaceKey onPress={onKeyDown} />
       </Row>
     </View>
@@ -63,14 +93,14 @@ const Key = ({value, onPress}: KeyboardKeyProps) => {
 
 const EmptyKey = () => {
   const {styles} = useStyles()
-  return <View style={[styles.keyboardKey, styles.keyboardKeyDisabled]} testID="pinKeyEmpty" />
+  return <View style={styles.emptyKey} testID="pinKeyEmpty" />
 }
 
 const BackspaceKey = ({onPress}: {onPress: (value: string) => void}) => {
   const {styles, color} = useStyles()
   return (
     <TouchableHighlight
-      style={[styles.keyboardKey, styles.keyboardKeyDisabled]}
+      style={styles.backspaceKey}
       onPress={() => onPress('BACKSPACE')}
       underlayColor="#bbbbbb"
       testID="pinKeyBACKSPACE"
@@ -86,33 +116,50 @@ const Row = (props: ViewProps) => {
 }
 
 const useStyles = () => {
-  const {color} = useTheme()
+  const {color, isDark} = useTheme()
 
   const styles = StyleSheet.create({
     keyboard: {
-      height: 250,
-      backgroundColor: color.gray_cmin,
+      height: 290,
+      padding: 5,
+      backgroundColor: isDark ? '#222324' : '#D1D5DB',
     },
     keyboardRow: {
       flex: 1,
+      maxHeight: 50,
       flexDirection: 'row',
     },
     keyboardKey: {
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
-      borderTopWidth: 2 * StyleSheet.hairlineWidth,
+      borderTopWidth: StyleSheet.hairlineWidth,
       borderLeftWidth: StyleSheet.hairlineWidth,
       borderRightWidth: StyleSheet.hairlineWidth,
-      borderColor: color.gray_c400,
-    },
-    keyboardKeyDisabled: {
-      backgroundColor: color.gray_c100,
+      borderBottomWidth: 3 * StyleSheet.hairlineWidth,
+      borderColor: 'rgba(0, 0, 0, 0.30)',
+      borderRadius: 5,
+      backgroundColor: isDark ? '#6B6B6B' : color.white_static,
     },
     keyboardKeyText: {
       fontSize: 30,
       lineHeight: 35,
       textAlign: 'center',
+    },
+    backspaceKey: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 5,
+    },
+    keyboardKeyDisabled: {
+      backgroundColor: color.gray_c100,
+    },
+    emptyKey: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 5,
     },
   })
 
