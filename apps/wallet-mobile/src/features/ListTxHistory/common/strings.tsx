b5198864036387f8a1f57b06afa5d8a1a55c513e
banklesss <105349292+banklesss@users.noreply.github.com>
2024-07-12 16:25:07 +0200

    refactor(tx-details): revamp + dark theme (#3396)
    Co-authored-by: jorbuedo <jorbuedo@gmail.com>

--- apps/wallet-mobile/src/features/ListTxHistory/common/strings.tsx
+++ apps/wallet-mobile/src/features/ListTxHistory/common/strings.tsx
@@ -37,10 +37,91 @@ export const useStrings = () => {
     staking: intl.formatMessage(messages.staking),
     addessModalTitle: intl.formatMessage(messages.addessModalTitle),
     verifyLabel: intl.formatMessage(messages.verifyLabel),
+    txDetailsFee: intl.formatMessage(messages.txDetailsFee),
+    fromAddresses: intl.formatMessage(messages.fromAddresses),
+    toAddresses: intl.formatMessage(messages.toAddresses),
+    memo: intl.formatMessage(messages.memo),
+    transactionId: intl.formatMessage(messages.transactionId),
+    txAssuranceLevel: intl.formatMessage(messages.txAssuranceLevel),
+    confirmations: (cnt: number) => intl.formatMessage(messages.confirmations, {cnt}),
+    omittedCount: (cnt: number) => intl.formatMessage(messages.omittedCount, {cnt}),
+    openInExplorer: intl.formatMessage(messages.openInExplorer),
+    SENT: intl.formatMessage(txTypeMessages.SENT),
+    RECEIVED: intl.formatMessage(txTypeMessages.RECEIVED),
+    SELF: intl.formatMessage(txTypeMessages.SELF),
+    MULTI: intl.formatMessage(txTypeMessages.MULTI),
+    assetsLabel: intl.formatMessage(globalMessages.assetsLabel),
   }
 }
 
-const messages = defineMessages({
+export const txTypeMessages = defineMessages({
+  SENT: {
+    id: 'components.txhistory.txdetails.txTypeSent',
+    defaultMessage: '!!!Sent funds',
+  },
+  RECEIVED: {
+    id: 'components.txhistory.txdetails.txTypeReceived',
+    defaultMessage: '!!!Received funds',
+  },
+  SELF: {
+    id: 'components.txhistory.txdetails.txTypeSelf',
+    defaultMessage: '!!!Intrawallet transaction',
+  },
+  MULTI: {
+    id: 'components.txhistory.txdetails.txTypeMulti',
+    defaultMessage: '!!!Multi-party transaction',
+  },
+})
+
+export const messages = defineMessages({
+  addressPrefixReceive: {
+    id: 'components.txhistory.txdetails.addressPrefixReceive',
+    defaultMessage: '!!!/{idx}',
+  },
+  addressPrefixChange: {
+    id: 'components.txhistory.txdetails.addressPrefixChange',
+    defaultMessage: '!!!/change',
+  },
+  addressPrefixNotMine: {
+    id: 'components.txhistory.txdetails.addressPrefixNotMine',
+    defaultMessage: '!!!not mine',
+  },
+  txDetailsFee: {
+    id: 'components.txhistory.txdetails.fee',
+    defaultMessage: '!!!Fee: ',
+  },
+  fromAddresses: {
+    id: 'components.txhistory.txdetails.fromAddresses',
+    defaultMessage: '!!!From Addresses',
+  },
+  toAddresses: {
+    id: 'components.txhistory.txdetails.toAddresses',
+    defaultMessage: '!!!To Addresses',
+  },
+  memo: {
+    id: 'components.txhistory.txdetails.memo',
+    defaultMessage: '!!!Memo',
+  },
+  transactionId: {
+    id: 'components.txhistory.txdetails.transactionId',
+    defaultMessage: '!!!Transaction ID',
+  },
+  txAssuranceLevel: {
+    id: 'components.txhistory.txdetails.txAssuranceLevel',
+    defaultMessage: '!!!Transaction assurance level',
+  },
+  confirmations: {
+    id: 'components.txhistory.txdetails.confirmations',
+    defaultMessage: '!!!{cnt} {cnt, plural, one {CONFIRMATION} other {CONFIRMATIONS}}',
+  },
+  omittedCount: {
+    id: 'components.txhistory.txdetails.omittedCount',
+    defaultMessage: '!!!+ {cnt} omitted {cnt, plural, one {address} other {addresses}}',
+  },
+  openInExplorer: {
+    id: 'global.openInExplorer',
+    defaultMessage: '!!!Open in explorer',
+  },
   title: {
     id: 'components.txhistory.txhistory.title',
     defaultMessage: '!!!Transactions',
