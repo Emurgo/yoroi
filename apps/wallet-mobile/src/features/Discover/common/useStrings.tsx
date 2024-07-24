b4a7d0926bf08137c2200fce6d789059a88a2b0a
Michal <miso.szorad@gmail.com>
2024-07-24 14:50:18 +0100

    Add translations

--- apps/wallet-mobile/src/features/Discover/common/useStrings.tsx
+++ apps/wallet-mobile/src/features/Discover/common/useStrings.tsx
@@ -37,14 +37,14 @@ export const useStrings = () => {
     signTransaction: intl.formatMessage(txLabels.signTransaction),
     signDataNotSupported: intl.formatMessage(messages.signDataNotSupported),
     continueOnLedger: intl.formatMessage(ledgerMessages.continueOnLedger),
-    transactionReview: 'Transaction review',
-    inputs: 'Inputs',
-    outputs: 'Outputs',
-    transactionIdCopied: 'Transaction ID Copied',
-    addressCopied: 'Address Copied',
-    yourAddress: 'Your address',
-    externalAddress: 'Foreign address',
-    fee: 'Transaction fee',
+    transactionReview: intl.formatMessage(messages.transactionReview),
+    inputs: intl.formatMessage(messages.inputs),
+    outputs: intl.formatMessage(messages.outputs),
+    transactionIdCopied: intl.formatMessage(messages.transactionIdCopied),
+    addressCopied: intl.formatMessage(messages.addressCopied),
+    yourAddress: intl.formatMessage(messages.yourAddress),
+    externalAddress: intl.formatMessage(messages.externalAddress),
+    fee: intl.formatMessage(messages.fee),
   }).current
 }
 
@@ -163,6 +163,38 @@ export const messages = Object.freeze(
     signDataNotSupported: {
       id: 'discover.ledger.signDataNotSupported',
       defaultMessage: '!!!The Ledger Cardano app does not support data signing at this moment.',
+    },
+    transactionReview: {
+      id: 'discover.reviewTransaction.transactionReview',
+      defaultMessage: '!!!Transaction review',
+    },
+    inputs: {
+      id: 'discover.reviewTransaction.inputs',
+      defaultMessage: '!!!Inputs',
+    },
+    outputs: {
+      id: 'discover.reviewTransaction.outputs',
+      defaultMessage: '!!!Outputs',
+    },
+    transactionIdCopied: {
+      id: 'discover.reviewTransaction.transactionIdCopied',
+      defaultMessage: '!!!Transaction ID copied',
+    },
+    addressCopied: {
+      id: 'discover.reviewTransaction.addressCopied',
+      defaultMessage: '!!!Address copied',
+    },
+    yourAddress: {
+      id: 'discover.reviewTransaction.yourAddress',
+      defaultMessage: '!!!Your address',
+    },
+    externalAddress: {
+      id: 'discover.reviewTransaction.externalAddress',
+      defaultMessage: '!!!Foreign address',
+    },
+    fee: {
+      id: 'discover.reviewTransaction.fee',
+      defaultMessage: '!!!Transaction fee',
     },
   }),
 )
