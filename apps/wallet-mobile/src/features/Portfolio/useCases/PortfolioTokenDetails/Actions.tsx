e0ab331268f46c1ea00bdc8d5f717e08f7409887
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-08-14 11:10:23 +0100

    Update apps/wallet-mobile/src/features/Portfolio/useCases/PortfolioTokenDetails/Actions.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/src/features/Portfolio/useCases/PortfolioTokenDetails/Actions.tsx
+++ apps/wallet-mobile/src/features/Portfolio/useCases/PortfolioTokenDetails/Actions.tsx
@@ -21,7 +21,6 @@ export const Actions = ({tokenInfo}: Props) => {
 
   const handleOnSwap = () => {
     swapForm.resetSwapForm()
-    swap.resetState()
     swap.buyTokenInfoChanged(tokenInfo)
     swapForm.buyTouched()
     navigateTo.swap()
