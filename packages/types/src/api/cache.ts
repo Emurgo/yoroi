de91178c88a00548df2d63d0391b37344e6eaa52
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-08-16 16:45:13 +0100

    chore(types): new cache response for multi token - 500 with message

--- packages/types/src/api/cache.ts
+++ packages/types/src/api/cache.ts
@@ -3,5 +3,10 @@ import {ApiHttpStatusCode} from './status-code'
 export type ApiResponseRecordWithCache<T> =
   | [StatusCode: ApiHttpStatusCode.Ok, Record: T, ETag: string, MaxAge: number]
   | [StatusCode: ApiHttpStatusCode.NotModified, MaxAge: number]
+  | [
+      StatusCode: ApiHttpStatusCode.InternalServerError,
+      Reason: string,
+      MaxAge: number,
+    ]
 
 export type ApiRequestRecordWithCache<T> = [Record: T, ETag: string]
