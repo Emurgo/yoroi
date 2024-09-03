f249a242349f9d5aad23cbeeabbaf1a85fd6d882
jorbuedo <jorbuedo@gmail.com>
2024-09-03 15:38:10 +0200

    Fix app background color

--- apps/wallet-mobile/src/hooks/useStatusBar.ts
+++ apps/wallet-mobile/src/hooks/useStatusBar.ts
@@ -1,4 +1,5 @@
 import {ThemedPalette, useTheme} from '@yoroi/theme'
+import * as SystemUI from 'expo-system-ui'
 import * as React from 'react'
 import {Platform, StatusBar, StatusBarStyle} from 'react-native'
 
@@ -25,6 +26,15 @@ export const useStatusBar = (currentRouteName: string | undefined) => {
         StatusBar.setTranslucent(style.translucent)
       }
       style.statusBarStyle !== undefined && StatusBar.setBarStyle(style.statusBarStyle, true)
+    }
+    if (
+      ['history-list', 'dashboard-portfolio', 'discover-select-dapp-from-list', '_menu'].includes(
+        currentRouteName ?? '',
+      )
+    ) {
+      SystemUI.setBackgroundColorAsync(color.gray_min)
+    } else {
+      SystemUI.setBackgroundColorAsync(color.bg_color_max)
     }
   }, [currentRouteName, isDark, color])
 }
