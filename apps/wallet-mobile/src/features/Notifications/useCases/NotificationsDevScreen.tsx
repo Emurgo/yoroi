624206b750be41f30e00ee9fbe207775bf4455c5
Michal <miso.szorad@gmail.com>
2024-09-17 13:36:22 +0100

    Refactor

--- apps/wallet-mobile/src/features/Notifications/useCases/NotificationsDevScreen.tsx
+++ apps/wallet-mobile/src/features/Notifications/useCases/NotificationsDevScreen.tsx
@@ -1,46 +1,57 @@
+import {Notification, Notifications} from '@jamsinclair/react-native-notifications'
 import * as React from 'react'
 import {useEffect} from 'react'
 import {Text, View} from 'react-native'
-import {Notification, Notifications} from 'react-native-notifications'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {Button} from '../../../components'
 
-export const NotificationsDevScreen = () => {
+const useRequestPermissions = () => {
   useEffect(() => {
     Notifications.registerRemoteNotifications()
+  }, [])
+}
 
+const useSendNotification = () => {
+  useEffect(() => {
     const s = Notifications.events().registerNotificationReceivedForeground(
       (notification: Notification, completion) => {
         console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`)
         completion({alert: true, sound: true, badge: true})
       },
     )
-
     return () => {
       s.remove()
     }
   }, [])
 
-  const handleOnPress = () => {
-    // const notification = new Notification({
-    //   title: 'Test',
-    //   body: 'Test',
-    //   sound: 'default',
-    // })
-    // Notifications.postLocalNotification(notification.payload)
-
-    let localNotification = Notifications.postLocalNotification({
-      body: 'Local notification!',
-      title: 'Local Notification Title',
-      sound: 'chime.aiff',
+  const send = (title: string, body: string) => {
+    Notifications.postLocalNotification({
+      body,
+      title,
       silent: false,
       category: 'SOME_CATEGORY',
       userInfo: {},
       fireDate: new Date(),
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } as any)
+    // const notification = new Notification({
+    //   title,
+    //   body,
+    //   sound: 'default',
+    // })
+    // Notifications.postLocalNotification(notification.payload)
+  }
+  return {send}
+}
 
-    console.log('Notification posted')
+export const NotificationsDevScreen = () => {
+  const {send} = useSendNotification()
+
+  useRequestPermissions()
+
+  const handleOnPress = () => {
+    send('Random notification', 'This is a random notification')
   }
 
   return (
