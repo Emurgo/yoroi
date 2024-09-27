688feb1453e65d905ddf8979a4ee9a21e5cc786e
jorbuedo <jorbuedo@users.noreply.github.com>
2024-09-27 16:21:10 +0200

    feature: update catalyst min ada for fund 13 (#3661)

--- apps/wallet-mobile/src/features/WalletManager/common/adapters/cardano/catalyst-config.ts
+++ apps/wallet-mobile/src/features/WalletManager/common/adapters/cardano/catalyst-config.ts
@@ -3,6 +3,5 @@ import {freeze} from 'immer'
 import {cardanoConfig} from './cardano-config'
 
 export const catalystConfig = freeze({
-  minAda: 450n * cardanoConfig.denominations.ada,
-  displayedMinAda: 500n * cardanoConfig.denominations.ada,
+  minAda: 25n * cardanoConfig.denominations.ada,
 } as const)
