28cd39593cc891f0b51dbb110394819a084bec66
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-09-27 14:38:29 +0100

    wip: core packages

--- apps/wallet/jest.config.js
+++ apps/wallet/jest.config.js
@@ -2,9 +2,26 @@ module.exports = {
   preset: 'react-native',
   collectCoverage: true,
   collectCoverageFrom: [
+    'App.tsx',
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.stories.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
   ],
   coverageReporters: ['text-summary', 'html'],
-};
+  globals: {
+    'ts-jest': {
+      astTransformers: {
+        before: [
+          {
+            path: '@formatjs/ts-transformer/ts-jest-integration',
+            options: {
+              // options
+              overrideIdFn: '[sha512:contenthash:base64:6]',
+              ast: true,
+            },
+          },
+        ],
+      },
+    },
+  },
+}
