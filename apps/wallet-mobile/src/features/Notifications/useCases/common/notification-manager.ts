3ef4ef0b51575455924114bf3e41436064737682
Michal <miso.szorad@gmail.com>
2024-10-10 10:34:08 +0100

    Update storage path

--- apps/wallet-mobile/src/features/Notifications/useCases/common/notification-manager.ts
+++ apps/wallet-mobile/src/features/Notifications/useCases/common/notification-manager.ts
@@ -6,10 +6,11 @@ import {displayNotificationEvent} from './notifications'
 import {transactionReceivedSubject} from './transaction-received-notification'
 
 const appStorage = mountAsyncStorage({path: '/'})
+const notificationStorage = appStorage.join('notifications/')
 
 export const notificationManager = notificationManagerMaker({
-  eventsStorage: appStorage.join('events/'),
-  configStorage: appStorage.join('settings/'),
+  eventsStorage: notificationStorage.join('events/'),
+  configStorage: notificationStorage.join('settings/'),
   display: displayNotificationEvent,
   subscriptions: {
     [Notifications.Trigger.TransactionReceived]: transactionReceivedSubject,
