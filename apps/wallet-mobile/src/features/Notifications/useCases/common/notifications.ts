7abc5130fa93d5fcb6e13b188f690db3128c1c58
Michal <miso.szorad@gmail.com>
2024-10-30 15:33:49 +0000

    Fix import

--- apps/wallet-mobile/src/features/Notifications/useCases/common/notifications.ts
+++ apps/wallet-mobile/src/features/Notifications/useCases/common/notifications.ts
@@ -1,7 +1,6 @@
 import {Notification, Notifications} from '@jamsinclair/react-native-notifications'
 import {mountAsyncStorage} from '@yoroi/common'
 import {Notifications as NotificationTypes} from '@yoroi/types'
-import {NotificationTrigger} from '@yoroi/types/lib/typescript/notifications/manager'
 
 import {formatCurrency, getCurrencySymbol} from '../../../Settings/useCases/changeAppSettings/Currency/CurrencyContext'
 
@@ -38,7 +37,7 @@ export const displayNotificationEvent = async (notificationEvent: NotificationTy
     })
   }
 
-  if (notificationEvent.trigger === NotificationTrigger.RewardsUpdated) {
+  if (notificationEvent.trigger === NotificationTypes.Trigger.RewardsUpdated) {
     sendNotification({
       title: 'Rewards updated',
       body: 'Your rewards have been updated',
