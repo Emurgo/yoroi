2e4fe0bb1a9cbd2db3d3ab66a72c2fc1b14eff9b
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2024-11-29 09:47:13 +0000

    Update apps/wallet-mobile/src/legacy/Dashboard/ShowSubmittedTxScreen/SubmittedTxScreen.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/src/legacy/Dashboard/ShowSubmittedTxScreen/SubmittedTxScreen.tsx
+++ apps/wallet-mobile/src/legacy/Dashboard/ShowSubmittedTxScreen/SubmittedTxScreen.tsx
@@ -1,5 +1,5 @@
 import {useTheme} from '@yoroi/theme'
-import React from 'react'
+import * as React from 'react'
 import {defineMessages, useIntl} from 'react-intl'
 import {StyleSheet, Text, View} from 'react-native'
 
