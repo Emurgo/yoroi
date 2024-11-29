34b0165fd47a5cd40a070e37b17fbf81e739edae
jorbuedo <jorbuedo@gmail.com>
2024-11-29 10:47:11 +0100

    fix

--- apps/wallet-mobile/src/yoroi-wallets/cardano/usePrimaryTokenActivity.tsx
+++ apps/wallet-mobile/src/yoroi-wallets/cardano/usePrimaryTokenActivity.tsx
@@ -46,8 +46,8 @@ export const usePrimaryTokenActivity = ({
         // NOTE: transformer
         const tickers = response.value.data.tickers
         const ts = tickers[0]?.timestamp ?? 0
-        const close = tickers[0]?.prices[to] ?? 1
-        const open = tickers[1]?.prices[to] ?? 1
+        const close = tickers[0]?.prices[to] ?? 0
+        const open = tickers[1]?.prices[to] ?? 0
         return {
           ts,
           close,
