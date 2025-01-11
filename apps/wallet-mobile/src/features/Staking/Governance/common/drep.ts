4025efc08339e4e50f412fcb160116dd54033411
Michal <miso.szorad@gmail.com>
2025-01-11 20:37:09 +0100

    Remove logs

--- apps/wallet-mobile/src/features/Staking/Governance/common/drep.ts
+++ apps/wallet-mobile/src/features/Staking/Governance/common/drep.ts
@@ -3,8 +3,7 @@ import {convertDrepHashToCIP129Format} from '@yoroi/staking'
 export const formatDrepHash = (hash: string, kind: 'script' | 'key'): string => {
   try {
     return convertDrepHashToCIP129Format(hash, kind)
-  } catch (e) {
-    console.log('errorr', e)
+  } catch {
     return hash
   }
 }
