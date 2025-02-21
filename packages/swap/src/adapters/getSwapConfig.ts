acc241a6cdfda2866c9698eab6ba36684fcb105a
jorbuedo <jorbuedo@gmail.com>
2025-02-21 12:06:03 +0100

    PR comments

--- packages/swap/src/adapters/getSwapConfig.ts
+++ packages/swap/src/adapters/getSwapConfig.ts
@@ -1,18 +1,17 @@
 import {FetchData, fetchData, getApiError, isLeft} from '@yoroi/common'
 import {TokenIdSchema} from '@yoroi/portfolio'
-import {Portfolio} from '@yoroi/types'
+import {Api, Portfolio} from '@yoroi/types'
+import {freeze} from 'immer'
 import {z} from 'zod'
 
 type SwapConfig = z.infer<typeof SwapConfigResponseSchema>
+type Options = {
+  request: FetchData
+}
 
+const initialDeps = freeze({request: fetchData}, true)
 export const getSwapConfigApiMaker =
-  (
-    {
-      request,
-    }: {
-      request: FetchData
-    } = {request: fetchData},
-  ) =>
+  ({request}: Options = initialDeps) =>
   async (): Promise<SwapConfig> => {
     const response = await request<SwapConfig>({
       url: 'https://daehx1qv45z7c.cloudfront.net/swapConfig.json',
@@ -21,7 +20,7 @@ export const getSwapConfigApiMaker =
     if (isLeft(response)) throw getApiError(response.error)
 
     if (!SwapConfigResponseSchema.safeParse(response.value.data).success) {
-      throw new Error(
+      throw new Api.Errors.ResponseMalformed(
         'Invalid swap config response: ' + JSON.stringify(response.value.data),
       )
     }
