28191dca4827f8100811c3a5fe4c8ff24e824aa0
jorbuedo <jorbuedo@gmail.com>
2025-03-31 11:59:27 +0200

    remove log

--- apps/wallet-mobile/src/features/Swap/common/AmountCard/AmountCard.tsx
+++ apps/wallet-mobile/src/features/Swap/common/AmountCard/AmountCard.tsx
@@ -38,7 +38,6 @@ export const AmountCard = ({direction}: {direction: 'in' | 'out'}) => {
 
   const decimals = info?.decimals ?? 0
   const value = tokenInput.value
-  console.log({value, decimals: info?.decimals})
   const quantity = BigInt(Math.floor(Number(value ?? 0) * 10 ** (info?.decimals ?? 0)))
   const touched = tokenInput.isTouched
   const inputRef = direction === 'in' ? swapForm.tokenInInputRef : swapForm.tokenOutInputRef
