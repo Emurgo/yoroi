2704df97d6f18fe79334924d80a08207022d4779
jorbuedo <jorbuedo@gmail.com>
2025-04-07 11:06:51 +0200

    fix

---
+++ apps/wallet-mobile/src/features/Transactions/common/HeaderRightSwap.tsx
@@ -0,0 +1,18 @@
+import {useNavigation} from '@react-navigation/native'
+import {useTheme} from '@yoroi/theme'
+import React from 'react'
+import {TouchableOpacity} from 'react-native'
+
+import {Icon} from '../../../components/Icon'
+import {TxHistoryRouteNavigation} from '../../../kernel/navigation'
+
+export const HeaderRightSwap = React.memo(() => {
+  const navigation = useNavigation<TxHistoryRouteNavigation>()
+  const {color} = useTheme()
+
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('swap-orders')} style={{paddingRight: 8}}>
+      <Icon.TermsOfUse color={color.gray_max} size={24} />
+    </TouchableOpacity>
+  )
+})
