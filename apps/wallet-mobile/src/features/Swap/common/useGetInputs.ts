bef26703801a1b35dc44a460da5d73a581b5936e
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-05-08 12:55:09 +0100

    Update apps/wallet-mobile/src/features/Swap/common/useGetInputs.ts
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/wallet-mobile/src/features/Swap/common/useGetInputs.ts
+++ apps/wallet-mobile/src/features/Swap/common/useGetInputs.ts
@@ -12,7 +12,7 @@ export const useGetInputs = () => {
       const {csl, release} = getCSL()
 
       const result = await Promise.all(
-        ((await _getRequiredUtxos(csl, wallet, amounts, wallet.allUtxos, meta)) || []).map(async (u) => {
+        ((await _getRequiredUtxos(csl, wallet, amounts, wallet.utxos, meta)) || []).map(async (u) => {
           return Buffer.from(await u.toBytes()).toString('hex')
         }),
       )
