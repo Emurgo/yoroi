7c5f6e7faa23a706f9f560ee18b2e1ac61fcedad
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-06-03 09:52:49 +0100

    chore: safe-are-context

--- apps/mobile/PlatformShell.tsx
+++ apps/mobile/PlatformShell.tsx
@@ -1,5 +1,14 @@
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context'
+
 import {RouterContainer} from './src/kernel/navigation/Router'
 
 export function PlatformShell({children}: React.PropsWithChildren) {
-  return <RouterContainer>{children}</RouterContainer>
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <RouterContainer>{children}</RouterContainer>
+    </SafeAreaProvider>
+  )
 }
