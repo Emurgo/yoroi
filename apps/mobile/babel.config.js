a84160fd2d371f0ba956ff7626c25bef1d5c9935
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-06-03 12:42:59 +0100

    chore(mobile): added plugins + intl

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,24 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: [
+      '@babel/plugin-transform-export-namespace-from',
+      [
+        // Used by @yoroi/dapp-connector to create injectable JS code
+        'babel-plugin-show-source',
+        {
+          // directive needs to be changed from 'show source' when in use with hermes
+          directive: 'babel plugin show source',
+          removeFunction: true,
+        },
+      ],
+      [
+        "formatjs",
+        {
+          "idInterpolationPattern": "[sha512:contenthash:base64:6]",
+          "ast": true
+        }
+      ]
+    ],
   }
 }
