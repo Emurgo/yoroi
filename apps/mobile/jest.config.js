3d4a08281a131b25047101a91e29da8e81f9bd41
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-06-03 09:02:23 +0100

    chore: upds

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@emurgo/dullahan/.*|@sentry/.*)',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -1,17 +1,3 @@
-jest.mock("@react-native-async-storage/async-storage", () =>
-  require("@react-native-async-storage/async-storage/jest/async-storage-mock")
-);
-
-jest.mock('react-native/Libraries/Utilities/Platform', () => ({
-  OS: 'ios',
-  select: jest.fn((obj) => obj.ios),
-}));
-
-jest.mock('react-native', () => {
-  const RN = jest.requireActual('react-native');
-  RN.Platform = {
-    OS: 'ios',
-    select: jest.fn((obj) => obj.ios),
-  };
-  return RN;
-});
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+)
