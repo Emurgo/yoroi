9f554348e2e9522558618c7f5861a84ad66621c5
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-06-12 11:36:25 +0100

    chore(mobile): csl

--- apps/mobile/src/kernel/crypto/decrypt-data.test.ts
+++ apps/mobile/src/kernel/crypto/decrypt-data.test.ts
@@ -1,16 +1,15 @@
 import {decryptData} from './decrypt-data'
 
 describe('decryptData', () => {
-  it('can decrypt data', async () => {
-    let ciphertextHex = ''
+  it('can decrypt data', () => {
     const salt =
       '50515253c0c1c2c3c4c5c6c750515253c0c1c2c3c4c5c6c750515253c0c1c2c3'
     const nonce = '50515253c0c1c2c3c4c5c6c7'
     const payload = '308f9977d04e7f3a45abd148905c628e2bb2621360a585f352'
     const password = 'password'
-    ciphertextHex = ciphertextHex.concat(salt, nonce, payload)
+    const ciphertextHex = ''.concat(salt, nonce, payload)
 
-    const decryptedHex = await decryptData(ciphertextHex, password)
+    const decryptedHex = decryptData(ciphertextHex, password)
     expect(Buffer.from(decryptedHex, 'hex').toString('utf8')).toBe('masterkey')
   })
 })
