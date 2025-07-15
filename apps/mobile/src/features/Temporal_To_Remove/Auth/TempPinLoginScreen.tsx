3fb0281c10e0f74a5763100393c32270450999ac
banklesss <105349292+banklesss@users.noreply.github.com>
2025-07-15 16:20:41 +0200

    rn79: modal first version (#3979)
    Signed-off-by: banklesss <105349292+banklesss@users.noreply.github.com>

--- apps/mobile/src/features/Temporal_To_Remove/Auth/TempPinLoginScreen.tsx
+++ apps/mobile/src/features/Temporal_To_Remove/Auth/TempPinLoginScreen.tsx
@@ -8,7 +8,7 @@ export const TempPinLoginScreen = () => {
 
   const checkPin = (pin: string) => {
     if (pin === '000000') {
-      navigation.navigate('setup-wallet')
+      navigation.navigate('initial')
       return
     }
     Alert.alert('Incorret PIN')
---
+++ apps/mobile/src/features/Temporal_To_Remove/InitialScreen/InitialScreenNavigatorNavigator.tsx
@@ -0,0 +1,52 @@
+import {createStackNavigator} from '@react-navigation/stack'
+import {atoms as a, useTheme} from '@yoroi/theme'
+import * as React from 'react'
+
+import {useNavigation} from '@react-navigation/native'
+import {Button, Text, View} from 'react-native'
+import {SystemBars} from 'react-native-edge-to-edge'
+import {DevMenu} from '../../../../DevMenu'
+import {useModal} from '../../../ui/Modal/ModalContext'
+
+const Stack = createStackNavigator<any>()
+export const InitialScreenNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="root">{() => <InitialScreen />}</Stack.Screen>
+      <Stack.Screen name="dev">{() => <DevMenu visible={true} />}</Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
+const InitialScreen = () => {
+  const {openModal, isOpen, closeModal} = useModal()
+  const {isDark} = useTheme()
+  const navigation = useNavigation<any>()
+
+  const openTestModal = React.useCallback(() => {
+    if (isOpen) {
+      closeModal()
+      return
+    }
+
+    openModal({
+      content: (
+        <View style={{height: 500}}>
+          <Text>TESTTSTS</Text>
+        </View>
+      ),
+    })
+  }, [isOpen, openModal, closeModal])
+
+  const openDevMenu = React.useCallback(() => {
+    navigation.navigate('dev')
+  }, [navigation])
+
+  return (
+    <View style={[a.full_screen, a.flex_1, {backgroundColor: 'grey'}]}>
+      <SystemBars style={isDark ? 'light' : 'dark'} />
+      <Button title="Test Modal" onPress={openTestModal} />
+      <Button title="Dev Menu" onPress={openDevMenu} />
+    </View>
+  )
+}
