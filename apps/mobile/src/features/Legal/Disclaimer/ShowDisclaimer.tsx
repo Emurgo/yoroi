303f6b31abff412e5cd624a40b2827ade4899166
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-07-17 16:51:19 +0200

    Update apps/mobile/src/features/Legal/Disclaimer/ShowDisclaimer.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/mobile/src/features/Legal/Disclaimer/ShowDisclaimer.tsx
+++ apps/mobile/src/features/Legal/Disclaimer/ShowDisclaimer.tsx
@@ -1,6 +1,6 @@
 import {useNavigation} from '@react-navigation/native'
 import {atoms as a, useTheme} from '@yoroi/theme'
-import React from 'react'
+import * as React from 'react'
 import {useIntl} from 'react-intl'
 import {View} from 'react-native'
 
