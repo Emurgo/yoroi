5df3044b579809ad2815c759ea1d67fc2610be0c
banklesss <105349292+banklesss@users.noreply.github.com>
2025-07-18 18:56:46 +0200

    fix: restore metro config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -80,7 +80,7 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
 }
 
 // Fix axios
-config.resolver.unstable_conditionNames = [ 'browser', 'require', 'react-native', ]
+config.resolver.unstable_conditionNames = [ 'browser', 'require', 'react-native']
 
 // Add WASM support - treat as asset only
 config.resolver.assetExts.push('wasm')
