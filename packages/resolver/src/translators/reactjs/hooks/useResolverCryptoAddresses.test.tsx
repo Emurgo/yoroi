3ee28f442460b70b446a1ad8638e7a14e2daab89
Folarin <opefolarin1@gmail.com>
2025-08-07 17:51:50 +0100

    rn79: Update Resolver Package Added more test cases; Statements and Lines => 100%

--- packages/resolver/src/translators/reactjs/hooks/useResolverCryptoAddresses.test.tsx
+++ packages/resolver/src/translators/reactjs/hooks/useResolverCryptoAddresses.test.tsx
@@ -46,4 +46,38 @@ describe('useResolverCryptoAddresses', () => {
       mockResolverManager.crypto.getCardanoAddresses,
     ).toHaveBeenCalledTimes(1)
   })
+
+  it('error', async () => {
+    const TestResolverAddresses = () => {
+      const {data, error, isError} = useResolverCryptoAddresses({
+        resolve: domain,
+      })
+      return (
+        <View>
+          <Text testID="addresses">{JSON.stringify(data)}</Text>
+          <Text testID="error">{JSON.stringify(error)}</Text>
+          <Text testID="isError">{JSON.stringify(isError)}</Text>
+        </View>
+      )
+    }
+
+    const testError = new Error('Test error')
+    mockResolverManager.crypto.getCardanoAddresses = jest
+      .fn()
+      .mockRejectedValue(testError)
+
+    const wrapper = wrapperManagerFixture({
+      resolverManager: mockResolverManager,
+    })
+
+    const {getByTestId} = render(<TestResolverAddresses />, {wrapper})
+
+    await waitFor(() => {
+      expect(getByTestId('isError').props.children).toBe('true')
+    })
+
+    expect(
+      mockResolverManager.crypto.getCardanoAddresses,
+    ).toHaveBeenCalledTimes(1)
+  })
 })
