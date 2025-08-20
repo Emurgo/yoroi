3ce8f0c3967403b714bcc4f87512b8c2d36c61af
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-08-20 17:06:01 +0100

    Update apps/mobile/src/ui/ConfirmTxWithSpendingPasswordModal/ConfirmTxWithSpendingPasswordModal.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/mobile/src/ui/ConfirmTxWithSpendingPasswordModal/ConfirmTxWithSpendingPasswordModal.tsx
+++ apps/mobile/src/ui/ConfirmTxWithSpendingPasswordModal/ConfirmTxWithSpendingPasswordModal.tsx
@@ -1,6 +1,6 @@
 import {atoms as a, useTheme} from '@yoroi/theme'
 import {App} from '@yoroi/types'
-import React, {useEffect, useRef, useState} from 'react'
+import * as React from 'react'
 import {ActivityIndicator, TextInput as RNTextInput, View} from 'react-native'
 
 import {useStrings} from '../../kernel/i18n/useStrings'
