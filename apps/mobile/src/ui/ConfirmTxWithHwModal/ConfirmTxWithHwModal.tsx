acb55feb860cc2d485734b3d78bd0221c422fbea
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-08-20 17:05:43 +0100

    Update apps/mobile/src/ui/ConfirmTxWithHwModal/ConfirmTxWithHwModal.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- apps/mobile/src/ui/ConfirmTxWithHwModal/ConfirmTxWithHwModal.tsx
+++ apps/mobile/src/ui/ConfirmTxWithHwModal/ConfirmTxWithHwModal.tsx
@@ -1,6 +1,6 @@
 import {atoms as a, useTheme} from '@yoroi/theme'
 import {HW} from '@yoroi/types'
-import React, {useState} from 'react'
+import * as React from 'react'
 import {ErrorBoundary} from 'react-error-boundary'
 import {ActivityIndicator, View} from 'react-native'
 
