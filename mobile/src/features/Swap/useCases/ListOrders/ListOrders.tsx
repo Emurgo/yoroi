9d9b1d4ffa5be9c02ca93093c9e26cd771320f75
banklesss <105349292+banklesss@users.noreply.github.com>
2025-08-26 15:52:12 +0200

    Update mobile/src/features/Swap/useCases/ListOrders/ListOrders.tsx
    Co-authored-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
    Signed-off-by: banklesss <105349292+banklesss@users.noreply.github.com>

--- mobile/src/features/Swap/useCases/ListOrders/ListOrders.tsx
+++ mobile/src/features/Swap/useCases/ListOrders/ListOrders.tsx
@@ -152,7 +152,7 @@ const Content = ({filter}: {filter: Filter}) => {
   if (isTestnet) {
     return (
       <View style={[a.flex_1, a.gap_lg, a.justify_center, a.align_center]}>
-        <Text style={[a.heading_3_medium, {color: p.text_gray_medium}]}>
+        <Text style={[a.heading_3_medium, ta.text_gray_medium]}>
           Swap orders are not available on testnet
         </Text>
         <Text style={[a.body_1_lg_regular, {color: p.text_gray_low}]}>
