031a202a2d25aa86db023999f8f7bc6483da9cad
amurmurmur <amurmurmur@gmail.com>
2025-09-04 19:28:50 +0300

    feat: eas setup

--- mobile/src/kernel/constants.ts
+++ mobile/src/kernel/constants.ts
@@ -1,21 +1,18 @@
 import {ThemeName} from '@yoroi/theme'
 import {App, Portfolio} from '@yoroi/types'
 
-import Constants from 'expo-constants'
 import * as Device from 'expo-device'
 import {freeze} from 'immer'
 import {Platform} from 'react-native'
 
-const getEnvString = (key: string) => Constants.expoConfig?.extra?.[key] ?? ''
-
 // Resolvers API Keys
-export const unstoppableApiKey = getEnvString('UNSTOPPABLE_API_KEY')
+export const unstoppableApiKey = process.env.EXPO_PUBLIC_UNSTOPPABLE_API_KEY
 
 // Dev
-export const commit = getEnvString('COMMIT')
-export const buildVariant = getEnvString('BUILD_VARIANT')
+export const commit = process.env.EXPO_PUBLIC_COMMIT
+export const buildVariant = process.env.EXPO_PUBLIC_BUILD_VARIANT
 export const disableLogbox = Boolean(
-  Constants.expoConfig?.extra?.DISABLE_LOGBOX,
+  process.env.EXPO_PUBLIC_DISABLE_LOGBOX,
 )
 
 // Runtime
@@ -45,19 +42,18 @@ export const isWeb =
 export const platform = isIOS ? 'ios' : isAndroid ? 'android' : 'web'
 
 // Swap Fees
-export const frontendFeeAddressMainnet = getEnvString(
-  'FRONTEND_FEE_ADDRESS_MAINNET',
-)
-export const frontendFeeAddressPreprod = getEnvString(
-  'FRONTEND_FEE_ADDRESS_PREPROD',
-)
+export const frontendFeeAddressMainnet = 
+  process.env.EXPO_PUBLIC_FRONTEND_FEE_ADDRESS_MAINNET
+export const frontendFeeAddressPreprod = 
+  process.env.EXPO_PUBLIC_FRONTEND_FEE_ADDRESS_PREPROD
+
 
 // Ramp on/off
-export const banxaTestWallet = getEnvString('BANXA_TEST_WALLET')
+export const banxaTestWallet = process.env.EXPO_PUBLIC_BANXA_TEST_WALLET
 
 // Logger
-export const sentryDsn = getEnvString('SENTRY_DSN')
-const envLoggerFilter = getEnvString('LOGGER_FILTER')
+export const sentryDsn = process.env.EXPO_PUBLIC_SENTRY_DSN
+const envLoggerFilter = process.env.EXPO_PUBLIC_LOGGER_FILTER
 export const loggerFilter = envLoggerFilter ? new RegExp(envLoggerFilter) : null
 export const loggerLevel: App.Logger.Level = isDev
   ? App.Logger.Level.Debug
