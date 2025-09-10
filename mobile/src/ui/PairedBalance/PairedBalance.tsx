c1144de671e2d9d483add7a279b4d899e87d3c2a
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-10 13:03:02 +0200

    CR: udpate

--- mobile/src/ui/PairedBalance/PairedBalance.tsx
+++ mobile/src/ui/PairedBalance/PairedBalance.tsx
@@ -41,7 +41,7 @@ export const PairedBalance = React.forwardRef<ResetErrorRef, Props>(
   },
 )
 
-const Price = ({amount, ignorePrivacy, hidePrimaryPair}: Props) => {
+const Price = ({amount, ignorePrivacy, hidePrimaryPair, textStyle}: Props) => {
   const {isPrivacyActive, privacyPlaceholder} = usePrivacyMode()
   const {isPrimaryTokenActive} = usePortfolio()
   const {
