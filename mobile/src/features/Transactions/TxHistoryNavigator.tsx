8d591cfef7e35163ef6d8149551317614d0d462b
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-10 17:02:21 +0200

    feature(mobile): restore ramp om-off initial changes

--- mobile/src/features/Transactions/TxHistoryNavigator.tsx
+++ mobile/src/features/Transactions/TxHistoryNavigator.tsx
@@ -1,4 +1,9 @@
 import {
+  ExchangeProvider,
+  exchangeApiMaker,
+  exchangeManagerMaker,
+} from '@yoroi/exchange'
+import {
   ResolverProvider,
   resolverApiMaker,
   resolverManagerMaker,
@@ -33,6 +38,9 @@ import {TxHistoryRoutes} from '~/kernel/navigation/types'
 import {Boundary} from '~/ui/Boundary/Boundary'
 
 import {ShowSuccessScreen} from '../Claim/useCases/ShowSuccessScreen'
+import {CreateExchangeOrderScreen} from '../Exchange/useCases/CreateExchangeOrderScreen/CreateExchangeOrderScreen'
+import {SelectProviderFromListScreen} from '../Exchange/useCases/SelectProviderFromListScreen/SelectProviderFromListScreen'
+import {ShowExchangeResultOrderScreen} from '../Exchange/useCases/ShowExchangeResultOrderScreen/ShowExchangeResultOrderScreen'
 import {ViewNotificationHistoryScreen} from '../Notifications/useCases/ViewNotificationHistory/ViewNotificationHistoryScreen'
 import {SwapNavigator} from '../Swap/navigator'
 import {useSelectedNetwork} from '../WalletManager/hooks/useSelectedNetwork'
@@ -74,181 +82,224 @@ export const TxHistoryNavigator = () => {
     return resolverManagerMaker(resolverStorage, resolverApi)
   }, [resolverStorage, resolverApi])
 
+  const exchangeManager = React.useMemo(() => {
+    const api = exchangeApiMaker({
+      isProduction: isMainnet,
+      partner: 'emurgo',
+    })
+    return exchangeManagerMaker({api})
+  }, [isMainnet])
+
   const {meta} = useSelectedWallet()
 
   return (
     <ResolverProvider resolverManager={resolverManager}>
       <ReceiveProvider>
-        <Boundary loading={{size: 'full'}}>
-          <Stack.Navigator
-            screenOptions={{
-              ...navigationOptions,
-              headerTitle: ({children}) => <NetworkTag>{children}</NetworkTag>,
-            }}
-          >
-            <Stack.Screen
-              name="history-list"
-              options={{
-                title: meta.name,
-                headerRight: () => <HeaderRightHistory />,
-                headerTransparent: true,
-                headerStyle: {
-                  ...a.bg_transparent,
-                  elevation: 0,
-                  shadowOpacity: 0,
-                },
-                headerTitleStyle: {
-                  color: p.gray_max,
-                },
-                headerTintColor: p.gray_max,
+        <ExchangeProvider manager={exchangeManager}>
+          <Boundary loading={{size: 'full'}}>
+            <Stack.Navigator
+              screenOptions={{
+                ...navigationOptions,
+                headerTitle: ({children}) => (
+                  <NetworkTag>{children}</NetworkTag>
+                ),
               }}
-              getComponent={() => TxHistory}
-            />
+            >
+              <Stack.Screen
+                name="history-list"
+                options={{
+                  title: meta.name,
+                  headerRight: () => <HeaderRightHistory />,
+                  headerTransparent: true,
+                  headerStyle: {
+                    ...a.bg_transparent,
+                    elevation: 0,
+                    shadowOpacity: 0,
+                  },
+                  headerTitleStyle: {
+                    color: p.gray_max,
+                  },
+                  headerTintColor: p.gray_max,
+                }}
+                getComponent={() => TxHistory}
+              />
 
-            <Stack.Screen
-              name="tx-details"
-              options={{
-                title: strings.transactions.history.txDetailsTitle,
-              }}
-              getComponent={() => TxDetails}
-            />
+              <Stack.Screen
+                name="tx-details"
+                options={{
+                  title: strings.transactions.history.txDetailsTitle,
+                }}
+                getComponent={() => TxDetails}
+              />
 
-            <Stack.Screen
-              name="utxo-list"
-              options={{
-                title: strings.transactions.utxo.utxoListTitle,
-              }}
-              getComponent={() => UtxoList}
-            />
+              <Stack.Screen
+                name="utxo-list"
+                options={{
+                  title: strings.transactions.utxo.utxoListTitle,
+                }}
+                getComponent={() => UtxoList}
+              />
 
-            <Stack.Screen
-              name="utxo-consolidation"
-              options={{
-                title: strings.transactions.utxo.utxoConsolidationTitle,
-              }}
-              getComponent={() => UtxoConsolidation}
-            />
+              <Stack.Screen
+                name="utxo-consolidation"
+                options={{
+                  title: strings.transactions.utxo.utxoConsolidationTitle,
+                }}
+                getComponent={() => UtxoConsolidation}
+              />
 
-            {/* Send Screens */}
-            <Stack.Screen
-              name="send-start-tx"
-              options={{
-                title: strings.send.sendTitle,
-              }}
-              getComponent={() => StartMultiTokenTxScreen}
-            />
+              {/* Send Screens */}
+              <Stack.Screen
+                name="send-start-tx"
+                options={{
+                  title: strings.send.sendTitle,
+                }}
+                getComponent={() => StartMultiTokenTxScreen}
+              />
 
-            <Stack.Screen
-              name="send-list-amounts-to-send"
-              options={{
-                title: strings.send.listAmountsToSendTitle,
-              }}
-              getComponent={() => ListAmountsToSendScreen}
-            />
+              <Stack.Screen
+                name="send-list-amounts-to-send"
+                options={{
+                  title: strings.send.listAmountsToSendTitle,
+                }}
+                getComponent={() => ListAmountsToSendScreen}
+              />
 
-            <Stack.Screen
-              name="send-edit-amount"
-              options={{
-                title: strings.send.editAmountTitle,
-              }}
-              getComponent={() => EditAmountScreen}
-            />
+              <Stack.Screen
+                name="send-edit-amount"
+                options={{
+                  title: strings.send.editAmountTitle,
+                }}
+                getComponent={() => EditAmountScreen}
+              />
 
-            <Stack.Screen
-              name="send-select-token-from-list"
-              options={{
-                title: strings.send.selectTokenTitle,
-              }}
-              getComponent={() => SelectTokenFromListScreen}
-            />
+              <Stack.Screen
+                name="send-select-token-from-list"
+                options={{
+                  title: strings.send.selectTokenTitle,
+                }}
+                getComponent={() => SelectTokenFromListScreen}
+              />
 
-            <Stack.Screen
-              name="send-submitted-tx"
-              options={{
-                headerShown: false,
-              }}
-              getComponent={() => SendSubmittedTxScreen}
-            />
+              <Stack.Screen
+                name="send-submitted-tx"
+                options={{
+                  headerShown: false,
+                }}
+                getComponent={() => SendSubmittedTxScreen}
+              />
 
-            <Stack.Screen
-              name="send-failed-tx"
-              options={{
-                headerShown: false,
-              }}
-              getComponent={() => SendFailedTxScreen}
-            />
+              <Stack.Screen
+                name="send-failed-tx"
+                options={{
+                  headerShown: false,
+                }}
+                getComponent={() => SendFailedTxScreen}
+              />
 
-            {/* Receive Screens */}
-            <Stack.Screen
-              name="receive-single"
-              options={{
-                title: strings.receive.receiveTitle,
-              }}
-              getComponent={() => DescribeSelectedAddressScreen}
-            />
+              {/* Receive Screens */}
+              <Stack.Screen
+                name="receive-single"
+                options={{
+                  title: strings.receive.receiveTitle,
+                }}
+                getComponent={() => DescribeSelectedAddressScreen}
+              />
 
-            <Stack.Screen
-              name="receive-multiple"
-              options={{
-                title: strings.receive.multipleAddress,
-              }}
-              getComponent={() => ListMultipleAddressesScreen}
-            />
+              <Stack.Screen
+                name="receive-multiple"
+                options={{
+                  title: strings.receive.multipleAddress,
+                }}
+                getComponent={() => ListMultipleAddressesScreen}
+              />
 
-            <Stack.Screen
-              name="receive-specific-amount"
-              options={{
-                title: strings.receive.specificAmount,
-              }}
-              getComponent={() => RequestSpecificAmountScreen}
-            />
+              <Stack.Screen
+                name="receive-specific-amount"
+                options={{
+                  title: strings.receive.specificAmount,
+                }}
+                getComponent={() => RequestSpecificAmountScreen}
+              />
 
-            {/* Scan Screens */}
-            <Stack.Screen
-              name="scan-start"
-              options={{
-                title: strings.scan.scanTitle,
-              }}
-              getComponent={() => ScanCodeScreen}
-            />
+              {/* Scan Screens */}
+              <Stack.Screen
+                name="scan-start"
+                options={{
+                  title: strings.scan.scanTitle,
+                }}
+                getComponent={() => ScanCodeScreen}
+              />
 
-            <Stack.Screen
-              name="scan-show-camera-permission-denied"
-              options={{
-                title: strings.scan.cameraPermissionDeniedTitle,
-              }}
-              getComponent={() => ShowCameraPermissionDeniedScreen}
-            />
+              <Stack.Screen
+                name="scan-show-camera-permission-denied"
+                options={{
+                  title: strings.scan.cameraPermissionDeniedTitle,
+                }}
+                getComponent={() => ShowCameraPermissionDeniedScreen}
+              />
 
-            <Stack.Screen
-              name="swap"
-              options={{
-                headerShown: false,
-              }}
-              getComponent={() => SwapNavigator}
-            />
+              <Stack.Screen
+                name="swap"
+                options={{
+                  headerShown: false,
+                }}
+                getComponent={() => SwapNavigator}
+              />
 
-            {/* Notification Center */}
-            <Stack.Screen
-              name="notification-center-history"
-              options={{
-                title: strings.notifications.notificationCenter,
-                headerShown: true,
-              }}
-              getComponent={() => ViewNotificationHistoryScreen}
-            />
+              {/* Notification Center */}
+              <Stack.Screen
+                name="notification-center-history"
+                options={{
+                  title: strings.notifications.notificationCenter,
+                }}
+                getComponent={() => ViewNotificationHistoryScreen}
+              />
 
-            {/* Claim Screens */}
-            <Stack.Screen
-              name="claim-show-success"
-              options={{
-                title: strings.claim.showSuccessTitle,
-                headerShown: true,
-              }}
-              getComponent={() => ShowSuccessScreen}
-            />
-          </Stack.Navigator>
-        </Boundary>
+              {/* Claim Screens */}
+              <Stack.Screen
+                name="claim-show-success"
+                options={{
+                  title: strings.claim.showSuccessTitle,
+                }}
+                getComponent={() => ShowSuccessScreen}
+              />
+
+              {/* Exchange Screens */}
+              <Stack.Screen
+                name="exchange-create-order"
+                options={{
+                  title: strings.exchange.buyCrypto,
+                }}
+                getComponent={() => CreateExchangeOrderScreen}
+              />
+
+              <Stack.Screen
+                name="exchange-result"
+                options={{
+                  title: strings.exchange.title,
+                }}
+                getComponent={() => ShowExchangeResultOrderScreen}
+              />
+
+              <Stack.Screen
+                name="exchange-select-buy-provider"
+                options={{
+                  title: strings.exchange.provider,
+                }}
+                getComponent={() => SelectProviderFromListScreen}
+              />
+
+              <Stack.Screen
+                name="exchange-select-sell-provider"
+                options={{
+                  title: strings.exchange.provider,
+                }}
+                getComponent={() => SelectProviderFromListScreen}
+              />
+            </Stack.Navigator>
+          </Boundary>
+        </ExchangeProvider>
       </ReceiveProvider>
     </ResolverProvider>
   )
--- mobile/src/kernel/i18n/LocalizableError.ts
+++ mobile/src/kernel/i18n/LocalizableError.ts
@@ -1,7 +1,13 @@
 import {MessageDescriptor} from 'react-intl'
 
 export class LocalizableError extends Error {
-  constructor(readonly descriptor: MessageDescriptor) {
+  public values?: Record<string, any>
+
+  constructor(
+    readonly descriptor: MessageDescriptor,
+    values?: Record<string, any>,
+  ) {
     super()
+    this.values = values
   }
 }
