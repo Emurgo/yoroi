43fc26975bef2cb4ace8b9705d2b85c8a13da60d
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-11 12:40:42 +0200

    feature(mobile): ramp on-off integration

--- mobile/src/kernel/i18n/LocalizableError.ts
+++ mobile/src/kernel/i18n/LocalizableError.ts
@@ -1,13 +1,7 @@
 import {MessageDescriptor} from 'react-intl'
 
 export class LocalizableError extends Error {
-  public values?: Record<string, any>
-
-  constructor(
-    readonly descriptor: MessageDescriptor,
-    values?: Record<string, any>,
-  ) {
+  constructor(readonly descriptor: MessageDescriptor) {
     super()
-    this.values = values
   }
 }
