51097f8cdf352910b1f54a9b7af47ffd836c7127
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-11 12:43:44 +0200

    feature(mobile): ramp on-off integration

--- mobile/src/kernel/i18n/helpers.ts
+++ mobile/src/kernel/i18n/helpers.ts
@@ -5,7 +5,7 @@ import {LocalizableError} from './LocalizableError'
 export const getTranslatedError = (intl: IntlShape) => {
   return (error?: Error | LocalizableError) => {
     if (error instanceof LocalizableError) {
-      return intl.formatMessage(error.descriptor, error.values)
+      return intl.formatMessage(error.descriptor)
     }
 
     const errorMessage = error?.toString()
