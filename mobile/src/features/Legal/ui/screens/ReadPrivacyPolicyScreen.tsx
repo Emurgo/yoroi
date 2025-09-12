5d71ed4895e2f8506803422b7fddcc2ebac6e873
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-09-12 23:49:15 +0100

    fix(mobile): CR updates

--- mobile/src/features/Legal/ui/screens/ReadPrivacyPolicyScreen.tsx
+++ mobile/src/features/Legal/ui/screens/ReadPrivacyPolicyScreen.tsx
@@ -1,18 +1,20 @@
-import {atoms as a} from '@yoroi/theme'
+import {atoms as a, useTheme} from '@yoroi/theme'
 
 import * as React from 'react'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
-import {PrivacyPolicy} from '~/features/Legal/ui/shared/PrivacyPolicy/PrivacyPolicy'
 import {useLanguage} from '~/kernel/i18n/LanguageProvider'
+
+import {PrivacyPolicy} from '../shared/PrivacyPolicy/PrivacyPolicy'
 
 export const ReadPrivacyPolicyScreen = () => {
   const {languageCode} = useLanguage()
+  const {atoms: ta} = useTheme()
 
   return (
     <SafeAreaView
       edges={['left', 'right', 'bottom']}
-      style={[a.flex_1, a.px_lg]}
+      style={[a.flex_1, a.px_lg, ta.bg_color_max]}
     >
       <PrivacyPolicy languageCode={languageCode} />
     </SafeAreaView>
--- mobile/src/features/Legal/ui/screens/ReadTermsOfServiceScreen.tsx
+++ mobile/src/features/Legal/ui/screens/ReadTermsOfServiceScreen.tsx
@@ -1,18 +1,20 @@
-import {atoms as a} from '@yoroi/theme'
+import {atoms as a, useTheme} from '@yoroi/theme'
 
 import * as React from 'react'
 import {SafeAreaView} from 'react-native-safe-area-context'
 
-import {TermsOfService} from '~/features/Legal/ui/shared/TermsOfService/TermsOfService'
 import {useLanguage} from '~/kernel/i18n/LanguageProvider'
+
+import {TermsOfService} from '../shared/TermsOfService/TermsOfService'
 
 export const ReadTermsOfServiceScreen = () => {
   const {languageCode} = useLanguage()
+  const {atoms: ta} = useTheme()
 
   return (
     <SafeAreaView
       edges={['left', 'right', 'bottom']}
-      style={[a.flex_1, a.px_lg]}
+      style={[a.flex_1, a.px_lg, ta.bg_color_max]}
     >
       <TermsOfService languageCode={languageCode} />
     </SafeAreaView>
