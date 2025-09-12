f017c8ba53b9ec7fa18fbf983b725638c44a0be8
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-12 15:34:12 +0200

    fix(mobile): ios startup crash

--- mobile/index.ts
+++ mobile/index.ts
@@ -1,11 +1,9 @@
 /* prettier-ignore-file */
-
 import {registerRootComponent} from 'expo'
-
 import 'react-native-gesture-handler'
-import './src/kernel/logger/helpers/init-logger'
-import './src/kernel/shims'
 
 import App from './App'
+import './src/kernel/logger/helpers/init-logger'
+import './src/kernel/shims'
 
 registerRootComponent(App)
