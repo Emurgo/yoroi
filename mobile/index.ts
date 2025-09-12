face68f1b56003f0d83662e79e770e6fa368cf09
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-09-12 19:11:08 +0100

    fix(mobile): index prettier

--- mobile/index.ts
+++ mobile/index.ts
@@ -1,9 +1,9 @@
 /* prettier-ignore-file */
-import {registerRootComponent} from 'expo'
-import 'react-native-gesture-handler'
+import { registerRootComponent } from "expo";
+import "react-native-gesture-handler";
+import "./src/kernel/logger/helpers/init-logger";
+import "./src/kernel/shims";
 
-import App from './App'
-import './src/kernel/logger/helpers/init-logger'
-import './src/kernel/shims'
+import App from "./App";
 
-registerRootComponent(App)
+registerRootComponent(App);
