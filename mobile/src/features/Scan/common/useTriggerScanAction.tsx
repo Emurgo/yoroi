794a59f401dc84465bdee784c271bde3f27bda56
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-15 16:09:51 +0200

    CR: update

--- mobile/src/features/Scan/common/useTriggerScanAction.tsx
+++ mobile/src/features/Scan/common/useTriggerScanAction.tsx
@@ -57,11 +57,6 @@ export const useTriggerScanAction = ({
       const claimErrorDialog = claimErrorResolver(error)
       if (claimErrorDialog) {
         Alert.alert(claimErrorDialog.title, claimErrorDialog.message)
-      } else {
-        Alert.alert(
-          'Error',
-          'An unexpected error occurred during claim processing',
-        )
       }
     },
   })
