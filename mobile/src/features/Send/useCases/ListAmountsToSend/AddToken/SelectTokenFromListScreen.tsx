c8f95a51c3e53a152997c9b983a0010d8f4fc9c1
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-09-17 09:46:22 +0100

    Update mobile/src/features/Send/useCases/ListAmountsToSend/AddToken/SelectTokenFromListScreen.tsx
    Signed-off-by: Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>

--- mobile/src/features/Send/useCases/ListAmountsToSend/AddToken/SelectTokenFromListScreen.tsx
+++ mobile/src/features/Send/useCases/ListAmountsToSend/AddToken/SelectTokenFromListScreen.tsx
@@ -98,7 +98,7 @@ export const SelectTokenFromListScreen = () => {
   return (
     <SafeAreaView
       style={[a.flex_1, {backgroundColor: p.bg_color_max}]}
-      edges={['bottom']}
+      edges={['bottom', 'left', 'right']}
     >
       <View style={[a.px_lg]}>
         {isSearchOpened === false && (
