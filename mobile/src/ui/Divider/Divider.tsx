c12027f609436b08d66adb3f3eadf2ef3c10237c
banklesss <105349292+banklesss@users.noreply.github.com>
2025-09-18 19:35:08 +0200

    refactor(mobile): tx review

--- mobile/src/ui/Divider/Divider.tsx
+++ mobile/src/ui/Divider/Divider.tsx
@@ -1,9 +1,7 @@
-import {SpacingSize, atoms as a, useTheme} from '@yoroi/theme'
+import {SpacingSize, atoms as a, tokens, useTheme} from '@yoroi/theme'
 
 import * as React from 'react'
 import {View} from 'react-native'
-
-import {Space} from '~/ui/Space/Space'
 
 export const Divider = ({
   verticalSpace = 'xs',
@@ -11,37 +9,13 @@ export const Divider = ({
   verticalSpace?: SpacingSize
 }) => {
   const {palette: p} = useTheme()
-
-  const SpaceComponent = React.useMemo(() => {
-    switch (verticalSpace) {
-      case '_2xs':
-        return Space.Height._2xs
-      case 'xs':
-        return Space.Height.xs
-      case 'sm':
-        return Space.Height.sm
-      case 'md':
-        return Space.Height.md
-      case 'lg':
-        return Space.Height.lg
-      case 'xl':
-        return Space.Height.xl
-      case '_2xl':
-        return Space.Height._2xl
-      default:
-        return Space.Height.xs
-    }
-  }, [verticalSpace])
+  const spacing = tokens.space[verticalSpace]
 
   return (
-    <>
-      <SpaceComponent />
-
+    <View style={{paddingVertical: spacing}}>
       <View
         style={[{height: 1}, a.align_stretch, {backgroundColor: p.gray_200}]}
       />
-
-      <SpaceComponent />
-    </>
+    </View>
   )
 }
