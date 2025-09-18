b6b10533a73a2e5c86774365346c9a7e0bfbc926
jorbuedo <jorbuedo@gmail.com>
2025-09-18 12:35:09 +0200

    fix

--- mobile/src/features/Swap/useCases/CreateOrder/SwapMainScreen.tsx
+++ mobile/src/features/Swap/useCases/CreateOrder/SwapMainScreen.tsx
@@ -3,6 +3,7 @@ import {atoms as a, useTheme} from '@yoroi/theme'
 import * as React from 'react'
 import {Text, View, useWindowDimensions} from 'react-native'
 import {ScrollView} from 'react-native-gesture-handler'
+import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {ShowDisclaimer} from '~/features/Legal/ui/shared/Disclaimer/ShowDisclaimer'
 import {AmountCard} from '~/features/Swap/common/AmountCard/AmountCard'
@@ -30,7 +31,7 @@ const BOTTOM_ACTION_SECTION = 180
 export const SwapMainScreen = () => {
   const [contentHeight, setContentHeight] = React.useState(0)
   const strings = useStrings()
-  const {palette: p} = useTheme()
+  const {palette: p, atoms: ta} = useTheme()
   const {height: deviceHeight} = useWindowDimensions()
   const isKeyboardOpen = useIsKeyboardOpen()
   const swapForm = useSwap()
@@ -89,7 +90,8 @@ export const SwapMainScreen = () => {
   }
 
   return (
-    <View style={[a.flex_1, a.pb_lg, {backgroundColor: p.bg_color_max}]}>
+    <SafeAreaView style={[a.flex_1, a.pb_lg, ta.bg_color_max]}>
+      {' '}
       <ScrollView style={[a.px_lg]}>
         <ShowDisclaimer type="swap" />
 
@@ -223,7 +225,6 @@ export const SwapMainScreen = () => {
           </View>
         </View>
       </ScrollView>
-
       <View
         style={[
           a.p_lg,
@@ -245,6 +246,6 @@ export const SwapMainScreen = () => {
           onPress={onSwapPress}
         />
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
