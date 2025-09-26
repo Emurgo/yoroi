c5f5f63628fe653a15196a95c13d9eeac0cc1738
Rahul <rahulnair87@gmail.com>
2025-09-26 12:33:25 +0530

    fix bottom padding in Dex screen

--- mobile/src/features/Swap/useCases/CreateOrder/SelectProtocolScreen.tsx
+++ mobile/src/features/Swap/useCases/CreateOrder/SelectProtocolScreen.tsx
@@ -44,7 +44,7 @@ export const SelectProtocolScreen = () => {
 
   return (
     <SafeAreaView
-      style={[{backgroundColor: p.bg_color_max}]}
+      style={[{backgroundColor: p.bg_color_max, flex: 1}]}
       edges={['left', 'right', 'bottom']}
     >
       <FlatList
