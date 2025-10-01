76d103682a73ff70d888f4771af7a285a0165b9e
Rahul <rahulnair87@gmail.com>
2025-10-01 20:46:26 +0530

    fix tests

--- mobile/jest.setup.js
+++ mobile/jest.setup.js
@@ -149,3 +149,10 @@ jest.mock('react-native-ble-plx', () => ({
     BleError: class BleError {
     },
 }))
+
+jest.mock('@yoroi/blockchains', () => ({
+    cardanoConfig: {
+      denominations: {lovelace: 1n, ada: 1_000_000n},
+      params: {minUtxoValue: 1_000_000n},
+    },
+  }));
