2a2cff58157255c03b7b0507300b6d9ecdd9d823
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-10-10 11:00:56 +0100

    fix(mobile): receive modals

--- mobile/src/features/Send/useCases/ListAmountsToSend/ListAmountsToSendScreen.tsx
+++ mobile/src/features/Send/useCases/ListAmountsToSend/ListAmountsToSendScreen.tsx
@@ -7,7 +7,6 @@ import {useNavigation} from '@react-navigation/native'
 import * as React from 'react'
 import {TouchableOpacity, View} from 'react-native'
 import {FlatList} from 'react-native-gesture-handler'
-import {SafeAreaView} from 'react-native-safe-area-context'
 
 import {useReviewTx} from '~/features/ReviewTx/common/ReviewTxProvider'
 import {useSearch} from '~/features/Search/SearchContext'
@@ -25,7 +24,7 @@ import {Boundary} from '~/ui/Boundary/Boundary'
 import {Button} from '~/ui/Button/Button'
 import {Icon} from '~/ui/Icon'
 import {RemoveAmountButton} from '~/ui/RemoveAmountButton/RemoveAmountButton'
-import {Space} from '~/ui/Space/Space'
+import {SafeArea} from '~/ui/SafeArea/SafeArea'
 import {TokenAmountItem} from '~/ui/TokenAmountItem/TokenAmountItem'
 import {YoroiEntry, YoroiSignedTx, YoroiUnsignedTx} from '~/wallets/types/yoroi'
 
@@ -38,7 +37,6 @@ export const ListAmountsToSendScreen = () => {
   const {track} = useMetrics()
   const {wallet} = useSelectedWallet()
   const {unsignedTxChanged} = useReviewTx()
-  const {atoms: ta} = useTheme()
   const {
     memo,
     targets,
@@ -137,10 +135,7 @@ export const ListAmountsToSendScreen = () => {
     createUnsignedTx([toYoroiEntry(targets[selectedTargetIndex].entry)])
   }
   return (
-    <SafeAreaView
-      edges={['left', 'right', 'bottom']}
-      style={[a.flex_1, a.px_lg, a.pb_lg, a.gap_xl, ta.bg_color_max]}
-    >
+    <SafeArea>
       <AmountsList
         data={Object.values(amounts)}
         renderItem={({item: amount}) => (
@@ -155,16 +150,12 @@ export const ListAmountsToSendScreen = () => {
         bounces={false}
         keyExtractor={(item) => item.info.id}
         testID="selectedTokens"
+        contentContainerStyle={[a.px_lg]}
+        style={[a.pt_lg]}
       />
 
-      <Actions style={[a.bg_transparent]}>
-        <Row>
-          <Space.Height._2xs fill />
-
-          <AddTokenButton onPress={handleOnAdd} />
-        </Row>
-
-        <Space.Height.xl />
+      <SafeArea.Footer style={[a.bg_transparent, a.gap_lg]}>
+        <AddTokenButton onPress={handleOnAdd} />
 
         <NextButton
           onPress={handleOnNext}
@@ -172,8 +163,8 @@ export const ListAmountsToSendScreen = () => {
           disabled={selectedTokensCounter === 0}
           isLoading={isPending}
         />
-      </Actions>
-    </SafeAreaView>
+      </SafeArea.Footer>
+    </SafeArea>
   )
 }
 
@@ -241,7 +232,6 @@ const ListAmountsNavigateBackButton = () => {
 
 const Left = View
 const Right = View
-const Actions = View
 const Row = View
 const NextButton = Button
 const AmountsList = FlatList
