2ac0a3fb45ac4bae00e0039d059b7abf16a8eaba
Abdurashid Abdullaev <abdulllayevabdurashid@gmail.com>
2025-10-14 11:38:25 +0200

    Refactor SingleOrMultipleAddressesModal: Separate content and footer components
    - Created SingleOrMultipleAddressesModalContent for the modal's main content.
    - Renamed SingleOrMultipleAddressesModal to SingleOrMultipleAddressesModalFooter.
    - Updated imports and adjusted layout styles for better UI organization.

--- mobile/src/ui/SingleOrMultipleAddressesModal/SingleOrMultipleAddressesModal.tsx
+++ mobile/src/ui/SingleOrMultipleAddressesModal/SingleOrMultipleAddressesModal.tsx
@@ -8,15 +8,34 @@ import {useMultipleAddressesInfo} from '~/features/Receive/common/useMultipleAdd
 import {useAddressMode} from '~/features/WalletManager/hooks/useAddressMode'
 import {useStrings} from '~/kernel/i18n/useStrings'
 import {Button, ButtonType} from '~/ui/Button/Button'
+import {Modal} from '~/ui/Modal/ui/screens/Modal/Modal'
 
 export const singleOrMultipleAddressesModalHeight = 580
 
-type Props = {
-  onConfirm: (addressMode: Wallet.AddressMode) => void
-  onClose: () => void
+const SingleOrMultipleAddressesModalContent = () => {
+  const strings = useStrings()
+  const {atoms: ta} = useTheme()
+
+  return (
+    <View style={[a.flex_1, a.align_center, a.justify_between, a.py_lg]}>
+      {/* @bankless TODO: maybe rexport the component from Figma, it's blowing the UI */}
+      {/* <QRs /> */}
+
+      <Text
+        style={[
+          a.body_1_lg_regular,
+          a.justify_center,
+          a.text_center,
+          ta.text_gray_medium,
+        ]}
+      >
+        {strings.receive.singleOrMultipleDetails}
+      </Text>
+    </View>
+  )
 }
 
-export const SingleOrMultipleAddressesModal = ({onConfirm, onClose}: Props) => {
+const SingleOrMultipleAddressesModalFooter = ({onConfirm, onClose}: Props) => {
   const strings = useStrings()
   const {enableMultipleMode, enableSingleMode} = useAddressMode()
 
