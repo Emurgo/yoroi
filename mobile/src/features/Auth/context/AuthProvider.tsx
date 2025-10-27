413804ae1d0fa17f26b77bfc140c2994a7125eb4
Juliano Lazzarotto <30806844+stackchain@users.noreply.github.com>
2025-10-27 10:06:50 +0000

    Revert "Apply suggestion from @stackchain"
    This reverts commit 3800c80a9a93c03fc31495c768109b641286b325.

--- mobile/src/features/Auth/context/AuthProvider.tsx
+++ mobile/src/features/Auth/context/AuthProvider.tsx
@@ -26,7 +26,7 @@ export const AuthProvider: React.FC<React.PropsWithChildren<Props>> = ({
 
   // NOTE: This should be configurable
   useBackgroundTimer({
-    after: time.minutes(1),
+    after: time.minutes(5),
     execute: () => {
       if (loggedState.status === 'logged-in') {
         logger.debug('logout (auto)', {origin: 'AuthProvider', type: 'user'})
