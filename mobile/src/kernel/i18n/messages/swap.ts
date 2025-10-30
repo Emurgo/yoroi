004c57259bc558461f3af81266cb940f22e4bad8
jorbuedo <jorbuedo@gmail.com>
2025-10-30 13:05:40 +0100

    fix min ada

--- mobile/src/kernel/i18n/messages/swap.ts
+++ mobile/src/kernel/i18n/messages/swap.ts
@@ -250,7 +250,7 @@ export const swapMessages = defineMessages({
     defaultMessage: '!!!Swap Min ADA',
   },
   swapMinAdaTitle: {
-    id: 'swap.swapScreen.swapTitle',
+    id: 'swap.swapScreen.swapMinAdaTitle',
     defaultMessage: '!!!Swap Min ADA Title',
   },
   swapMinReceived: {
