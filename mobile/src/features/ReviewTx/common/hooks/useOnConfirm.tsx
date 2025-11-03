8d6ddf047c6761bddaf5f3878cfaed70bbfe724e
banklesss <105349292+banklesss@users.noreply.github.com>
2025-11-03 10:59:22 +0100

    refactor(mobile): submit tx

--- mobile/src/features/ReviewTx/common/hooks/useOnConfirm.tsx
+++ mobile/src/features/ReviewTx/common/hooks/useOnConfirm.tsx
@@ -134,7 +134,9 @@ export const useOnConfirm = ({
       onSuccess: async (rootKey: string) => {
         if (!preventSubmit) {
           try {
-            await submitTx(cbor, rootKey, wallet, meta)
+            const success = await submitTx(cbor, rootKey, wallet, meta)
+            if (!success)
+              throw new Error('useOnConfirm:: not possible to sign tx')
           } catch (e) {
             handleOnError(e)
             return
@@ -156,15 +158,16 @@ const submitTx = async (
   rootKey: string,
   wallet: YoroiWallet,
   meta: Wallet.Meta,
-) => {
+): Promise<boolean> => {
   return CardanoMobileWrapped.cslScope(async (csl) => {
     const signers = getTransactionSigners(cbor, wallet, meta)
     const keys = signers.map((signer) =>
       createRawTxSigningKey(rootKey, signer, csl),
     )
     const response = await wallet.signRawTx(cbor, keys)
-    if (!response) throw new Error('useOnConfirm:: not possible to sign tx')
+    if (!response) return false
     const hexBase64 = Buffer.from(response).toString('base64')
     await wallet.submitTransaction(hexBase64)
+    return true
   })
 }
