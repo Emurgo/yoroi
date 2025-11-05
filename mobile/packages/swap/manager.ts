5dbbd2dcc7f7598038f20d5549514a59f35e0b21
jorbuedo <jorbuedo@gmail.com>
2025-11-05 12:04:45 +0100

    add failsafe

--- mobile/packages/swap/manager.ts
+++ mobile/packages/swap/manager.ts
@@ -158,6 +158,10 @@ const apiManagerMaker = (
             order.aggregator === Swap.Aggregator.Dexhunter
           )
             merged[key] = order
+
+          // Make sure we have Dexhunter's customId in case we need to cancel the order with them
+          if (order.customId && merged[key])
+            merged[key].customId = order.customId
         }
 
         responses
