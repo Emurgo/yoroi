2e4bf44e91c1d491bc91f2d11db1fe1c7aab9d5f
Abdurashid Abdullaev <abdulllayevabdurashid@gmail.com>
2025-11-19 16:18:22 +0100

    refactor(mobile): simplify EarnRewardsBanner styles and remove unused StyleSheet

--- mobile/src/features/Staking/Governance/useCases/EarnRewardsBanner/EarnRewardsBanner.tsx
+++ mobile/src/features/Staking/Governance/useCases/EarnRewardsBanner/EarnRewardsBanner.tsx
@@ -3,7 +3,7 @@ import {atoms as a, useTheme} from '@yoroi/theme'
 
 import {LinearGradient} from 'expo-linear-gradient'
 import * as React from 'react'
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native'
+import {Image, Pressable, Text, View} from 'react-native'
 
 import GovernanceIllustration from '~/assets/img/governance-banner.png'
 import {useStrings} from '~/kernel/i18n/useStrings'
@@ -24,25 +24,61 @@ export const EarnRewardsBanner = ({onPress, onDismiss}: Props) => {
     : p.bg_gradient_1
 
   return (
-    <View style={[a.p_lg, a.relative, a.overflow_hidden, styles.container]}>
+    <View
+      style={[
+        a.p_lg,
+        a.relative,
+        a.overflow_hidden,
+        {
+          borderRadius: 8,
+          marginHorizontal: 16,
+          paddingRight: 48,
+          minHeight: 182,
+        },
+      ]}
+    >
       <LinearGradient
         colors={gradientColors}
         start={{x: 0, y: 0}}
         end={{x: 1, y: 1}}
-        style={StyleSheet.absoluteFillObject}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
       />
 
       <Pressable
         onPress={onDismiss}
-        style={styles.closeButton}
+        style={{
+          position: 'absolute',
+          top: 12,
+          right: 12,
+          zIndex: 1,
+          padding: 4,
+        }}
         hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}
         testID="earn-rewards-banner-close"
       >
         <Icon.Close size={24} color={p.gray_900} />
       </Pressable>
 
-      <View style={styles.content}>
-        <View style={styles.textContainer}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            paddingRight: 0,
+            paddingTop: 0,
+          }}
+        >
           <Text
             style={[a.body_1_lg_medium, a.font_semibold, {color: p.gray_900}]}
           >
@@ -52,11 +88,7 @@ export const EarnRewardsBanner = ({onPress, onDismiss}: Props) => {
           <Space.Height.sm />
 
           <Text
-            style={[
-              a.body_2_md_regular,
-              {color: p.gray_900},
-              styles.description,
-            ]}
+            style={[a.body_2_md_regular, {color: p.gray_900, width: '90%'}]}
           >
             {strings.staking.earnRewardsBannerDescription}
           </Text>
@@ -65,13 +97,15 @@ export const EarnRewardsBanner = ({onPress, onDismiss}: Props) => {
 
           <Pressable
             onPress={onPress}
-            style={[
-              styles.ctaButton,
-              {
-                borderColor: p.primary_600,
-                backgroundColor: 'transparent',
-              },
-            ]}
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 24,
+              borderRadius: 8,
+              borderWidth: 2,
+              alignSelf: 'flex-start',
+              borderColor: p.primary_600,
+              backgroundColor: 'transparent',
+            }}
             testID="earn-rewards-banner-cta"
           >
             <Text
@@ -87,62 +121,26 @@ export const EarnRewardsBanner = ({onPress, onDismiss}: Props) => {
         </View>
       </View>
 
-      <View style={styles.illustrationContainer}>
+      <View
+        style={{
+          position: 'absolute',
+          right: 0,
+          bottom: 0,
+          width: 10,
+          height: 140,
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end',
+        }}
+      >
         <Image
           source={GovernanceIllustration}
-          style={styles.illustration}
+          style={{
+            width: 100,
+            height: 120,
+          }}
           resizeMode="contain"
         />
       </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    borderRadius: 8,
-    marginHorizontal: 16,
-    paddingRight: 48,
-    minHeight: 182,
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 12,
-    right: 12,
-    zIndex: 1,
-    padding: 4,
-  },
-  content: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  textContainer: {
-    flex: 1,
-    paddingRight: 0,
-    paddingTop: 0,
-  },
-  description: {
-    width: '90%',
-  },
-  ctaButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    borderWidth: 2,
-    alignSelf: 'flex-start',
-  },
-  illustrationContainer: {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-    width: 10,
-    height: 140,
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-  },
-  illustration: {
-    width: 100,
-    height: 120,
-  },
-})
