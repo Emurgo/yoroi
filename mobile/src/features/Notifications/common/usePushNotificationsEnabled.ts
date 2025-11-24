67ec2531dbbeb2fa00e374e2a1379877dd075ed5
banklesss <105349292+banklesss@users.noreply.github.com>
2025-11-24 18:47:17 +0100

    feature(mobile): push notifications feature flasg and packages pin

--- mobile/src/features/Notifications/common/usePushNotificationsEnabled.ts
+++ mobile/src/features/Notifications/common/usePushNotificationsEnabled.ts
@@ -3,5 +3,5 @@ import {useRemoteConfig} from '~/features/RemoteConfig/hooks/useRemoteConfig'
 export const usePushNotificationsEnabled = () => {
   const {config} = useRemoteConfig()
 
-  return config?.features?.pushNotifications ?? true
+  return config?.features?.pushNotifications ?? false
 }
