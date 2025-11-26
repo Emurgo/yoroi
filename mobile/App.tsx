0fda615733cb13e06ab81107b7911c8ca121c048
jorbuedo <jorbuedo@gmail.com>
2025-11-26 15:09:19 +0100

    fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -21,7 +21,6 @@ import * as Updates from 'expo-updates'
 import * as React from 'react'
 
 import {BrowserProvider} from '~/features/Discover/common/BrowserProvider'
-import {P2PConnectionProviderWrapper} from '~/features/P2P/components/P2PConnectionProviderWrapper'
 import {PortfolioTokenActivityProvider} from '~/features/Portfolio/context/PortfolioTokenActivityProvider'
 import {ReceiveProvider} from '~/features/Receive/common/ReceiveProvider'
 import {isDev} from '~/kernel/constants'
@@ -109,17 +108,15 @@ function BusinessShell({children}: React.PropsWithChildren) {
                     <TransferProvider>
                       <SetupWalletProvider>
                         <BrowserProvider>
-                          <P2PConnectionProviderWrapper>
-                            <LinksProvider>
-                              <YoroiNotificationManager>
-                                <CurrencyProvider>
-                                  <CatalystProvider manager={catalystManager}>
-                                    <ReceiveProvider>{children}</ReceiveProvider>
-                                  </CatalystProvider>
-                                </CurrencyProvider>
-                              </YoroiNotificationManager>
-                            </LinksProvider>
-                          </P2PConnectionProviderWrapper>
+                          <LinksProvider>
+                            <YoroiNotificationManager>
+                              <CurrencyProvider>
+                                <CatalystProvider manager={catalystManager}>
+                                  <ReceiveProvider>{children}</ReceiveProvider>
+                                </CatalystProvider>
+                              </CurrencyProvider>
+                            </YoroiNotificationManager>
+                          </LinksProvider>
                         </BrowserProvider>
                       </SetupWalletProvider>
                     </TransferProvider>
