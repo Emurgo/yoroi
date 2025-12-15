977b4f1595b40cc1d0bc70b06164d483b4465d97
banklesss <105349292+banklesss@users.noreply.github.com>
2025-12-15 20:50:49 +0100

    fix(mobile): notifications settings navigation

--- mobile/src/features/Settings/ui/navigation/SettingsScreenNavigator.tsx
+++ mobile/src/features/Settings/ui/navigation/SettingsScreenNavigator.tsx
@@ -55,7 +55,10 @@ export const SettingsScreenNavigator = () => {
   const navOptions = React.useMemo(() => defaultStackNavigationOptions(p), [p])
 
   return (
-    <Stack.Navigator screenOptions={navOptions} initialRouteName="main-settings">
+    <Stack.Navigator
+      screenOptions={navOptions}
+      initialRouteName="main-settings"
+    >
       <Stack.Screen //
         name="app-settings"
         getComponent={() => ApplicationSettingsScreen}
